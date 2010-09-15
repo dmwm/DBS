c1c8106cdeb545f34d54736168c6595062ac2b8d
akhukhun <akhukhun>
2010-09-15 19:21:28 +0000

    Add site_name argument.
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9234 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Block/List.py
+++ Server/Python/src/dbs/dao/Oracle/Block/List.py
@@ -2,8 +2,8 @@
 """
 This module provides Block.List data access object.
 """
-__revision__ = "$Id: List.py,v 1.10 2009/12/27 13:40:20 akhukhun Exp $"
-__version__ = "$Revision: 1.10 $"
+__revision__ = "$Id: List.py,v 1.11 2010/01/01 18:55:10 akhukhun Exp $"
+__version__ = "$Revision: 1.11 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -22,13 +22,13 @@ class List(DBFormatter):
 SELECT B.BLOCK_ID, B.BLOCK_NAME, B.OPEN_FOR_WRITING, 
         B.BLOCK_SIZE, B.FILE_COUNT,
         B.DATASET_ID, DS.DATASET,
-        B.ORIGIN_SITE, SI.SITE_NAME
+        SI.SITE_NAME
 FROM %sBLOCKS B
 JOIN %sDATASETS DS ON DS.DATASET_ID = B.DATASET_ID
 LEFT OUTER JOIN %sSITES SI ON SI.SITE_ID = B.ORIGIN_SITE
 """ % ((self.owner,)*3)
 
-    def execute(self, dataset="", block_name="", conn=None):
+    def execute(self, dataset="", block_name="", site_name="", conn=None):
         """
         dataset: /a/b/c
         block: /a/b/c#d
@@ -40,18 +40,29 @@ LEFT OUTER JOIN %sSITES SI ON SI.SITE_ID = B.ORIGIN_SITE
         op = ("=", "like")["%" in block_name]
         if dataset:
             sql += "WHERE DS.DATASET = :dataset"
-            binds.update({"dataset":dataset})
+            binds.update(dataset=dataset)
         
             if block_name:
                 sql += " AND B.BLOCK_NAME %s :block_name" % op
                 binds.update({"block_name":block_name})
+            if site_name:
+                sql += " AND SI.SITE_NAME = :site_name"
+                binds.update(site_name = site_name)
                 
         elif block_name:
-            sql += "WHERE B.BLOCK_NAME %s :block_name" % op
-            binds.update({"block_name":block_name})
-        
+            sql += "WHERE B.BLOCK_NAME = :block_name"
+            binds.update(block_name = block_name)
+            
+            if site_name:
+                sql += " AND SI.SITE_NAME = :site_name"
+                binds.update(site_name = site_name)
+                
+        elif site_name:
+            sql += " WHERE SI.SITE_NAME = :site_name"
+            binds.update( site_name = site_name)
+            
         else: 
-            raise Exception("Either dataset or block must be provided")
+            raise Exception("dataset, block_name or site_name must be provided")
         
         cursor = conn.connection.cursor()
         cursor.execute(sql, binds)
