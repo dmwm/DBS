49e9525b4a8663203ffd499dfbb6b775730f8055
afaq <afaq>
2010-09-15 17:09:57 +0000

    CVssary file. ----------------------------------------------------------------------
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9022 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/dd.sh
+++
@@ -1,8 +0,0 @@
-ll=`ls -1`
-for l in $ll; do
-	cd /home/anzar/devDBS3/DBS/DBS3/Server/Python/src/dbs/dao/Oracle/$l
-	echo $PWD
-	cvs add *.py
-	cd /home/anzar/devDBS3/DBS/DBS3/Server/Python/src/dbs/dao/Oracle
-done
-
