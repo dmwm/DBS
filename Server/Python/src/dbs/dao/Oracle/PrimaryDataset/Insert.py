bf50182c88ed3297c8b1459d77116ad448988599
akhukhun <akhukhun>
2010-09-15 17:23:01 +0000

    adding schemaowner
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9042 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/PrimaryDataset/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/PrimaryDataset/Insert.py
@@ -2,29 +2,28 @@
 """
 This module provides PrimaryDataset.Insert data access object.
 """
-__revision__ = "$Id: Insert.py,v 1.2 2009/10/15 15:32:37 akhukhun Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: Insert.py,v 1.3 2009/10/17 17:57:50 akhukhun Exp $"
+__version__ = "$Revision: 1.3 $"
 
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Insert(DBFormatter):
-    """
-    PrimaryDataset Insert DAO Class.
-    """
+    """ PrimaryDataset Insert DAO Class."""
 
-    sql = \
+    def __init__(self, logger, dbi):
+        DBFormatter.__init__(self, logger, dbi)
+        self.owner = "%s." % self.dbi.engine.url.username
+        self.sql = \
 """
-INSERT INTO PRIMARY_DATASETS
+INSERT INTO %sPRIMARY_DATASETS
 (PRIMARY_DS_ID, PRIMARY_DS_NAME, PRIMARY_DS_TYPE_ID, CREATION_DATE, CREATE_BY) 
 VALUES(:primarydsid, :primarydsname, :primarydstypeid, :creationdate, :createby)
-"""
+""" % (self.owner)
 
-    def execute(self, primdsobject, conn=None, transaction=False):
+    def execute(self, inputdict, conn=None, transaction=False):
         """
-        primdsobject dictionary has to have the following keys:
+        inputdict must be validated to have the following keys:
         primarydsid, primarydsname, primarydstypeid, creationdate, createby
         """
-        binds = primdsobject
-        self.dbi.processData(self.sql, binds, conn, transaction)
-            
+        self.dbi.processData(self.sql, inputdict, conn, transaction)
