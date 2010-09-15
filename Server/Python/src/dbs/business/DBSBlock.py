5c3a28f872bb413126aa3b0cd8dad80ccd806a96
akhukhun <akhukhun>
2010-09-15 17:51:33 +0000

    add listBlocks
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9084 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/business/DBSBlock.py
+++ Server/Python/src/dbs/business/DBSBlock.py
@@ -3,8 +3,8 @@
 This module provides business object class to interact with Block. 
 """
 
-__revision__ = "$Id: DBSBlock.py,v 1.2 2009/10/28 09:53:24 akhukhun Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: DBSBlock.py,v 1.3 2009/10/30 16:41:24 akhukhun Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.DAOFactory import DAOFactory
 
@@ -17,39 +17,33 @@ class DBSBlock:
         self.logger = logger
         self.dbi = dbi
 
-    def listBlocks(self, primdsname=""):
+    def listBlocks(self, dataset, block = ""):
         """
-        Returns all primary datasets if primdsname is not passed.
+        Returns all blocks in a dataset if block pattern ("can include %") 
+        is not provided.
         """
-        primdslist = self.daofactory(classname="Block.List")
-        result = primdslist.execute(primdsname)
+        dao = self.daofactory(classname="Block.List")
+        result = dao.execute(dataset, block)
         return result
 
     def insertBlock(self, businput):
         """
         Input dictionary has to have the following keys:
-        blockname, dataset, openforwriting, sitename, blocksize,
+        blockname, dataset(id), openforwriting, originsite(name), blocksize,
         filecount, creationdate, createby, lastmodificationdate, lastmodifiedby
         it builds the correct dictionary for dao input and executes the dao
         """
-        datasetgetid = self.daofactory(classname="Dataset.GetID")
-        sitegetid = self.daofactory(classname="Site.GetID")
-        blockinsert = self.daofactory(classname="Block.Insert")
-        seqmanager = self.daofactory(classname="SequenceManager")
+        datasetgetid = self.daofactory(classname = "Dataset.GetID")
+        sitegetid = self.daofactory(classname = "Site.GetID")
+        blockinsert = self.daofactory(classname = "Block.Insert")
+        seqmanager = self.daofactory(classname = "SequenceManager")
 
-        dataset = businput["dataset"]
-        businput.pop("dataset")
-        sitename = businput["sitename"]
-        businput.pop("sitename")
         conn = self.dbi.connection()
         tran = conn.begin()
         try:
-            datasetid = datasetgetid.execute(dataset, conn, True)
-            siteid = sitegetid.execute(sitename, conn, True)
-            blockid = seqmanager.increment("SEQ_BK", conn, True)
-            businput.update({"datasetid":datasetid,
-                             "originsite":siteid,
-                             "blockid":blockid})
+            businput["dataset"] = datasetgetid.execute(businput["dataset"], conn, True)
+            businput["originsite"] = sitegetid.execute(businput["originsite"], conn, True)
+            businput["blockid"] =  seqmanager.increment("SEQ_BK", conn, True)
             blockinsert.execute(businput, conn, True)
             tran.commit()
         except Exception, e:
