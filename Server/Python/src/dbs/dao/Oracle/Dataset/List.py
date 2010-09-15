7d485007de6ea6e326f9c6dd419f4ddce1e97021
akhukhun <akhukhun>
2010-09-15 19:22:06 +0000

    Add more input and output parameters.
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9235 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Dataset/List.py
+++ Server/Python/src/dbs/dao/Oracle/Dataset/List.py
@@ -2,8 +2,8 @@
 """
 This module provides Dataset.List data access object.
 """
-__revision__ = "$Id: List.py,v 1.15 2009/12/27 13:40:31 akhukhun Exp $"
-__version__ = "$Revision: 1.15 $"
+__revision__ = "$Id: List.py,v 1.16 2010/01/01 18:56:21 akhukhun Exp $"
+__version__ = "$Revision: 1.16 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -14,6 +14,9 @@ class List(DBFormatter):
     def __init__(self, logger, dbi, owner=""):
         """
         Add schema owner and sql.
+        For now IS_DATASET_VALID = 1 and
+        DP.DATASET_TYPE <> 'DELETED' are hardcoded in self.sql.
+        We might need to pass these parameters from outside later
         """
         DBFormatter.__init__(self, logger, dbi)
         self.owner = ("", "%s." % owner)[bool(owner)]
@@ -22,17 +25,29 @@ class List(DBFormatter):
 SELECT D.DATASET_ID, D.DATASET, D.IS_DATASET_VALID, 
         D.XTCROSSSECTION, D.GLOBAL_TAG, 
         D.CREATION_DATE, D.CREATE_BY, 
-        D.LAST_MODIFICATION_DATE,
-        D.DATASET_TYPE_ID, D.PHYSICS_GROUP_ID,
-        DP.DATASET_TYPE, PH.PHYSICS_GROUP_NAME
-FROM %sDATASETS D 
-LEFT OUTER JOIN %sPHYSICS_GROUPS PH ON PH.PHYSICS_GROUP_ID = D.PHYSICS_GROUP_ID
+        D.LAST_MODIFICATION_DATE, D.LAST_MODIFIED_BY,
+        P.PRIMARY_DS_NAME,
+        PD.PROCESSED_DS_NAME,
+        DT.DATA_TIER_NAME,
+        DP.DATASET_TYPE,
+        AE.ACQUISITION_ERA_NAME,
+        PE.PROCESSING_VERSION,
+        PH.PHYSICS_GROUP_NAME
+FROM %sDATASETS D
+JOIN %sPRIMARY_DATASETS P ON P.PRIMARY_DS_ID = D.PRIMARY_DS_ID
+JOIN %sPROCESSED_DATASETS PD ON PD.PROCESSED_DS_ID = D.PROCESSED_DS_ID
+JOIN %sDATA_TIERS DT ON DT.DATA_TIER_ID = D.DATA_TIER_ID
 JOIN %sDATASET_TYPES DP on DP.DATASET_TYPE_ID = D.DATASET_TYPE_ID
-""" % ((self.owner,)*3)
+LEFT OUTER JOIN %sACQUISITION_ERAS AE ON AE.ACQUISITION_ERA_ID = D.ACQUISITION_ERA_ID
+LEFT OUTER JOIN %sPROCESSING_ERAS PE ON PE.PROCESSING_ERA_ID = D.PROCESSING_ERA_ID
+LEFT OUTER JOIN %sPHYSICS_GROUPS PH ON PH.PHYSICS_GROUP_ID = D.PHYSICS_GROUP_ID
+WHERE D.IS_DATASET_VALID = 1
+AND DP.DATASET_TYPE <> 'DELETED'
+""" % ((self.owner,)*8)
 
     def execute(self, dataset="", conn=None):
         """
-        dataset key must be of /a/b/c pattern
+        dataset is a wild card parameter and can include % character
         """	
         if not conn:
             conn = self.dbi.connection()
@@ -44,7 +59,7 @@ JOIN %sDATASET_TYPES DP on DP.DATASET_TYPE_ID = D.DATASET_TYPE_ID
             cursor.execute(sql)
         else:
             op = ("=", "like")["%" in dataset]
-            sql += " WHERE D.DATASET %s :dataset" % op 
+            sql += " AND D.DATASET %s :dataset" % op 
             binds = {"dataset":dataset}
             cursor.execute(sql, binds)
             
