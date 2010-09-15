2a26b952cd76d7fd2f3a460a8073ac932cfefd18
yuyi <yuyi>
2010-09-15 18:55:29 +0000

    update the input data
    From: yuyi <yuyi>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9187 4525493e-7705-40b1-a816-d608a930855b

--- Client/utils/insertprimary.py
+++ Client/utils/insertprimary.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python
 #DBS-3 imports
 from dbs.apis.dbsClient import *
 #url="http://cmssrv48.fnal.gov:8989/DBSServlet"
-#url="http://cmssrv18.fnal.gov:8585/dbs3"
-url="http://vocms09.cern.ch:8989/DBSServlet"
+url="http://cmssrv48.fnal.gov:8989/dbs3"
+#url="http://vocms09.cern.ch:8989/DBSServlet"
 #url="http://vocms09.cern.ch:8585/dbs3"
 # API Object    
 dbs3api = DbsApi(url=url)
 #print dbs3api.listPrimaryDatasets()
-print dbs3api.insertPrimaryDataset({"PRIMARY_DS_TYPE": "test", "PRIMARY_DS_NAME": "TkCosmics38T_ANZAR00r65"})
+print dbs3api.insertPrimaryDataset({"primary_ds_type": "test", "primary_ds_name": "TkCosmics38T_ANZAR00r65"})
 # Is service Alive
 """
 print dbs3api.ping()
