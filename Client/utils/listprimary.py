10c99cd28db3d21d39446e887a5a832eaefd78f5
afaq <afaq>
2010-09-15 23:58:44 +0000

    added insertFiles API calls and related changes gto tests etc
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9534 4525493e-7705-40b1-a816-d608a930855b

--- Client/utils/listprimary.py
+++ Client/utils/listprimary.py
@@ -5,7 +5,9 @@ url="http://cmssrv48.fnal.gov:8989/DBSServlet"
 dbs3api = DbsApi(url=url)
 # Is service Alive
 print dbs3api.ping()
+"""
 print dbs3api.listPrimaryDatasets()
 print dbs3api.listPrimaryDataset("Wmunu_Wplus-powheg")
 print dbs3api.listPrimaryDataset("ANZAR003")
-#print dbs3api.listPrimaryDataset("ANZAR%")
+"""
+print dbs3api.listPrimaryDataset("ANZAR004")
