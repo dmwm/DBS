f8de29b4820924deddaf5a2c6e8780e96ea1f64f
akhukhun <akhukhun>
2010-09-15 17:53:02 +0000

    initial check-in
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9086 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/Oracle/Block/List.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+"""
+This module provides Block.List data access object.
+"""
+__revision__ = "$Id: List.py,v 1.1 2009/10/30 16:45:10 akhukhun Exp $"
+__version__ = "$Revision: 1.1 $"
+
+def op(pattern):
+    """ returns 'like' if pattern includes '%' and '=' otherwise"""
+    if pattern.find("%") == -1:
+        return '='
+    else:
+        return 'like'
+
+from WMCore.Database.DBFormatter import DBFormatter
+class List(DBFormatter):
+    """
+    Block List DAO class.
+    """
+    def __init__(self, logger, dbi):
+        """
+        Add schema owner and sql.
+        """
+        DBFormatter.__init__(self, logger, dbi)
+        self.owner = "%s." % self.dbi.engine.url.username
+        self.sql = \
+"""
+SELECT B.BLOCK_ID, B.BLOCK_NAME, B.OPEN_FOR_WRITING, 
+        B.BLOCK_SIZE, B.FILE_COUNT,
+        B.DATASET_ID, DS.DATASET,
+        B.ORIGIN_SITE, SI.SITE_NAME
+FROM %sBLOCKS B
+JOIN %sDATASETS DS ON DS.DATASET_ID = B.DATASET_ID
+LEFT OUTER JOIN %sSITES SI ON SI.SITE_ID = B.ORIGIN_SITE
+""" % (self.owner, self.owner, self.owner)
+	
+    
+    def execute(self, dataset, block = "",  \
+                conn = None, transaction = False):
+        """
+        dataset: /a/b/c
+        block: /a/b/c#d
+        """	
+        sql = self.sql
+        binds = {}
+        
+        sql += "WHERE DS.DATASET = :datasetname"
+        binds.update({"datasetname":dataset})
+        
+        if not block == "":
+            sql += " AND B.BLOCK_NAME %s :blockname" % op(block)
+            binds.update({"blockname":block})
+        
+        result = self.dbi.processData(sql, binds, conn, transaction)
+        ldict = self.formatDict(result)
+        
+        output = []
+        
+        for idict in ldict:
+            datasetdo = {"dataset_id":idict["dataset_id"],
+                                "dataset":idict["dataset"]}
+            originsitedo = {"site_id":idict["origin_site"],
+                                  "site_name":idict["site_name"]}
+            
+            idict.update({"dataset_do":datasetdo, 
+                          "originsitedo":originsitedo})
+            
+            idict.pop("origin_site") 
+            idict.pop("site_name") 
+            idict.pop("dataset_id") 
+            idict.pop("dataset") 
+            output.append(idict)
+           
+        return output 
+
