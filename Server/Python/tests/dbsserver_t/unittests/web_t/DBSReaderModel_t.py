669ddbbe95c7f008def011ea1088c16561fd6163
yuyi <yuyi>
2010-09-15 20:07:47 +0000

    change the setup
    From: yuyi <yuyi>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9322 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/tests/dbsserver_t/unittests/web_t/DBSReaderModel_t.py
+++ Server/Python/tests/dbsserver_t/unittests/web_t/DBSReaderModel_t.py
@@ -2,13 +2,26 @@
 web unittests
 """
 
-__revision__ = "$Id: DBSReaderModel_t.py,v 1.6 2010/01/26 16:27:29 yuyi Exp $"
-__version__ = "$Revision: 1.6 $"
+__revision__ = "$Id: DBSReaderModel_t.py,v 1.7 2010/01/26 17:54:17 yuyi Exp $"
+__version__ = "$Revision: 1.7 $"
 
-import os
+import os, sys, imp
 import json
 import unittest
 from dbsserver_t.utils.DBSRestApi import DBSRestApi
+
+def importCode(code,name,add_to_sys_modules=0):
+    module = imp.new_module(name)
+    exec code in module.__dict__
+    if add_to_sys_modules:
+        sys.modules[name] = module
+    return module
+
+infofile=open("info.dict","r")    
+testparams=importCode(infofile, "testparams", 0).info
+config = os.environ["DBS_TEST_CONFIG_READER"]
+api = DBSRestApi(config)
+
 
 class DBSReaderModel_t(unittest.TestCase):
     
@@ -17,49 +30,46 @@ class DBSReaderModel_t(unittest.TestCase):
         """setup all necessary parameters"""
 	#import pdb
 	#pdb.set_trace()
-		
-        config = os.environ["DBS_TEST_CONFIG_READER"] 
-        self.api = DBSRestApi(config) 
 
     def test01(self):
         """Test01 web.DBSReaderModel.listPrimaryDatasets: basic test"""
-        self.api.list('primarydatasets')
+        api.list('primarydatasets')
        
     def test02(self):
 	"""Test02 web.DBSReaderModel.listPrimaryDatasets: basic test"""
-	self.api.list('primarydatasets', primary_ds_name='*')
+	api.list('primarydatasets', primary_ds_name='*')
        
     def test03(self):
         """Test03 web.DBSReaderModel.listDatasets: basic test"""
-        self.api.list('datasets')
+        api.list('datasets')
     
     def test04(self):
         """Test04 web.DBSReaderModel.listDatasets: basic test"""
-        self.api.list('datasets', dataset='*')
+        api.list('datasets', dataset='*')
 
     def test05(self):
         """Test05 web.DBSReaderModel.listDatasets: basic test"""
-        self.api.list('datasets', parent_dataset='*')
+        api.list('datasets', parent_dataset='*')
     
     def test06(self):
         """Test06 web.DBSReaderModel.listDatasets: basic test"""
-        self.api.list('datasets', release_version='*')
+        api.list('datasets', release_version='*')
 
     def test07(self):
         """Test07 web.DBSReaderModel.listDatasets: basic test"""
-        self.api.list('datasets', pset_hash='*')
+        api.list('datasets', pset_hash='*')
 
     def test08(self):
         """Test08 web.DBSReaderModel.listDatasets: basic test"""
-        self.api.list('datasets', app_name='*')
+        api.list('datasets', app_name='*')
     
     def test09(self):
         """Test09 web.DBSReaderModel.listDatasets: basic test"""
-	self.api.list('datasets', output_module_label='*')
+	api.list('datasets', output_module_label='*')
 
     def test10(self):
         """Test10 web.DBSReaderModel.listDatasets: basic test"""
-	self.api.list('datasets', dataset='*', 
+	api.list('datasets', dataset='*', 
                                   parent_dataset='*',
                                   release_version='*',
                                   pset_hash='*',
@@ -68,7 +78,7 @@ class DBSReaderModel_t(unittest.TestCase):
     def test11(self):
         """Test11 web.DBSReaderModel.listBlocks: basic test"""
 	try:
-	    self.api.list('blocks', dataset='*')
+	    api.list('blocks', dataset='*')
         except:
 	    pass
 	else:
@@ -77,7 +87,7 @@ class DBSReaderModel_t(unittest.TestCase):
     def test12(self):
         """Test12 web.DBSReaderModel.listBlocks: basic test"""
         try:
-            self.api.list('blocks', block_name='*')
+            api.list('blocks', block_name='*')
         except:
             pass
         else:
@@ -86,7 +96,7 @@ class DBSReaderModel_t(unittest.TestCase):
     def test13(self):
         """Test13 web.DBSReaderModel.listBlocks: basic test"""
         try:
-            self.api.list('blocks', site_name='*')
+            api.list('blocks', site_name='*')
         except:
             pass
         else:
@@ -95,7 +105,7 @@ class DBSReaderModel_t(unittest.TestCase):
     def test14(self):
         """Test14 web.DBSReaderModel.listBlocks: basic test"""
         try:
-            self.api.list('blocks', dataset='*',
+            api.list('blocks', dataset='*',
                                 block_name='*',
                                 site_name='*')
         except:
@@ -108,7 +118,7 @@ class DBSReaderModel_t(unittest.TestCase):
     def test15(self):
         """Test15 web.DBSReaderModel.listBlocks: takes exact dataset name, not pattern"""
 	try:
-	    result=self.api.list('blocks', dataset='*')
+	    result=api.list('blocks', dataset='*')
 	    #import pdb
 	    #pdb.set_trace()
 	except:
@@ -120,7 +130,7 @@ class DBSReaderModel_t(unittest.TestCase):
         """Test16 web.DBSReaderModel.listBlocks: Must raise an exception if no parameter is passed."""
 	
         try:
-	    self.api.list('blocks')
+	    api.list('blocks')
         except: 
 	    pass
         else: 
@@ -129,7 +139,7 @@ class DBSReaderModel_t(unittest.TestCase):
     def test17(self):
         """Test17 web.DBSReaderModel.listFiles: basic test"""
 	try:
-	    self.api.list('files', dataset='*')
+	    api.list('files', dataset='*')
 	except:
 	    pass
 	else:
@@ -138,7 +148,7 @@ class DBSReaderModel_t(unittest.TestCase):
     def test18(self):
         """Test18 web.DBSReaderModel.listFiles: basic test"""
 	try:
-	    self.api.list('files', block_name='*')
+	    api.list('files', block_name='*')
 	except:
             pass
         else:
@@ -148,7 +158,7 @@ class DBSReaderModel_t(unittest.TestCase):
     def test19(self):
         """Test19 web.DBSReaderModel.listFiles: basic test"""
 	try:
-	    self.api.list('files', logical_file_name='*')
+	    api.list('files', logical_file_name='*')
 	except:
             pass
         else:
@@ -156,17 +166,17 @@ class DBSReaderModel_t(unittest.TestCase):
        
     def test21(self):
         """Test21 web.DBSReaderModel.listFiles: Must raise an exception if no parameter is passed."""
-        try: self.api.list('files')
+        try: api.list('files')
         except: pass
         else: self.fail("Exception was expected and was not raised")
             
     def test22(self):
         """Test22 web.DBSReaderModel.listDatasetParents: basic test"""
-        self.api.list('datasetparents', dataset="*")
+        api.list('datasetparents', dataset="*")
         
     def test23(self):
         """Test23 web.DBSReaderModel.listDatasetParents: must raise an exception if no parameter is passed"""
-        try: self.api.list('datasetparents')
+        try: api.list('datasetparents')
         except: 
 	    pass
         else: 
@@ -174,35 +184,35 @@ class DBSReaderModel_t(unittest.TestCase):
             
     def test24(self):
         """Test24 web.DBSReaderModel.listOutputConfigs: basic test"""
-	self.api.list('outputconfigurations')
+	api.list('outputconfigurations')
     
     def test25(self):
         """Test25 web.DBSReaderModel.listOutputConfigs: basic test"""
-	self.api.list('outputconfigurations', dataset="*")
+	api.list('outputconfigurations', dataset="*")
 	
     def test26(self):
         """Test26 web.DBSReaderModel.listOutputConfigs: basic test"""
-        self.api.list('outputconfigurations', logical_file_name="*")
+        api.list('outputconfigurations', logical_file_name="*")
 
     def test27(self):
         """Test27 web.DBSReaderModel.listOutputConfigs: basic test"""
-        self.api.list('outputconfigurations', release_version="*")
+        api.list('outputconfigurations', release_version="*")
 
     def test28(self):
         """Test28 web.DBSReaderModel.listOutputConfigs: basic test"""
-        self.api.list('outputconfigurations', pset_hash="*")
+        api.list('outputconfigurations', pset_hash="*")
 
     def test29(self):
 	"""Test29 web.DBSReaderModel.listOutputConfigs: basic test"""
-	self.api.list('outputconfigurations', app_name="*")
+	api.list('outputconfigurations', app_name="*")
 
     def test30(self):
         """Test30 web.DBSReaderModel.listOutputConfigs: basic test"""
-	self.api.list('outputconfigurations', output_module_label="*")
+	api.list('outputconfigurations', output_module_label="*")
  
     def test31(self):
         """Test31 web.DBSReaderModel.listOutputConfigs: basic test"""
-	self.api.list('outputconfigurations', dataset="*",
+	api.list('outputconfigurations', dataset="*",
                                               logical_file_name="*",
                                               release_version="*",
                                               pset_hash="*",
@@ -211,25 +221,25 @@ class DBSReaderModel_t(unittest.TestCase):
 
     def test32(self):
         """Test32 web.DBSReaderModel.listFileParents: basic test"""
-        self.api.list('fileparents', logical_file_name="*")
+        api.list('fileparents', logical_file_name="*")
     
     def test33(self):
         """Test33 web.DBSReaderModel.listFileParents: must raise an exception if no parameter is passed"""
-        try: self.api.list('fileparents')
+        try: api.list('fileparents')
         except: pass
         else: self.fail("Exception was expected and was not raised")
         
     def test34(self):
         """Test34 web.DBSReaderModel.listFileLumis: basic test"""
-        self.api.list('filelumis', logical_file_name="*")
+        api.list('filelumis', logical_file_name="*")
 
     def test35(self):
         """Test35 web.DBSReaderModel.listFileLumis: basic test"""
-        self.api.list('filelumis', block_name="*")
+        api.list('filelumis', block_name="*")
 
     def test36(self):
         """Test36 web.DBSReaderModel.listFileLumis: must raise an exception if no parameter is passed"""
-        try: self.api.list('filelumis')
+        try: api.list('filelumis')
         except: pass
         else: self.fail("Exception was expected and was not raised")
         
