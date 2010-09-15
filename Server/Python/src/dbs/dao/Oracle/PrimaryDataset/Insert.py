8aafa367d967f56307337b8d4498c85ae8457f9d
yuyi <yuyi>
2010-09-15 19:08:47 +0000

    using general InsertSingle class
    From: yuyi <yuyi>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9211 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/PrimaryDataset/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/PrimaryDataset/Insert.py
@@ -1,34 +1,17 @@
 #!/usr/bin/env python
 """ DAO Object for PrimaryDatasets table """ 
 
-__revision__ = "$Revision: 1.9 $"
-__version__  = "$Id: Insert.py,v 1.9 2009/12/17 17:51:28 yuyi Exp $ "
+__revision__ = "$Revision: 1.10 $"
+__version__  = "$Id: Insert.py,v 1.10 2009/12/23 15:42:05 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 from sqlalchemy import exceptions
+from dbs.dao.Oracle.InsertTable.Insert import InsertSingle
 
-class Insert(DBFormatter):
+class Insert(InsertSingle):
     """ PrimaryDataset Insert DAO Class"""
-
-    def __init__(self, logger, dbi, owner):
-        DBFormatter.__init__(self, logger, dbi)
-        self.owner = "%s." % owner
-        self.logger = logger
-        self.sql = \
-"""
-INSERT INTO %sPRIMARY_DATASETS 
-(PRIMARY_DS_ID, PRIMARY_DS_NAME, PRIMARY_DS_TYPE_ID, CREATION_DATE, CREATE_BY) 
-VALUES(:primary_ds_id, :primary_ds_name, :primary_ds_type, :creation_date, :create_by)
-""" % self.owner
-
     def execute(self, daoinput, conn = None, transaction = False):
-        """
-        inputdict must be validated to have the following keys:
-        primaryi_ds_id, primary_ds_name, primary_ds_type(id), creation_date, create_by
-        """
-        
-        try:
-            self.dbi.processData(self.sql, daoinput, conn, transaction)
-        except exceptions.IntegrityError, ex:
-            self.logger.warning("Unique constraint violation being ignored...")
-            self.logger.warning("%s" % ex)
+	try:
+	    self.executeSingle(daoinput, "PRIMARY_DATASETS", conn = None, transaction = False)
+	except Exception:
+	    raise
