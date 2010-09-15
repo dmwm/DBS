e9629ba1d2c7878bdba7ea4f5b890ea68fe6904a
akhukhun <akhukhun>
2010-09-15 17:56:31 +0000

    used by block insert
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9091 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Site/GetID.py
+++ Server/Python/src/dbs/dao/Oracle/Site/GetID.py
@@ -2,8 +2,8 @@
 """
 This module provides Site.GetID data access object.
 """
-__revision__ = "$Id: GetID.py,v 1.1 2009/10/28 09:51:14 akhukhun Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: GetID.py,v 1.2 2009/10/30 16:52:49 akhukhun Exp $"
+__version__ = "$Revision: 1.2 $"
 
 
 from WMCore.Database.DBFormatter import DBFormatter
@@ -23,15 +23,15 @@ SELECT S.SITE_ID, S.SITE_NAME
 FROM %sSITES S 
 """ % (self.owner)
 
-    def execute(self, sitename = "", conn = None, transaction = False):
+    def execute(self, name, conn = None, transaction = False):
         """
         returns id for a give site
         """
         sql = self.sql
         sql += "WHERE S.SITE_NAME = :sitename" 
-        binds = {"sitename":sitename}
+        binds = {"sitename":name}
         result = self.dbi.processData(sql, binds, conn, transaction)
         plist = self.formatDict(result)
         assert len(plist) == 1, \
-            "Site %s does not exist." % sitename
+            "Site %s does not exist." % name
         return plist[0]["site_id"]
