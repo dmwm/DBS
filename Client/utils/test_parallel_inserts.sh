be241c2a2791bc8f2d29623458890aa55e757460
yuyi <yuyi>
2010-09-15 18:43:12 +0000

    put log files in logs dir
    From: yuyi <yuyi>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9163 4525493e-7705-40b1-a816-d608a930855b

--- Client/utils/test_parallel_inserts.sh
+++ Client/utils/test_parallel_inserts.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /GlobalAug07-C/Online/RAW > log1 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /GlobalAug07-B/Online/RAW  > log2 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /GlobalAug07-A/Online/RAW  > log3 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /GlobalNov07-Express/Online/RAW  > log4 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /Monitor/Commissioning08-v1/RAW  > log5 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /TestBeam2007-combined/Online/RAW  > log6 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /Test/Commissioning09-Express-v1/RAW  > log7 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /DaqTest-default/Online/RAW  > log8 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /TestBeam2007Ecal-A/Online/RAW  > log9 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /GlobalNov07-A/Online/RAW  > log10 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /DaqTest-C/Online/RAW  > log11 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /DaqTest-B/Online/RAW  > log12 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /MuonPrivateGlobal-A/Online/RAW  > log13 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /MuonPrivateGlobal-Express/Online/RAW  > log14 2>&1 &
-python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /DaqTest-A/Online/RAW  > log15 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /DaqTest-default/Online/RAW  > logs/log8J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /TestBeam2007Ecal-A/Online/RAW  > logs/log9J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /GlobalNov07-A/Online/RAW  > logs/log10J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /DaqTest-C/Online/RAW  > logs/log11J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /DaqTest-B/Online/RAW  > logs/log12J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /MuonPrivateGlobal-A/Online/RAW  > logs/log13J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /MuonPrivateGlobal-Express/Online/RAW  > logs/log14J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /DaqTest-A/Online/RAW  > logs/log15J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW > logs/log1J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-B/Online/RAW  > logs/log2J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-A/Online/RAW  > logs/log3J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /GlobalNov07-Express/Online/RAW  > logs/log4J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /Monitor/Commissioning08-v1/RAW  > logs/log5J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /TestBeam2007-combined/Online/RAW  > logs/log6J 2>&1 &
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8989/DBSServlet /Test/Commissioning09-Express-v1/RAW  > logs/log7J 2>&1 &
 wait
--- Client/utils/test_parallel_read.sh
+++ Client/utils/test_parallel_read.sh
@@ -1,17 +1,52 @@
 #!/bin/sh
-python listprimary.py > /dev/null 2>&1 &
-python listprimary.py > /dev/null 2>&1 &
-python listprimary.py > /dev/null 2>&1 &
-python listprimary.py > /dev/null 2>&1 &
-python listprimary.py > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
-python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
+python listfiles.py http://vocms09.cern.ch:8989/DBSServlet /GlobalAug07-C/Online/RAW >> logs/log2JR 2>&1 &
 wait
