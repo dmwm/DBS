241a5729d8250b8bf853d1d6092e2f290de467ee
akhukhun <akhukhun>
2010-09-15 23:56:09 +0000

    add list block and insert dataset
    From: akhukhun <akhukhun>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9518 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/tests/TestBusiness.py
+++ Server/Python/tests/TestBusiness.py
@@ -1,18 +1,20 @@
 """This module provides all bascs tests for the business objects."""
 
-__revision__ = "$Id: TestBusiness.py,v 1.2 2009/10/28 09:54:18 akhukhun Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: TestBusiness.py,v 1.3 2009/10/30 16:56:01 akhukhun Exp $"
+__version__ = "$Revision: 1.3 $"
 
 import unittest
 import logging
 from WMCore.Database.DBFactory import DBFactory
 
+INSERTCOUNT = "2023"
 
 class TestBusiness(unittest.TestCase):
     """Business layer unittests class"""
     def setUp(self):
         """setup all necessary parameters"""
-        url = "oracle://cms_dbs_afaq:anzpw03062009@oradev10.cern.ch:10520/D10"
+        #url = "oracle://cms_dbs_afaq:anzpw03062009@oradev10.cern.ch:10520/D10"
+        url = "oracle://CMS_DBS3_OWNER:new4_dbs3@uscmsdb03.fnal.gov:1521/cmscald"
         self.logger = logging.getLogger("dbs test logger")
         self.dbi = DBFactory(self.logger, url).connect()
         
@@ -20,7 +22,7 @@ class TestBusiness(unittest.TestCase):
         """DBSPrimaryDataset.insertPrimaryDataset"""
         from dbs.business.DBSPrimaryDataset import DBSPrimaryDataset
         bo = DBSPrimaryDataset(self.logger, self.dbi)
-        binput = {"primarydsname":"QCD_100_509",
+        binput = {"primarydsname":"QCD_700_" + INSERTCOUNT,
                  "primarydstype":"mc",
                  "creationdate":1234,
                  "createby":"akhukhun@cern.ch"}
@@ -33,6 +35,27 @@ class TestBusiness(unittest.TestCase):
         bo.listPrimaryDatasets()
         result = bo.listPrimaryDatasets("ThisDoesNotExist")
         self.assertEqual(len(result), 0)
+        
+    def test03(self):
+        """DBSDataset.Insert"""
+        from dbs.business.DBSDataset import DBSDataset
+        bo = DBSDataset(self.logger, self.dbi)
+        binput = {"dataset":"/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v%s/GEN-SIM-RECO" % INSERTCOUNT,
+                  "isdatasetvalid":1,
+                  "primaryds":"RelValQCD_Pt_80_120",
+                  "processedds":"CMSSW_3_1_3-MC_31X_V5-v%s" % INSERTCOUNT,
+                  "datatier":"GEN-SIM-RECO",
+                  "datasettype":"PRODUCTION",
+                  "acquisitionera":"TODAY",
+                  "processingversion":"TEST",
+                  "physicsgroup":"Individual",
+                  "xtcrosssection":999.,
+                  "globaltag":"DaoTESTGlobalTag",
+                  "creationdate":1234,
+                  "createby":"me",
+                  "lastmodificationdate":1235,
+                  "lastmodifiedby":"alsome"}                  
+        bo.insertDataset(binput)
     
     def test04(self):
         """DBSDataset.listDatasets"""
@@ -48,10 +71,10 @@ class TestBusiness(unittest.TestCase):
         """DBSBLock.Insert"""
         from dbs.business.DBSBlock import DBSBlock
         bo = DBSBlock(self.logger, self.dbi)
-        binput = {"blockname":"/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v1/GEN-SIM-RECO#1234567",
+        binput = {"blockname":"/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v1/GEN-SIM-RECO#" + INSERTCOUNT,
                   "dataset":"/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v1/GEN-SIM-RECO",
-                  "openforwriting":1,
-                  "sitename":"alekossite",
+                  "openforwriting":True,
+                  "originsite":"FNAL",
                   "blocksize":9999,
                   "filecount":1000,
                   "creationdate":123,
@@ -61,13 +84,18 @@ class TestBusiness(unittest.TestCase):
         
         bo.insertBlock(binput)
         
+        
+    def test06(self):
+        """DBSBlock.List"""
+        from dbs.business.DBSBlock import DBSBlock
+        bo = DBSBlock(self.logger, self.dbi)
+        print bo.listBlocks("/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v1/GEN-SIM-RECO")
+        print bo.listBlocks("/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v1/GEN-SIM-RECO",
+                            "/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v1/GEN-SIM-RECO#%20%")
+        
+        
 if __name__ == "__main__":
     
-    #unittest.main()
-
-    SUITE = unittest.TestSuite()
-    SUITE.addTest(TestBusiness("test01"))
-    SUITE.addTest(TestBusiness("test02"))
-    SUITE.addTest(TestBusiness("test04"))
-    SUITE.addTest(TestBusiness("test05"))
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(TestBusiness)
     unittest.TextTestRunner(verbosity=2).run(SUITE)
+
--- Server/Python/tests/TestServer.py
+++ Server/Python/tests/TestServer.py
@@ -1,9 +1,9 @@
 """This module provides all basic tests for the server"""
 
-__revision__ = "$Id: TestServer.py,v 1.2 2009/10/28 15:11:13 akhukhun Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: TestServer.py,v 1.3 2009/10/30 16:56:45 akhukhun Exp $"
+__version__ = "$Revision: 1.3 $"
 
-INSERTCOUNT = "08"
+INSERTCOUNT = "24"
 
 import json
 import unittest
@@ -20,7 +20,7 @@ class TestServer(unittest.TestCase):
         """listPrimaryDatasets"""
         self.cli.get("primds")
         self.cli.get("primds/*")
-        self.cli.get("primds?primdsname=*bla*")
+        self.cli.get("primds?primds=*bla*")
     
     def test02(self):
         """listDatasets"""
@@ -28,7 +28,7 @@ class TestServer(unittest.TestCase):
         self.cli.get("datasets/*")
         self.cli.get("datasets/a/b")
         self.cli.get("datasets/a/*/c")
-        self.cli.get("datasets?primdsname=a&datatiername=*")
+        self.cli.get("datasets?primds=a&tier=*")
         
     def test03(self):
         """insertPrimaryDataset"""
@@ -39,21 +39,33 @@ class TestServer(unittest.TestCase):
     def test04(self):
         """insertBlock"""
         dinput = {"blockname":"/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v1/GEN-SIM-RECO#inertedbyserverunittest_" + INSERTCOUNT,
-                  "openforwriting":1,
-                  "sitename":"alekossite",
+                  "openforwriting":True,
+                  "originsite":"FNAL",
                   "blocksize":345,
                   "filecount":20
                   }
-        dinput = self.cli.put("blocks", dinput)
+        self.cli.put("blocks", dinput)
+
+        
+    def test05(self):
+        """insertDataset"""
+        dinput = {"dataset":"/RelValQCD_Pt_80_120/CMSSW_3_1_3-MC_31X_V5-v%s/GEN-SIM-RECO" % INSERTCOUNT,
+                  "isdatasetvalid":True,
+                  "datasettype":"PRODUCTION",
+                  "acquisitionera":"TODAY",
+                  "processingversion":"TEST",
+                  "physicsgroup":"Individual",
+                  "xtcrosssection":234.,
+                  "globaltag":"GLOBALTAG"}
+        self.cli.put("datasets", dinput)
+    
+    def test06(self):
+        "listBlocks"
+        self.cli.get("blocks/a/b/c/d")
+
+
 
 if __name__ == "__main__":
-    """    
-    SUITE = unittest.TestSuite()
-    SUITE.addTest(TestBusiness("test01"))
-    SUITE.addTest(TestBusiness("test02"))
-    SUITE.addTest(TestBusiness("test03"))
-    SUITE.addTest(TestBusiness("test04"))
-    #unittest.TextTestRunner(verbosity=2).run(SUITE)
-    """
+    
     SUITE = unittest.TestLoader().loadTestsFromTestCase(TestServer)
     unittest.TextTestRunner(verbosity=2).run(SUITE)
