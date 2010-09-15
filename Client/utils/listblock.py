906550a5b661210ab5fd9d09137a67cb37128fe5
afaq <afaq>
2010-09-15 23:57:56 +0000

    Client side changes for REST URI adjustments
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9529 4525493e-7705-40b1-a816-d608a930855b

--- Client/utils/listblock.py
+++ Client/utils/listblock.py
@@ -5,4 +5,5 @@ url="http://cmssrv48.fnal.gov:8989/DBSServlet"
 dbs3api = DbsApi(url=url)
 # Is service Alive
 print dbs3api.ping()
-print dbs3api.listBlock("/TTbar/Summer09-MC_31X_V3-v1/GEN-SIM-RAW#f99b4c09-a68f-4e73-8f4c-560c1fa922fc")
+print dbs3api.listBlock("/TTbar/Summer09-MC_31X_V3-v1/GEN-SIM-RAW#f99b4c09-a68f-4e73-8f4c-560c1fa922fc:xyz=test")
+#print dbs3api.listBlock("/TTbar/Summer09-MC_31X_V3-v1/GEN-SIM-RAW#f99b4c09-a68f-4e73-8f4c-560c1fa922fc:xyz=test")
--- Client/utils/listprimary.py
+++ Client/utils/listprimary.py
@@ -5,5 +5,7 @@ url="http://cmssrv48.fnal.gov:8989/DBSServlet"
 dbs3api = DbsApi(url=url)
 # Is service Alive
 print dbs3api.ping()
+print dbs3api.listPrimaryDatasets()
 print dbs3api.listPrimaryDataset("Wmunu_Wplus-powheg")
 print dbs3api.listPrimaryDataset("ANZAR003")
+#print dbs3api.listPrimaryDataset("ANZAR%")
