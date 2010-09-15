3bd5d42a437a5cdc009e25f967d09ceeb8adf248
afaq <afaq>
2010-09-15 19:17:45 +0000

    Added two more tests
    From: afaq <afaq>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9229 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/tests/SimpleBizlogicTest.py
+++ Server/Python/tests/SimpleBizlogicTest.py
@@ -2,6 +2,8 @@ import logging
 from WMCore.Database.DBFactory import DBFactory
 from dbs.business.DBSOutputConfig import DBSOutputConfig
 from dbs.business.DBSDataset import DBSDataset
+from dbs.business.DBSAcquisitionEra import DBSAcquisitionEra
+from dbs.business.DBSProcessingEra import DBSProcessingEra
 
 class Test:
 
@@ -37,8 +39,28 @@ class Test:
 
         bo.insertDataset(binput)
 
+    def testAcquisitionEra(self):
+	        """
+		This method is being used for testing AcquisitionEra insert biz
+		"""
+		bo = DBSAcquisitionEra(self.logger, self.dbi, "anzar")
+		binput = {
+			    'acquisition_era_name' : 'test_acq_001', 'creation_date' : 1234, 'create_by' : 'anzar'
+			}
+		bo.insertAcquisitionEra(binput)
+
+    def testProcessingEra(self):
+	        """
+		This method is being used for testing AcquisitionEra insert biz
+		"""
+		bo = DBSProcessingEra(self.logger, self.dbi, "anzar")
+		binput = {
+		          'processing_version' : 'v001', 'creation_date' : 1234, 'create_by' : 'anzar', 'description' : 'anzar test description'
+		         }
+		bo.insertProcessingEra(binput)
+
 test=Test()
-#test.testOutputConfig()
+test.testOutputConfig()
 test.testDatasetInsert()
-
-
+test.testAcquisitionEra()
+test.testProcessingEra()
