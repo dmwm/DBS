183ce7df769aa9382c35a1e4beb06e1403c8ff21
afaq <afaq>
2010-09-15 19:38:19 +0000

    sorry for checking in setup file with relative paths to my home area
    From: afaq <afaq>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9267 4525493e-7705-40b1-a816-d608a930855b

--- Client/setup.sh
+++ Client/setup.sh
@@ -1,5 +1,3 @@
-cd /home/anzar/devDBS3/DBS/Clients/Python
-source setup.sh
-cd -
-export PYTHONPATH=$PYTHONPATH:$PWD/src/python
+export cjson=/home/anzar/devDBS3/external/python-cjson-1.0.5/build/lib.linux-i686-2.6/
+export PYTHONPATH=$PYTHONPATH:$PWD/src/python:$cjson
 
