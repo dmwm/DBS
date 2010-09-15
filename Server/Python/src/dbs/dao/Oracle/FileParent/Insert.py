3a7283542b116eb3554d9adeb3823d86436d2cc1
yuyi <yuyi>
2010-09-15 18:24:45 +0000

    Error corrections
    From: yuyi <yuyi>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9137 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/FileParent/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/FileParent/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for FileParents table """ 
 
-__revision__ = "$Revision: 1.3 $"
-__version__  = "$Id: Insert.py,v 1.3 2009/11/16 20:29:06 yuyi Exp $ "
+__revision__ = "$Revision: 1.4 $"
+__version__  = "$Id: Insert.py,v 1.4 2009/11/17 19:44:19 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -13,30 +13,31 @@ class Insert(DBFormatter):
             self.owner = "%s." % self.dbi.engine.url.username
 
             self.sql = """INSERT INTO %sFILE_PARENTS ( FILE_PARENT_ID, THIS_FILE_ID, PARENT_FILE_ID) 
-	     VALUES (:FILE_PARENT_ID, :THIS_FILE_ID, :FILE_PARENT_LFN) % (self.owner) ;"""
+	     VALUES (:FILE_PARENT_ID, :THIS_FILE_ID, : PARENT_FILE_ID) """  % (self.owner)
 
-    def getBinds_delme( self, file_parentsObj ):
+    def getBinds( self, file_parentsObj ):
+	    #note we replaced value in 'FILE_PARENT_LFN' with PARENT_FILE_ID in the DBSFile.py
             binds = {}
-            if type(file_parentsObj) == type ('object'):
+            if type(file_parentsObj) == type ({}):
             	binds = {
-			'fileparentid' : file_parentsObj['fileparentid'],
-			'thisfileid' : file_parentsObj['thisfileid'],
-			'parentfileid' : file_parentsObj['parentfileid'],
+			'FILE_PARENT_ID' : file_parentsObj['FILE_PARENT_ID'],
+			'THIS_FILE_ID' : file_parentsObj['THIS_FILE_ID'],
+			'PARENT_FILE_ID' : file_parentsObj['FILE_PARENT_LFN']
                  }
 
             elif type(file_parentsObj) == type([]):
                binds = []
                for item in file_parentsObj:
                    binds.append({
- 	                'fileparentid' : item['fileparentid'],
- 	                'thisfileid' : item['thisfileid'],
- 	                'parentfileid' : item['parentfileid'],
+			'FILE_PARENT_ID' : item['FILE_PARENT_ID'],
+			'THIS_FILE_ID' : item['THIS_FILE_ID'],
+			'PARENT_FILE_ID' : item['FILE_PARENT_LFN']
  	                })
                return binds
 
 
     def execute( self, file_parentsObj, conn=None, transaction=False ):
-            ##binds = self.getBinds( file_parentsObj )
+            binds = self.getBinds( file_parentsObj )
             result = self.dbi.processData(self.sql, binds, conn, transaction)
             return
 
