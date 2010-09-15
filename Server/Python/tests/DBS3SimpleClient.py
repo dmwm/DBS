039d8394f0b0a03d3adbaeca9a9ac3b890cb4f3a
akhukhun <akhukhun>
2010-09-15 17:49:01 +0000

    modify such that it can be used by the server unittests
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9079 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/tests/DBS3SimpleClient.py
+++ Server/Python/tests/DBS3SimpleClient.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python
 """
 Very simple dbs3 client:
-python dbs3Client.py GET primds 
-python dbs3Client.py GET primds/qcd_20_30 
-python dbs3Client.py GET primds?primdsname=qcd*
-python dbs3Client.py PUT primds '{"primdsname":"QCDTEST_700_800", \ 
-                                  "primdstype":"mc"}" 
 """
 
-__revision__ = "$Id: DBS3SimpleClient.py,v 1.1 2009/10/27 17:28:05 akhukhun Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: DBS3SimpleClient.py,v 1.2 2009/10/28 14:52:48 akhukhun Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import sys
 import json
 import urllib2
 
-def call(url, method = "GET", data = ""):
-    """Very simple client function""" 
-    header = {"Accept": "application/json"}
-    opener = urllib2.build_opener()
-
-    if method == 'GET':
-        req = urllib2.Request(url = url, headers = header)
-    elif method == 'PUT':
+class DBS3Client:
+    def __init__(self, baseurl):
+        self.baseurl = baseurl
+        self.header =  {"Accept": "application/json"}
+        self.opener =  urllib2.build_opener()
+        
+    def get(self, apiurl):
+        "method for GET verb"
+        url = self.baseurl + apiurl
+        req = urllib2.Request(url = url, headers = self.header)
+        data = self.opener.open(req)
+        ddata = json.JSONDecoder().decode(data.read())
+        return json.dumps(ddata, sort_keys = True, indent = 2)
+    
+    def put(self, apiurl, indata):
+        """method for PUT verb"""
+        url = self.baseurl + apiurl
+        header = self.header
         header['Content-Type'] = 'application/json'
-        req = urllib2.Request(url = url, data = data, headers = header)
+        endata = json.dumps(indata)
+        req = urllib2.Request(url = url, data = endata, headers = header)
         req.get_method = lambda: 'PUT'
-    data = opener.open(req)
-    ddata = json.JSONDecoder().decode(data.read())
-    return json.dumps(ddata, sort_keys = True, indent = 2)
+        self.opener.open(req)
 
 
 if __name__ == "__main__":
     URLBASE = "http://localhost:8585/dbs3/"
-    if sys.argv[1] == "GET":
-        print call(URLBASE + sys.argv[2], sys.argv[1])
-    elif sys.argv[1] == "PUT":
-        call(URLBASE + sys.argv[2], sys.argv[1], sys.argv[3])
-    else:
-        print "unknown verb", sys.argv[1]
+    CLI = DBS3Client(URLBASE)
+    print CLI.get(sys.argv[1])
