26f1d034293997379ac74b84611713d9fc0b3a17
yuyi <yuyi>
2010-09-15 19:39:02 +0000

    add uuid
    From: yuyi <yuyi>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9268 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/tests/dbsserver_t/unittests/web_t/DBSWriterModel_t.py
+++ Server/Python/tests/dbsserver_t/unittests/web_t/DBSWriterModel_t.py
@@ -2,17 +2,25 @@
 web unittests
 """
 
-__revision__ = "$Id: DBSWriterModel_t.py,v 1.1 2010/01/01 19:54:38 akhukhun Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: DBSWriterModel_t.py,v 1.2 2010/01/11 18:02:47 yuyi Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import os
 import unittest
 from dbsserver_t.utils.DBSRestApi import DBSRestApi
+from ctypes import *
 
+"""
+This is has to be change everytime running the test. So we need to make it using uuid. YG 1/11/10
 COUNTER = os.environ['DBS_TEST_COUNTER']
-
+"""
 class DBSWriterModel_t(unittest.TestCase):
     
+    def uuid(self):
+	lib = CDLL("libuuid.so.1")
+	uuid = create_string_buffer(16)
+	return lib.uuid_generate(byref(uuid))
+	
     def setUp(self):
         """setup all necessary parameters"""
         config = os.environ["DBS_TEST_CONFIG_WRITER"] 
@@ -20,6 +28,9 @@ class DBSWriterModel_t(unittest.TestCase):
 
     def test01(self):
         """web.DBSReaderModel.insertPrimaryDataset: basic test"""
+	COUNTER = self.uuid()
+	import pdb
+	pdb.set_trace()
         data = {'primary_ds_name':'unittest_web_primary_ds_name_%s' % COUNTER,
                 'primary_ds_type':'TEST'}
         self.api.insert('primarydatasets', data)
