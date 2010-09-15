05d67d4606fadf88fb97c4985626dd532b27a736
akhukhun <akhukhun>
2010-09-15 18:39:02 +0000

    change parameter names to lower case.
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9157 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/business/DBSDataset.py
+++ Server/Python/src/dbs/business/DBSDataset.py
@@ -3,8 +3,8 @@
 This module provides business object class to interact with Dataset. 
 """
 
-__revision__ = "$Id: DBSDataset.py,v 1.9 2009/11/27 09:55:02 akhukhun Exp $"
-__version__ = "$Revision: 1.9 $"
+__revision__ = "$Id: DBSDataset.py,v 1.10 2009/11/30 09:52:31 akhukhun Exp $"
+__version__ = "$Revision: 1.10 $"
 
 from WMCore.DAOFactory import DAOFactory
 
@@ -18,20 +18,20 @@ class DBSDataset:
         """
         initialize business object class.
         """
-        self.daofactory = DAOFactory(package='dbs.dao', logger=logger, dbinterface=dbi, owner=owner)
+        daofactory = DAOFactory(package='dbs.dao', logger=logger, 
+                                dbinterface=dbi, owner=owner)
         self.logger = logger
         self.dbi = dbi
         
-        self.DatasetList = self.daofactory(classname="Dataset.List")
-
-        self.SequenceManager = self.daofactory(classname="SequenceManager")
-        self.PrimaryDatasetGetID = self.daofactory(classname='PrimaryDataset.GetID')
-        self.DataTierGetID = self.daofactory(classname='DataTier.GetID')
-        self.DatasetTypeGetID = self.daofactory(classname='DatasetType.GetID')
-        self.PhysicsGroupGetID = self.daofactory(classname='PhysicsGroup.GetID')
-        self.ProcessedDatasetGetID = self.daofactory(classname='ProcessedDataset.GetID')
-        self.ProcessedDatasetInsert = self.daofactory(classname='ProcessedDataset.Insert')
-        self.DatasetInsert = self.daofactory(classname='Dataset.Insert')
+        self.datasetlist = daofactory(classname="Dataset.List")
+        self.sm = daofactory(classname="SequenceManager")
+        self.primdsid = daofactory(classname='PrimaryDataset.GetID')
+        self.tierid = daofactory(classname='DataTier.GetID')
+        self.datatypeid = daofactory(classname='DatasetType.GetID')
+        self.phygrpid = daofactory(classname='PhysicsGroup.GetID')
+        self.procdsid = daofactory(classname='ProcessedDataset.GetID')
+        self.procdsin = daofactory(classname='ProcessedDataset.Insert')
+        self.datasetin = daofactory(classname='Dataset.Insert')
 
 
     def listDatasets(self, dataset=""):
@@ -39,8 +39,9 @@ class DBSDataset:
         lists all datasets if dataset parameter is not given.
         The parameter can include % character. 
         """
-        return self.DatasetList.execute(dataset=dataset)
-
+        return self.datasetlist.execute(dataset=dataset)
+    
+    
     def insertDataset(self, businput):
         """
         input dictionary must have the following keys:
@@ -49,32 +50,26 @@ class DBSDataset:
         physicsgroup(name), xtcrosssection, globaltag, creationdate, createby, 
         lastmodificationdate, lastmodifiedby
         """ 
-        seqmanager = self.daofactory(classname = "SequenceManager")
-        classdict = {"primaryds":"PrimaryDataset.GetID",
-                 "datatier":"DataTier.GetID",
-                 "datasettype":"DatasetType.GetID",
-                 "physicsgroup":"PhysicsGroup.GetID"}
-        
         conn = self.dbi.connection()
         tran = conn.begin()
         try:
-            businput["primaryds"] = self.PrimaryDatasetGetID.execute(businput["primaryds"], conn, True)
-            businput["datatier"] = self.DataTierGetID.execute(businput["datatier"], conn, True)
-            businput["datasettype"] = self.DatasetTypeGetID.execute(businput["datasettype"], conn, True)
-            businput["physicsgroup"] = self.PhysicsGroupGetID.execute(businput["physicsgroup"], conn, True)
+            businput["primaryds"] = self.primdsid.execute(businput["primaryds"], conn, True)
+            businput["datatier"] = self.tierid.execute(businput["datatier"], conn, True)
+            businput["datasettype"] = self.datatypeid.execute(businput["datasettype"], conn, True)
+            businput["physicsgroup"] = self.phygrpid.execute(businput["physicsgroup"], conn, True)
 
-            procid = self.ProcessedDatasetGetID.execute(businput["processedds"])
+            procid = self.procdsid.execute(businput["processedds"])
             if procid > 0:
                 businput["processedds"] = procid
             else:
-                procid = self.SequenceManager.increment("SEQ_PSDS", conn, True)
+                procid = self.sm.increment("SEQ_PSDS", conn, True)
                 procdaoinput = {"processeddsname":businput["processedds"],
                                     "processeddsid":procid}
-                self.ProcessedDatasetInsert.execute(procdaoinput, conn, True)
+                self.procdsin.execute(procdaoinput, conn, True)
                 businput["processedds"] = procid
         
-            businput["datasetid"] = self.SequenceManager.increment("SEQ_DS", conn, True) 
-            self.DatasetInsert.execute(businput, conn, True)
+            businput["datasetid"] = self.sm.increment("SEQ_DS", conn, True) 
+            self.datasetin.execute(businput, conn, True)
             tran.commit()
         except Exception, e:
             tran.rollback()
