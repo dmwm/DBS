4e2647e1927bbff38f89b2afb5bfd09f32fb9631
akhukhun <akhukhun>
2010-09-15 19:24:24 +0000

    change the way a bind parameter is passed.
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9240 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/PrimaryDataset/List.py
+++ Server/Python/src/dbs/dao/Oracle/PrimaryDataset/List.py
@@ -2,8 +2,8 @@
 """
 This module provides PrimaryDataset.List data access object.
 """
-__revision__ = "$Id: List.py,v 1.8 2009/12/27 13:41:25 akhukhun Exp $"
-__version__ = "$Revision: 1.8 $"
+__revision__ = "$Id: List.py,v 1.9 2010/01/01 18:59:55 akhukhun Exp $"
+__version__ = "$Revision: 1.9 $"
 
 
 from WMCore.Database.DBFormatter import DBFormatter
@@ -20,11 +20,11 @@ class List(DBFormatter):
         self.owner = ("", "%s." % owner)[bool(owner)]
         self.sql = \
 """
-SELECT P.PRIMARY_DS_ID, P.PRIMARY_DS_NAME, P.PRIMARY_DS_TYPE_ID,
-    PT.PRIMARY_DS_TYPE, P.CREATION_DATE, P.CREATE_BY
+SELECT P.PRIMARY_DS_ID, P.PRIMARY_DS_NAME, 
+       P.CREATION_DATE, P.CREATE_BY,
+       PT.PRIMARY_DS_TYPE  
 FROM %sPRIMARY_DATASETS P
-JOIN %sPRIMARY_DS_TYPES PT
-ON PT.PRIMARY_DS_TYPE_ID=P.PRIMARY_DS_TYPE_ID
+JOIN %sPRIMARY_DS_TYPES PT ON PT.PRIMARY_DS_TYPE_ID = P.PRIMARY_DS_TYPE_ID
 """ % (self.owner, self.owner)
 
     def execute(self, primary_ds_name="", conn=None):
@@ -35,16 +35,15 @@ ON PT.PRIMARY_DS_TYPE_ID=P.PRIMARY_DS_TYPE_ID
             conn = self.dbi.connection()
             
         sql = self.sql
-        cursor = conn.connection.cursor()
+        binds = {}
         
-        if not primary_ds_name:
-            cursor.execute(sql)
-        else:
+        if primary_ds_name:
             op = ("=", "like")["%" in primary_ds_name]
             sql += "WHERE P.PRIMARY_DS_NAME %s :primary_ds_name" % op
-            binds = {"primary_ds_name":primary_ds_name}
-            cursor.execute(sql, binds)
+            binds.update(primary_ds_name=primary_ds_name)
             
+        cursor = conn.connection.cursor()
+        cursor.execute(sql, binds)
         result = self.formatCursor(cursor)
         conn.close()
         return result
