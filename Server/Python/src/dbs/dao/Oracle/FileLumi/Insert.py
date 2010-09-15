fd822ff71dd96836b36166fa3cd0ca59560b8155
akhukhun <akhukhun>
2010-09-15 18:29:47 +0000

    add lumis and parents to insert file
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9144 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/FileLumi/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/FileLumi/Insert.py
@@ -1,43 +1,27 @@
 #!/usr/bin/env python
-""" DAO Object for FileLumis table """ 
+""" DAO Object for FileParents table """ 
 
-__revision__ = "$Revision: 1.4 $"
-__version__  = "$Id: Insert.py,v 1.4 2009/11/17 19:46:30 yuyi Exp $ "
+__revision__ = "$Revision: 1.5 $"
+__version__  = "$Id: Insert.py,v 1.5 2009/11/19 17:37:42 akhukhun Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Insert(DBFormatter):
+
     def __init__(self, logger, dbi):
-	DBFormatter.__init__(self, logger, dbi)
-	self.owner = "%s." % self.dbi.engine.url.username
+        DBFormatter.__init__(self, logger, dbi)
+        self.owner = "%s." % self.dbi.engine.url.username
 
-	self.sql = """INSERT INTO %sFILE_LUMIS ( FILE_LUMI_ID, RUN_NUM,
-	    LUMI_SECTION_NUM, FILE_ID) VALUES (:FILE_LUMI_ID, :RUN_NUM,
-	    :LUMI_SECTION_NUM, :FILE_ID) """  % (self.owner)
+        self.sql = \
+"""
+INSERT INTO %sFILE_PARENTS 
+(FILE_PARENT_ID, THIS_FILE_ID, PARENT_FILE_ID) 
+VALUES (:FILE_PARENT_ID, :THIS_FILE_ID, :FILE_PARENT_LFN)
+""" % (self.owner)
 
-    def getBinds( self, file_lumisObj ):
-
-	binds = {}
-	if type(file_lumisObj) == type ({}):
-	    binds = {
-			'FILE_LUMI_ID' : file_lumisObj['FILE_LUMI_ID'],
-			'RUN_NUM' : file_lumisObj['RUN_NUM'],
-			'LUMI_SECTION_NUM' : file_lumisObj['LUMI_SECTION_NUM'],
-			'FILE_ID' : file_lumisObj['FILE_ID'] }
-
-	elif type(file_lumisObj) == type([]):
-	    binds = []
-	    for item in file_lumisObj:
-		binds.append({
-			'FILE_LUMI_ID' : item['FILE_LUMI_ID'],
-			'RUN_NUM' : item['RUN_NUM'],
-			'LUMI_SECTION_NUM' : item['LUMI_SECTION_NUM'],
-			'FILE_ID' : item['FILE_ID']
-		})
-	return binds
-    def execute( self, file_lumisObj, conn=None, transaction=False ):
-	binds = self.getBinds( file_lumisObj )
-	result = self.dbi.processData(self.sql, binds, conn, transaction)
-	return
-
-
+    def execute( self, daoinput, conn = None, transaction = False ):
+        """
+        daoinput must be validated to have the following keys:
+        FILE_PARENT_ID, THIS_FILE_ID, FILE_PARENT_LFN(ID)
+        """
+        self.dbi.processData(self.sql, daoinput, conn, transaction)
