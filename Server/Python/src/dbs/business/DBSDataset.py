7676be9e1f3eaf1e70747f98529b6a696162b7d6
akhukhun <akhukhun>
2010-09-15 23:54:45 +0000

    some minor changes
    From: akhukhun <akhukhun>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9509 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/business/DBSDataset.py
+++ Server/Python/src/dbs/business/DBSDataset.py
@@ -3,8 +3,8 @@
 This module provides business object class to interact with Dataset. 
 """
 
-__revision__ = "$Id: DBSDataset.py,v 1.3 2009/10/27 17:24:47 akhukhun Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: DBSDataset.py,v 1.4 2009/10/30 16:43:11 akhukhun Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from WMCore.DAOFactory import DAOFactory
 
@@ -29,3 +29,49 @@ class DBSDataset:
         result = datasetlist.execute(primdsname, procdsname, datatiername)
         return result
 
+    def insertDataset(self, businput):
+        """
+        input dictionary must have the following keys:
+        dataset, isdatasetvalid, primaryds(name), processedds(name), datatier(name),
+        datasettype(name), acquisitionera(name), processingversion(name), 
+        physicsgroup(name), xtcrosssection, globaltag, creationdate, createby, 
+        lastmodificationdate, lastmodifiedby
+        """ 
+        seqmanager = self.daofactory(classname = "SequenceManager")
+        classdict = {"primaryds":"PrimaryDataset.GetID",
+                 "datatier":"DataTier.GetID",
+                 "datasettype":"DatasetType.GetID",
+                 "acquisitionera":"AcquisitionEra.GetID",
+                 "processingversion":"ProcessingEra.GetID",
+                 "physicsgroup":"PhysicsGroup.GetID"}
+        
+        conn = self.dbi.connection()
+        tran = conn.begin()
+        try:
+            for k in classdict.keys():
+                dao = self.daofactory(classname = classdict[k])
+                businput[k] = dao.execute(businput[k], conn, True)
+            
+            processedds = self.daofactory(classname = "ProcessedDataset.GetID")
+            if processedds.execute(businput["processedds"]) > 0:
+                businput["processedds"] = processedds.execute(businput["processedds"])
+            else:
+                dao = self.daofactory(classname = "ProcessedDataset.Insert")
+                procid = seqmanager.increment("SEQ_PSDS", conn, True)
+                procdaoinput = {"processeddsname":businput["processedds"],
+                                    "processeddsid":procid}
+                dao.execute(procdaoinput, conn, True)
+                businput["processedds"] = procid
+        
+            businput["datasetid"] = seqmanager.increment("SEQ_DS", conn, True) 
+            datasetinsert = self.daofactory(classname = "Dataset.Insert")
+            datasetinsert.execute(businput, conn, True)
+            tran.commit()
+        except Exception, e:
+            tran.rollback()
+            self.logger.exception(e)
+            raise
+        finally:
+            conn.close()
+            
+            
--- Server/Python/src/dbs/dao/Oracle/Block/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/Block/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for Blocks table """ 
 
-__revision__ = "$Revision: 1.3 $"
-__version__  = "$Id: Insert.py,v 1.3 2009/10/27 17:24:48 akhukhun Exp $ "
+__revision__ = "$Revision: 1.4 $"
+__version__  = "$Id: Insert.py,v 1.4 2009/10/30 16:44:37 akhukhun Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -18,13 +18,13 @@ class Insert(DBFormatter):
 INSERT INTO %sBLOCKS 
 (BLOCK_ID, BLOCK_NAME, DATASET_ID, OPEN_FOR_WRITING, ORIGIN_SITE, BLOCK_SIZE, 
 FILE_COUNT, CREATION_DATE, CREATE_BY, LAST_MODIFICATION_DATE, LAST_MODIFIED_BY) 
-VALUES(:blockid, :blockname, :datasetid, :openforwriting, :originsite, :blocksize, 
+VALUES(:blockid, :blockname, :dataset, :openforwriting, :originsite, :blocksize, 
 :filecount, :creationdate, :createby, :lastmodificationdate, :lastmodifiedby)
 """ % (self.owner)
 
     def execute( self, daoinput, conn = None, transaction = False):
         """daoinput must be validated to have the following keys:
-        blockid, blockname, datasetid, openforwriting, originsite, blocksize,
+        blockid, blockname, dataset(id), openforwriting, originsite(id), blocksize,
         filecount, creationdate, createby, lastmodificationdate, lastmodifiedby
         """
         self.dbi.processData(self.sql, daoinput, conn, transaction)
