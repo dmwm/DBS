47e533070cc79fb8c6d3277eb40c0c3d2d273a36
akhukhun <akhukhun>
2010-09-15 17:53:42 +0000

    used by insert block
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9087 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Dataset/GetID.py
+++ Server/Python/src/dbs/dao/Oracle/Dataset/GetID.py
@@ -3,8 +3,8 @@
 This module provides Dataset.GetID data access object.
 Light dao object to get the id for a give /primds/procds/tier
 """
-__revision__ = "$Id: GetID.py,v 1.1 2009/10/28 12:32:57 akhukhun Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: GetID.py,v 1.2 2009/10/30 16:46:46 akhukhun Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 class GetID(DBFormatter):
@@ -24,14 +24,14 @@ FROM %sDATASETS D
 """ % ( self.owner )
         
 	
-    def execute(self, path, conn = None, transaction = False):
+    def execute(self, name, conn = None, transaction = False):
         """
         returns id for a given path = /primds/procds/tier
         """	
         sql = self.sql
         sql += "WHERE D.DATASET = :path"
-        binds = {"path":path}
+        binds = {"path":name}
         result = self.dbi.processData(sql, binds, conn, transaction)
         plist = self.formatDict(result)
-        assert len(plist) == 1, "Dataset %s does not exist" % path
+        assert len(plist) == 1, "Dataset %s does not exist" % name
         return plist[0]["dataset_id"]
