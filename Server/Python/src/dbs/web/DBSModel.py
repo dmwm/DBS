1873ee40ed5da7b305ff352ef3a9fef560e9f8c8
akhukhun <akhukhun>
2010-09-15 17:57:40 +0000

    Add listBlocks
    From: akhukhun <akhukhun>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9092 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/web/DBSModel.py
+++ Server/Python/src/dbs/web/DBSModel.py
@@ -3,8 +3,8 @@
 DBS Rest Model module
 """
 
-__revision__ = "$Id: DBSModel.py,v 1.6 2009/10/28 15:10:30 akhukhun Exp $"
-__version__ = "$Revision: 1.6 $"
+__revision__ = "$Id: DBSModel.py,v 1.7 2009/10/30 16:54:26 akhukhun Exp $"
+__version__ = "$Revision: 1.7 $"
 
 
 import re, json
@@ -26,10 +26,13 @@ class DBSModel(RESTModel):
         RESTModel.__init__(self, config)
         self.methods = {'GET':{}, 'PUT':{}, 'POST':{}, 'DELETE':{}}
         self.addService('GET', 'primds', self.listPrimaryDatasets, \
-                        ['primdsname'])
+                        ['primds'])
         self.addService('GET', 'datasets', self.listDatasets, \
-                        ['primdsname', 'procdsname', 'datatiername'])
+                        ['primds', 'procds', 'tier'])
+        self.addService('GET', 'blocks', self.listBlocks, \
+                        ['primds', 'procds', 'tier', 'block'])
         self.addService('PUT', 'primds', self.insertPrimaryDataset)
+        self.addService('PUT', 'datasets', self.insertDataset)
         self.addService('PUT', 'blocks', self.insertBlock)
         self.addService('POST', 'post', self.donothing)
         self.addService('DELETE', 'delete', self.donothing)
@@ -44,7 +47,7 @@ class DBSModel(RESTModel):
                                          'validation': validation,
                                          'version': version}
  
-    def listPrimaryDatasets(self, primdsname = ""):
+    def listPrimaryDatasets(self, primds = ""):
         """
         Example url's:
         http://dbs3/primds/
@@ -52,28 +55,40 @@ class DBSModel(RESTModel):
         http://dbs3/primds/qcd*
         """
         data = {}
-        primdsname = primdsname.replace("*","%")
+        primds = primds.replace("*","%")
         bo = DBSPrimaryDataset(self.logger, self.dbi)
-        result = bo.listPrimaryDatasets(primdsname) 
+        result = bo.listPrimaryDatasets(primds) 
         data.update({'result':result})
         return data
     
-    def listDatasets(self, primdsname = "", procdsname = "", datatiername = ""):
+    def listDatasets(self, primds = "", procds = "", tier = ""):
         """
         Example url's:
         http://dbs3/datasets
         http://dbs3/datasets/RelVal*
         http://dbs3/datasets/RelVal*/*/*RECO
         """
-        data = {}
-        primdsname = primdsname.replace("*", "%")
-        procdsname = procdsname.replace("*", "%")
-        datatiername = datatiername.replace("*", "%")
+        primds = primds.replace("*", "%")
+        procds = procds.replace("*", "%")
+        tier = tier.replace("*", "%")
         
         bo = DBSDataset(self.logger, self.dbi)
-        result = bo.listDatasets(primdsname, procdsname, datatiername)
-        data.update({'result':result})
-        return data
+        result = bo.listDatasets(primds, procds, tier)
+        return {'result':result}
+    
+    def listBlocks(self, primds, procds, tier, block=""):
+        """
+        Example url's:
+        http://dbs3/blocks/a/b/c
+        http://dbs3/blocks/a/b/c/s*df
+        """
+        block = block.replace("*","%")
+        bo = DBSBlock(self.logger, self.dbi)
+        dataset = "/".join(("/%s", "%s", "%s"))%(primds, procds, tier)
+        if not block == "": 
+            block = dataset + "#" + block
+        result = bo.listBlocks(dataset, block)
+        return {"result":result}
 
     def insertPrimaryDataset(self):
         """
@@ -85,7 +100,7 @@ class DBSModel(RESTModel):
         indata = json.loads(body)
 
         #input validation
-        valid = re.compile("^[\w\d_-]*$")
+        valid = re.compile("^[\w\d_-]+$")
         assert type(indata) == dict
         assert len(indata.keys()) == 2, "Invalid input, %s" %(str(indata))
         assert valid.match(indata["primarydsname"]), \
@@ -97,42 +112,75 @@ class DBSModel(RESTModel):
         bo = DBSPrimaryDataset(self.logger, self.dbi)
         bo.insertPrimaryDataset(indata)
     
+    def insertDataset(self):
+        """
+        gets the input from cherrypy request body.
+        input must have the following keys:
+        dataset, isdatasetvalid
+        datasettype, acquisitionera, processingversion,
+        physicsgroup, xtcrosssection, globaltag
+        """
+        body = request.body.read()
+        indata = json.loads(body)
+        
+        #>> validation.. will be moved to a separate method probably
+        assert type(indata) == dict
+        assert len(indata.keys()) == 8
+        vds = re.match(r"/([\w\d_-]+)/([\w\d_-]+)/([\w\d_-]+)", indata["dataset"])
+        assert vds
+        assert type(indata["isdatasetvalid"]) == bool
+        assert type(indata["xtcrosssection"]) == float
+        #finish validation
+        #<< end of validation
+        
+        indata.update({"primaryds":vds.groups()[0],
+                      "processedds":vds.groups()[1],
+                      "datatier":vds.groups()[2],
+                      "creationdate":1234,
+                      "createby":"me",
+                      "lastmodificationdate":1234,
+                      "lastmodifiedby":"alsome"})
+        
+        bo = DBSDataset(self.logger, self.dbi)
+        bo.insertDataset(indata)
+                
+         
+        
+        
     def insertBlock(self):
         """
         gets the input from cherrypy request body.
         input must be a dictionary with the following keys:
         blockname:/string/string/string#string
         openforwriting bool
-        sitename string
+        originsite string
         blocksize int
         filecount int
         """
         body = request.body.read()
         indata = json.loads(body)
         
-        validblockname = re.compile("^/[\w\d_-]+/[\w\d_-]+/[\w\d_-]+#[\w\d_-]+$")
-        
+        #>>validation - will be moved to the separate method later.
         assert type(indata) == dict
         assert len(indata.keys()) == 5
         assert "blockname" in indata.keys()
-        assert validblockname.match(indata["blockname"]), \
-            "Invalid input format of %s, must be /a/b/c#d." % indata["blockname"] 
+        validblockname = re.compile("^/[\w\d_-]+/[\w\d_-]+/[\w\d_-]+#[\w\d_-]+$")
+        assert validblockname.match(indata["blockname"])
+        assert type(indata["blocksize"]) == int
+        assert type(indata["filecount"]) == int
+        assert type(indata["openforwriting"]) == bool
+        
+        #<<end of validation
         dlist = indata["blockname"].split("#")
-        dataset = dlist[0]
-        indata["dataset"] = dataset
-        indata["blocksize"] = int(indata["blocksize"])
-        indata["filecount"] = int(indata["filecount"])
-        indata.update({"creationdate":123456,
+        indata.update({"dataset":dlist[0],
+                       "creationdate":123456,
                        "createby":"me",
                        "lastmodificationdate":12345,
                        "lastmodifiedby":"me"})
+        #<<possible end of the validation 
         bo = DBSBlock(self.logger, self.dbi)
         bo.insertBlock(indata)
         
-        
-            
-            
-            
         
     def donothing(self, *args, **kwargs):
         """
