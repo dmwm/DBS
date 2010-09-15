a9b1d3a6b22a97f953691450b9b74406e5ade53a
afaq <afaq>
2010-09-15 19:53:50 +0000

    updated dao objects, missed couple of changes after last schema changes i guess
    From: afaq <afaq>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9298 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/ParameterSetHashe/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/ParameterSetHashe/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for ParameterSetHashes table """ 
 
-__revision__ = "$Revision: 1.5 $"
-__version__  = "$Id: Insert.py,v 1.5 2010/01/11 22:56:44 afaq Exp $ "
+__revision__ = "$Revision: 1.6 $"
+__version__  = "$Id: Insert.py,v 1.6 2010/01/21 20:08:09 afaq Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -12,12 +12,12 @@ class Insert(DBFormatter):
             DBFormatter.__init__(self, logger, dbi)
 	    self.owner = "%s." % owner
 
-            self.sql = """INSERT INTO %sPARAMETER_SET_HASHES ( PARAMETER_SET_HASH_ID, HASH, NAME) VALUES (:parameter_set_hash_id, :pset_hash, :name)""" % (self.owner)
+            self.sql = """INSERT INTO %sPARAMETER_SET_HASHES ( PARAMETER_SET_HASH_ID, PSET_HASH, NAME) VALUES (:parameter_set_hash_id, :pset_hash, :name)""" % (self.owner)
 
     def execute( self, psetHashObj, conn=None, transaction=False ):
 	try:
 	    result = self.dbi.processData(self.sql, psetHashObj, conn, transaction)
-	except Exception, e:
+	except Exception, ex:
 	    if str(ex).lower().find("unique constraint") != -1 :
 		pass
 	    else:
