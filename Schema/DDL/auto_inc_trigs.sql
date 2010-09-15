f8c87450c280ce4dd2682a9cbdb4ca802acf8984
afaq <afaq>
2010-09-15 23:40:35 +0000

    auto inc triggers updated
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9423 4525493e-7705-40b1-a816-d608a930855b

--- Schema/DDL/auto_inc_trigs.sql
+++ Schema/DDL/auto_inc_trigs.sql
@@ -1,87 +1,87 @@
 
-CREATE OR REPLACE TRIGGER AQE_TRIG before insert on ACQUISITION_ERAS for each row begin if :NEW.ACQUISITION_ERA_ID is null then select seq_AQE.nextval into :NEW.ACQUISITION_ERA_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER AE_TRIG before insert on APPLICATION_EXECUTABLES for each row begin if :NEW.APP_EXEC_ID is null then select SEQ_AE.nextval into :NEW.APP_EXEC_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER AE_TRIG before insert on APPLICATION_EXECUTABLES for each row begin if :NEW.APP_EXEC_ID is null then select seq_AE.nextval into :NEW.APP_EXEC_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER RVV_TRIG before insert on RELEASE_VERSIONS for each row begin if :NEW.RELEASE_VERSION_ID is null then select SEQ_RVV.nextval into :NEW.RELEASE_VERSION_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER AF_TRIG before insert on ASSOCIATED_FILES for each row begin if :NEW.ASSOCATED_FILE_ID is null then select seq_AF.nextval into :NEW.ASSOCATED_FILE_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER PSDS_TRIG before insert on PROCESSED_DATASETS for each row begin if :NEW.PROCESSED_DS_ID is null then select SEQ_PSDS.nextval into :NEW.PROCESSED_DS_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER BK_TRIG before insert on BLOCKS for each row begin if :NEW.BLOCK_ID is null then select seq_BK.nextval into :NEW.BLOCK_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER BH_TRIG before insert on BRANCH_HASHES for each row begin if :NEW.BRANCH_HASH_ID is null then select SEQ_BH.nextval into :NEW.BRANCH_HASH_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER BP_TRIG before insert on BLOCK_PARENTS for each row begin if :NEW.BLOCK_PARENT_ID is null then select seq_BP.nextval into :NEW.BLOCK_PARENT_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER FT_TRIG before insert on FILE_TYPES for each row begin if :NEW.FILE_TYPE_ID is null then select SEQ_FT.nextval into :NEW.FILE_TYPE_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER BSE_TRIG before insert on BLOCK_STORAGE_ELEMENTS for each row begin if :NEW.BLOCK_SE_ID is null then select seq_BSE.nextval into :NEW.BLOCK_SE_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER PG_TRIG before insert on PHYSICS_GROUPS for each row begin if :NEW.PHYSICS_GROUP_ID is null then select SEQ_PG.nextval into :NEW.PHYSICS_GROUP_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER BH_TRIG before insert on BRANCH_HASHES for each row begin if :NEW.BRANCH_HASH_ID is null then select seq_BH.nextval into :NEW.BRANCH_HASH_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER PDT_TRIG before insert on PRIMARY_DS_TYPES for each row begin if :NEW.PRIMARY_DS_TYPE_ID is null then select SEQ_PDT.nextval into :NEW.PRIMARY_DS_TYPE_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER DT_TRIG before insert on DATA_TIERS for each row begin if :NEW.DATA_TIER_ID is null then select seq_DT.nextval into :NEW.DATA_TIER_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER DTP_TRIG before insert on DATASET_TYPES for each row begin if :NEW.DATASET_TYPE_ID is null then select SEQ_DTP.nextval into :NEW.DATASET_TYPE_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER DV_TRIG before insert on DBS_VERSIONS for each row begin if :NEW.DBS_VERSION_ID is null then select seq_DV.nextval into :NEW.DBS_VERSION_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER PSH_TRIG before insert on PARAMETER_SET_HASHES for each row begin if :NEW.PARAMETER_SET_HASH_ID is null then select SEQ_PSH.nextval into :NEW.PARAMETER_SET_HASH_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER FL_TRIG before insert on FILES for each row begin if :NEW.FILE_ID is null then select seq_FL.nextval into :NEW.FILE_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER DV_TRIG before insert on DBS_VERSIONS for each row begin if :NEW.DBS_VERSION_ID is null then select SEQ_DV.nextval into :NEW.DBS_VERSION_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER FLM_TRIG before insert on FILE_LUMIS for each row begin if :NEW.FILE_LUMI_ID is null then select seq_FLM.nextval into :NEW.FILE_LUMI_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER SI_TRIG before insert on SITES for each row begin if :NEW.SITE_ID is null then select SEQ_SI.nextval into :NEW.SITE_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER FP_TRIG before insert on FILE_PARENTS for each row begin if :NEW.FILE_PARENT_ID is null then select seq_FP.nextval into :NEW.FILE_PARENT_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER OMC_TRIG before insert on OUTPUT_MODULE_CONFIGS for each row begin if :NEW.OUTPUT_MOD_CONFIG_ID is null then select SEQ_OMC.nextval into :NEW.OUTPUT_MOD_CONFIG_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER FPC_TRIG before insert on FILE_PROCESS_CONFIGS for each row begin if :NEW.FILE_PROCESS_CONFIG_ID is null then select seq_FPC.nextval into :NEW.FILE_PROCESS_CONFIG_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER DT_TRIG before insert on DATA_TIERS for each row begin if :NEW.DATA_TIER_ID is null then select SEQ_DT.nextval into :NEW.DATA_TIER_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER FT_TRIG before insert on FILE_TYPES for each row begin if :NEW.FILE_TYPE_ID is null then select seq_FT.nextval into :NEW.FILE_TYPE_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER PDS_TRIG before insert on PRIMARY_DATASETS for each row begin if :NEW.PRIMARY_DS_ID is null then select SEQ_PDS.nextval into :NEW.PRIMARY_DS_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PSH_TRIG before insert on PARAMETER_SET_HASHES for each row begin if :NEW.PARAMETER_SET_HASH_ID is null then select seq_PSH.nextval into :NEW.PARAMETER_SET_HASH_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER DS_TRIG before insert on DATASETS for each row begin if :NEW.DATASET_ID is null then select SEQ_DS.nextval into :NEW.DATASET_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PH_TRIG before insert on PATHS for each row begin if :NEW.PATH_ID is null then select seq_PH.nextval into :NEW.PATH_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER BK_TRIG before insert on BLOCKS for each row begin if :NEW.BLOCK_ID is null then select SEQ_BK.nextval into :NEW.BLOCK_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PP_TRIG before insert on PATH_PARENTS for each row begin if :NEW.PATH_PARENT_ID is null then select seq_PP.nextval into :NEW.PATH_PARENT_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER BP_TRIG before insert on BLOCK_PARENTS for each row begin if :NEW.BLOCK_PARENT_ID is null then select SEQ_BP.nextval into :NEW.BLOCK_PARENT_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PPC_TRIG before insert on PATH_PROCESS_CONFIGS for each row begin if :NEW.PATH_PROCESS_CONF_ID is null then select seq_PPC.nextval into :NEW.PATH_PROCESS_CONF_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER FL_TRIG before insert on FILES for each row begin if :NEW.FILE_ID is null then select SEQ_FL.nextval into :NEW.FILE_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PR_TRIG before insert on PATH_RUNS for each row begin if :NEW.PATH_RUN_ID is null then select seq_PR.nextval into :NEW.PATH_RUN_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER DC_TRIG before insert on DATASET_OUTPUT_MOD_CONFIGS for each row begin if :NEW.DS_OUTPUT_MOD_CONF_ID is null then select SEQ_DC.nextval into :NEW.DS_OUTPUT_MOD_CONF_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PT_TRIG before insert on PATH_TYPES for each row begin if :NEW.PATH_TYPE_ID is null then select seq_PT.nextval into :NEW.PATH_TYPE_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER DP_TRIG before insert on DATASET_PARENTS for each row begin if :NEW.DATASET_PARENT_ID is null then select SEQ_DP.nextval into :NEW.DATASET_PARENT_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PG_TRIG before insert on PHYSICS_GROUPS for each row begin if :NEW.PHYSICS_GROUP_ID is null then select seq_PG.nextval into :NEW.PHYSICS_GROUP_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER DR_TRIG before insert on DATASET_RUNS for each row begin if :NEW.PATH_RUN_ID is null then select SEQ_DR.nextval into :NEW.PATH_RUN_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PDS_TRIG before insert on PRIMARY_DATASETS for each row begin if :NEW.PRIMARY_DS_ID is null then select seq_PDS.nextval into :NEW.PRIMARY_DS_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER FC_TRIG before insert on FILE_OUTPUT_MOD_CONFIGS for each row begin if :NEW.FILE_OUTPUT_CONFIG_ID is null then select SEQ_FC.nextval into :NEW.FILE_OUTPUT_CONFIG_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PDT_TRIG before insert on PRIMARY_DS_TYPES for each row begin if :NEW.PRIMARY_DS_TYPE_ID is null then select seq_PDT.nextval into :NEW.PRIMARY_DS_TYPE_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER AF_TRIG before insert on ASSOCIATED_FILES for each row begin if :NEW.ASSOCATED_FILE_ID is null then select SEQ_AF.nextval into :NEW.ASSOCATED_FILE_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PSDS_TRIG before insert on PROCESSED_DATASETS for each row begin if :NEW.PROCESSED_DS_ID is null then select seq_PSDS.nextval into :NEW.PROCESSED_DS_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER FP_TRIG before insert on FILE_PARENTS for each row begin if :NEW.FILE_PARENT_ID is null then select SEQ_FP.nextval into :NEW.FILE_PARENT_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PE_TRIG before insert on PROCESSING_ERAS for each row begin if :NEW.PROCESSING_ERA_ID is null then select seq_PE.nextval into :NEW.PROCESSING_ERA_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER FLM_TRIG before insert on FILE_LUMIS for each row begin if :NEW.FILE_LUMI_ID is null then select SEQ_FLM.nextval into :NEW.FILE_LUMI_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER PC_TRIG before insert on PROCESS_CONFIGURATIONS for each row begin if :NEW.PROCESS_CONFIG_ID is null then select seq_PC.nextval into :NEW.PROCESS_CONFIG_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER AQE_TRIG before insert on ACQUISITION_ERAS for each row begin if :NEW.ACQUISITION_ERA_ID is null then select SEQ_AQE.nextval into :NEW.ACQUISITION_ERA_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER RV_TRIG before insert on RELEASE_VERSIONS for each row begin if :NEW.RELEASE_VERSION_ID is null then select seq_RV.nextval into :NEW.RELEASE_VERSION_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER PE_TRIG before insert on PROCESSING_ERAS for each row begin if :NEW.PROCESSING_ERA_ID is null then select SEQ_PE.nextval into :NEW.PROCESSING_ERA_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER SI_TRIG before insert on SITES for each row begin if :NEW.SITE_ID is null then select seq_SI.nextval into :NEW.SITE_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER SE_TRIG before insert on STORAGE_ELEMENTS for each row begin if :NEW.SE_ID is null then select SEQ_SE.nextval into :NEW.SE_ID from dual; end if; end;
  /
 
-CREATE OR REPLACE TRIGGER SE_TRIG before insert on STORAGE_ELEMENTS for each row begin if :NEW.SE_ID is null then select seq_SE.nextval into :NEW.SE_ID from dual; end if; end;
+CREATE OR REPLACE TRIGGER BSE_TRIG before insert on BLOCK_STORAGE_ELEMENTS for each row begin if :NEW.BLOCK_SE_ID is null then select SEQ_BSE.nextval into :NEW.BLOCK_SE_ID from dual; end if; end;
  /
