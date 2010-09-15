2dc39d94559f6d2ccfb9fcf4fa739ae1de46c0f3
afaq <afaq>
2010-09-15 18:22:37 +0000

    insert dataset works for the python server, fixed the ProcessedDataset DAO, was returning name instead of id
    From: afaq <afaq>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9133 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Dataset/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/Dataset/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for Datasets table """ 
 
-__revision__ = "$Revision: 1.3 $"
-__version__  = "$Id: Insert.py,v 1.3 2009/10/30 16:47:22 akhukhun Exp $ "
+__revision__ = "$Revision: 1.4 $"
+__version__  = "$Id: Insert.py,v 1.4 2009/11/16 21:44:47 afaq Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -18,22 +18,29 @@ class Insert(DBFormatter):
 INSERT INTO %sDATASETS 
 (DATASET_ID, DATASET, IS_DATASET_VALID, 
 PRIMARY_DS_ID, PROCESSED_DS_ID, DATA_TIER_ID, DATASET_TYPE_ID, 
-ACQUISITION_ERA_ID, PROCESSING_ERA_ID, PHYSICS_GROUP_ID, 
+PHYSICS_GROUP_ID, 
 XTCROSSSECTION, GLOBAL_TAG, CREATION_DATE, CREATE_BY, 
 LAST_MODIFICATION_DATE, LAST_MODIFIED_BY) 
 VALUES(:datasetid, :dataset, :isdatasetvalid, 
 :primaryds, :processedds, :datatier, :datasettype, 
-:acquisitionera, :processingversion, :physicsgroup, 
+:physicsgroup, 
 :xtcrosssection, :globaltag, :creationdate, :createby, 
 :lastmodificationdate, :lastmodifiedby)
 """ % self.owner
-    
+
+# Removed    
+#ACQUISITION_ERA_ID, PROCESSING_ERA_ID, 
+#:processingversion
+#:acquisitionera, 
+
+
+#acquisitionera(id), processingversion(id),
 
     def execute(self, daoinput, conn = None, transaction = False):
         """
         daoinput must be a dictionary with the following keys:
         datasetid, dataset, isdatasetvalid, primaryds(id), processedds(id),
-        datatier(id), datasettype(id), acquisitionera(id), processingversion(id),
+        datatier(id), datasettype(id), 
         physicsgroup(id), xtcrosssection, globaltag, 
         creationdate, createby, 
         lastmodificationdate, lastmodifiedby"""
