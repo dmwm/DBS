ed39f25eca36d36df44893cfdb893a47272e558a
yuyi <yuyi>
2010-09-16 00:49:35 +0000

    replace List1
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9841 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/MySQL/Dataset/List.py
+++ Server/Python/src/dbs/dao/MySQL/Dataset/List.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 """
 This module provides Dataset.List data access object.
+Lists dataset_parent and output configuration parameters too.
 """
-__revision__ = "$Id: List.py,v 1.2 2010/02/11 19:39:30 afaq Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: List.py,v 1.3 2010/03/02 20:18:05 yuyi Exp $"
+__version__ = "$Revision: 1.3 $"
+
 from dbs.dao.Oracle.Dataset.List import List as OraDatasetList
 
 class List(OraDatasetList):
         pass
-
 
