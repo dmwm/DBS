8ad4127aa82e9f16d191ba643d5bab421b4d29e0
afaq <afaq>
2010-09-16 01:35:38 +0000

    updating DAO for mig srvc
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10113 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/MigrationRequests/ListOldest.py
+++ Server/Python/src/dbs/dao/Oracle/MigrationRequests/ListOldest.py
@@ -2,8 +2,8 @@
 """
 This module provides MigrationRequests.ListOldest data access object.
 """
-__revision__ = "$Id: ListOldest.py,v 1.1 2010/06/28 21:29:23 afaq Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: ListOldest.py,v 1.2 2010/06/29 19:23:39 afaq Exp $"
+__version__ = "$Revision: 1.2 $"
 
 
 from WMCore.Database.DBFormatter import DBFormatter
@@ -18,7 +18,7 @@ class ListOldest(DBFormatter):
         """
         DBFormatter.__init__(self, logger, dbi)
         self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
-        self.sql = """SELECT MIN(MIGRATION_REQUEST_ID) FROM %sMIGRATION_REQUESTS WHERE MIGRATION_STATUS='PENDING')""" % (self.owner)
+        self.sql = """SELECT MIN(MIGRATION_REQUEST_ID) AS MIGRATION_REQUEST_ID FROM %sMIGRATION_REQUESTS WHERE MIGRATION_STATUS='PENDING' """ % (self.owner)
 
     def execute(self, conn, migration_url="", migration_input="", create_by="", migration_request_id="", transaction=False):
         """
@@ -26,6 +26,10 @@ class ListOldest(DBFormatter):
         """
 
         binds = {}
-	result = self.dbi.processData(sql, binds, conn, transaction)
+	result = self.dbi.processData(self.sql, binds, conn, transaction)
         result = self.formatDict(result)
+	if len(result) == 0 :
+	    return []
+	if result[0]["migration_request_id"] in ('', None) :
+	    return []
         return result
