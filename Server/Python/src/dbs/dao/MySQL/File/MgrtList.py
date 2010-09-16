1cbe4ce927c845cf5ce6db5207fd3d4b13cdf7a9
yuyi <yuyi>
2010-09-16 01:52:42 +0000

    special dao for migration
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10215 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/MySQL/File/MgrtList.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides File.MgrtList data access object.
+"""
+__revision__ = "$Id: MgrtList.py,v 1.1 2010/08/18 14:59:19 yuyi Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from dbs.dao.Oracle.File.MgrtList import MgrtList as OraFileMgrtList
+
+class MgrtList(OraFileMgrtList):
+        pass
+
