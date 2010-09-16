6de0c10dbd25672fe24fbd0114cb895db772affa
yuyi <yuyi>
2010-09-16 01:53:35 +0000

    initial checking
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10220 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/MySQL/MigrationBlock/FindMigrateableBlocks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides MigrationBlock.FindMigrateableBlock data access object.
+"""
+__revision__ = "$Id: FindMigrateableBlocks.py,v 1.1 2010/08/18 19:21:12 yuyi Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from dbs.dao.Oracle.MigrationBlock.FindMigrateableBlocks import FindMigrateableBlocks as OraMigFindMigrateableBlock
+
+class FindMigrateableBlocks(OraMigFindMigrateableBlock):
+    pass
+		   
