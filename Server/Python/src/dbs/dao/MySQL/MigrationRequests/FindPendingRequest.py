8eee95ae9601de74fa7e76d12da61e2d565b9f9d
yuyi <yuyi>
2010-09-16 01:53:03 +0000

    *** empty log message ***
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10217 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/MySQL/MigrationRequests/FindPendingRequest.py
+++ Server/Python/src/dbs/dao/MySQL/MigrationRequests/FindPendingRequest.py
@@ -2,10 +2,10 @@
 """
 This module provides MigrationRequests.FindPendingRequest data access object.
 """
-__revision__ = "$Id: FindPendingRequest.py,v 1.1 2010/08/18 18:46:38 yuyi Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: FindPendingRequest.py,v 1.2 2010/08/18 18:48:59 yuyi Exp $"
+__version__ = "$Revision: 1.2 $"
 
-from dbs.dao.Oracle.MigrationRequests.FindPendingRequest import List as OraMigFindPendingRequest
+from dbs.dao.Oracle.MigrationRequests.FindPendingRequest import FindPendingRequest as OraMigFindPendingRequest
 
 class FindPendingRequest(OraMigFindPendingRequest):
     pass
--- Server/Python/src/dbs/dao/MySQL/MigrationRequests/UpdateRequestStatus.py
+++ Server/Python/src/dbs/dao/MySQL/MigrationRequests/UpdateRequestStatus.py
@@ -2,10 +2,10 @@
 """
 This module provides MigrationRequests.UpdateRequestStatus data access object.
 """
-__revision__ = "$Id: UpdateRequestStatus.py,v 1.1 2010/08/18 18:46:38 yuyi Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: UpdateRequestStatus.py,v 1.2 2010/08/18 18:48:59 yuyi Exp $"
+__version__ = "$Revision: 1.2 $"
 
-from dbs.dao.Oracle.MigrationRequests.UpdateRequestStatus import List as OraMigUpdateRequestStatus
+from dbs.dao.Oracle.MigrationRequests.UpdateRequestStatus import UpdateRequestStatus as OraMigUpdateRequestStatus
 
 class UpdateRequestStatus(OraMigUpdateRequestStatus):
     pass
