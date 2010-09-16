2c23077b87f9cc411fcc6abe23b977b2639d2e1f
afaq <afaq>
2010-09-16 00:06:49 +0000

    updated scripts
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9583 4525493e-7705-40b1-a816-d608a930855b

---
+++ Client/utils/run_py_test.sh
@@ -0,0 +1,1 @@
+python2.6 dbs2Todbs3DatasetMigrate.py http://vocms09.cern.ch:8585/dbs3 /TkCosmics38T/Summer09-STARTUP31X_V3-v1/GEN-SIM-DIGI-RAW
---
+++ Client/utils/test_parallel_read.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+python listprimary.py > /dev/null 2>&1 &
+python listprimary.py > /dev/null 2>&1 &
+python listprimary.py > /dev/null 2>&1 &
+python listprimary.py > /dev/null 2>&1 &
+python listprimary.py > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+python listfiles.py  > /dev/null 2>&1 &
+wait
