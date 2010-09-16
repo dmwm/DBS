d631f057f12931055657616e50370dbc29d6c98c
afaq <afaq>
2010-09-16 01:10:51 +0000

    a single space missing the queries
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9960 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Dataset/List.py
+++ Server/Python/src/dbs/dao/Oracle/Dataset/List.py
@@ -3,8 +3,8 @@
 This module provides Dataset.List data access object.
 Lists dataset_parent and output configuration parameters too.
 """
-__revision__ = "$Id: List.py,v 1.26 2010/03/18 20:19:22 afaq Exp $"
-__version__ = "$Revision: 1.26 $"
+__revision__ = "$Id: List.py,v 1.27 2010/03/23 16:57:43 afaq Exp $"
+__version__ = "$Revision: 1.27 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -100,11 +100,11 @@ AND DP.DATASET_TYPE <> 'DELETED'
             binds.update(output_module_label=output_module_label)
 	if processing_version:
 	    op = ("=", "like")["%" in processing_version]
-	    sql += "AND PE.PROCESSING_VERSION %s :pversion" % op
+	    sql += " AND PE.PROCESSING_VERSION %s :pversion" % op
 	    binds.update(pversion=processing_version)
 	if acquisition_era:
 	    op = ("=", "like")["%" in acquisition_era]
-	    sql += "AND AE.ACQUISITION_ERA_NAME %s :aera" % op
+	    sql += " AND AE.ACQUISITION_ERA_NAME %s :aera" % op
 	    binds.update(aera=acquisition_era)
 	cursors = self.dbi.processData(sql, binds, conn, transaction, returnCursor=True)
 	assert len(cursors) == 1, "block does not exist"
