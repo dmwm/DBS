4fc49aeef486a53937ce5497ca8cf5612126e745
afaq <afaq>
2010-09-16 00:45:29 +0000

    developer tests, check pointing
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9820 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/tests/SimpleBizlogicTest.py
+++ Server/Python/tests/SimpleBizlogicTest.py
@@ -1,5 +1,6 @@
 import logging
 from WMCore.Database.DBFactory import DBFactory
+from dbs.business.DBSPrimaryDataset import DBSPrimaryDataset
 from dbs.business.DBSDataset import DBSDataset
 from dbs.business.DBSOutputConfig import DBSOutputConfig
 from dbs.business.DBSAcquisitionEra import DBSAcquisitionEra
@@ -9,19 +10,30 @@ from dbs.business.DBSFile import DBSFile
 
 class Test:
 
+    
     def __init__(self):
         #url = "oracle://user:password@host:port/sid"
 	url="oracle://anzar:anzar_cms2009@uscmsdb03.fnal.gov:1521/cmscald"
+	self.owner="anzar"
+	#url="mysql://dbs3:dbs3_pass@cmssrv49.fnal.gov:3306/CMS_DBS3"
+	#self.owner="__MYSQL__"
         self.logger = logging.getLogger("dbs test logger")
         self.dbi = DBFactory(self.logger, url).connect()
 
+
+    def testPrimary(self):
+	
+	bo = DBSPrimaryDataset(self.logger, self.dbi, self.owner)
+	binput = {"primary_ds_type": "test", "primary_ds_name": "anzar_0002"}
+	bo.insertPrimaryDataset(binput)
+    
     def testOutputConfig(self):
 	"""
 	This method can be used to test OutputConfig Buisiness Object
 
 	"""
-        bo = DBSOutputConfig(self.logger, self.dbi, "anzar")
-	binput = {'app_name': 'Repacker', 'release_version': 'CMSSW_2_1_7',  'pset_hash': 'NO_PSET_HASH', 'output_module_label' : 'outmod_test_label', 'creation_date' : 1234, 'create_by' : 'anzar' }
+        bo = DBSOutputConfig(self.logger, self.dbi, self.owner)
+	binput = {'app_name': 'Re1-Repacker', 'release_version': 'CMSSW_12_1_8',  'pset_hash': 'N11O_PSET_HASH', 'output_module_label' : 'outmod_test_label_12', 'creation_date' : 1234, 'create_by' : 'anzar' }
         bo.insertOutputConfig(binput)
 
 
@@ -30,12 +42,12 @@ class Test:
         This method is being used for testing datasets's insert DAO
         """
 
-	bo = DBSDataset(self.logger, self.dbi, "anzar")
+	bo = DBSDataset(self.logger, self.dbi, self.owner)
         binput = {
 			'is_dataset_valid': 1, 'primary_ds_name': 'TkCosmics38T', 'physics_group_name': 'Tracker', 'global_tag': 'STARTUP31X_V3::All',
                         'processed_ds_name': 'Summer09-STARTUP31X_V3-v2', 'dataset': '/TkCosmics38T/Summer09-STARTUP31X_V3-v2/GEN-SIM-DIGI-RAW',
                         'dataset_type': 'PRODUCTION', 'xtcrosssection': 123, 'data_tier_name': 'GEN-SIM-DIGI-RAW',
-			'creation_date' : 1234, 'create_by' : 'anzar', "last_modification_date" : 1234, "last_modified_by" : "anzar",
+			'creation_date' : 1234, 'create_by' : '__MYSQL__', "last_modification_date" : 1234, "last_modified_by" : "anzar",
                         'output_configs' : [  {'app_name': 'Repacker', 'release_version': 'CMSSW_2_1_7',  'pset_hash': 'NO_PSET_HASH'}  ] 
 		}
 
@@ -64,7 +76,7 @@ class Test:
 	        """
 		This method is being used for testing AcquisitionEra insert biz
 		"""
-		bo = DBSAcquisitionEra(self.logger, self.dbi, "anzar")
+		bo = DBSAcquisitionEra(self.logger, self.dbi, self.owner)
 		binput = {
 			    'acquisition_era_name' : 'test_acq_001', 'creation_date' : 1234, 'create_by' : 'anzar'
 			}
@@ -74,7 +86,7 @@ class Test:
 	        """
 		This method is being used for testing AcquisitionEra insert biz
 		"""
-		bo = DBSProcessingEra(self.logger, self.dbi, "anzar")
+		bo = DBSProcessingEra(self.logger, self.dbi, self.owner)
 		binput = {
 		          'processing_version' : 'v001', 'creation_date' : 1234, 'create_by' : 'anzar', 'description' : 'anzar test description'
 		         }
@@ -85,7 +97,7 @@ class Test:
 		This method is used for testing Block insert Bi logic
 		"""
 		
-		bo = DBSBlock(self.logger, self.dbi, "anzar")
+		bo = DBSBlock(self.logger, self.dbi, self.owner)
 		binput = {'block_name': u'/QCD_BCtoMu_Pt20/Summer08_IDEAL_V9_v1/GEN-SIM-RAW#f930d82a-f72b-4f9e-8351-8a3cb0c43b79', 'file_count': u'100', 
 			    'origin_site': u'cmssrm.fnal.gov', 'last_modification_date': u'1263231733', 
 			    'create_by': u'/DC=org/DC=doegrids/OU=People/CN=Ajit Kumar Mohapatra 867118', 'block_size': u'228054411650', 
@@ -97,91 +109,30 @@ class Test:
 		This method tests the buisness login (and dao ) for the insertFiles() API
 		"""
 
-		bo = DBSFile(self.logger, self.dbi, "anzar")
-		binput = [{ 
-			    'file_type': 'EDM', 
-			    'logical_file_name': '/store110078/mc/Summer09/TkCosmics38T/GEN-SIM-DIGI-RAW/STARTUP31X_V3-v1/0010/66EE7132-FFB3-11-9D33-001E682F1FA68.root', 
-			    'file_size': '2824329131', 
-			    'last_modification_date': '1255099729', 
-			    'file_parent_list': [
-				    {"file_parent_lfn": "/store/data/Commissioning08/Monitor/RAW/v1/000/068/021/3678A458-E8A5-DD11-85D1-001617E30CC8.root"},
-				    {"file_parent_lfn": "/store/data/Commissioning08/Monitor/RAW/v1/000/068/021/04C7DFE0-E4A5-DD11-A766-000423D99B3E.root"},
-				    ], 
-			    'auto_cross_section': 0.0, 
-			    'md5': 'NOTSET', 
-			    'check_sum': '862355611', 
-			    'file_lumi_list': [{'lumi_section_num': 10018, 'run_num': 1}], 
-			    'file_output_config_list' : [  {'app_name': 'Repacker', 'release_version': 'CMSSW_2_1_7',  'pset_hash': 'NO_PSET_HASH'}  ],
-			    'adler32': 'NOTSET', 
-			    'event_count': '2041', 
-			    'creation_date' : 1234,
-			    'create_by': 'cmsprod@caraway.hep.wisc.edu', 
-			    'last_modified_by': '/DC=org/DC=doegrids/OU=People/CN=Ajit Kumar Mohapatra 867118', 
-			    'dataset': '/TkCosmics38T/Summer09-STARTUP31X_V3-v1/GEN-SIM-DIGI-RAW', 
-			    'block': '/TkCosmics38T/Summer09-STARTUP31X_V3-v1/GEN-SIM-DIGI-RAW#fc31bf9d-d44e-45a6-b87b-8fe6e2701062', 
-			    'is_file_valid': 1
-			}]
-		    
+		bo = DBSFile(self.logger, self.dbi, self.owner)
+    
 		binput = [
-			 {  'adler32': u'NOTSET', 'file_type': 'EDM', 
-			    'file_output_config_list': [
-							{'release_version': u'CMSSW_2_1_7', 'pset_hash': u'NO_PSET_HASH', 
-							    'app_name': u'cmsRun', 'output_module_label': u'Merged'}, 
-							{'release_version': u'CMSSW_2_1_7', 'pset_hash': u'76e303993a1c2f842159dbfeeed9a0dd', 
-							    'app_name': u'cmsRun', 'output_module_label': u'output'}
-							], 
-			    'dataset': u'/QCD_BCtoMu_Pt20/Summer08_IDEAL_V9_v1/GEN-SIM-RAW', 
-			    'file_size': u'2012211901', 'last_modification_date': u'1263231727', 'auto_cross_section': 0.0, 'md5': u'NOTSET', 
-			    'check_sum': u'1504266448', 
-			    'file_lumi_list': [
-					{'lumi_section_num': u'27414', 'run_num': u'1'}, 
-					{'lumi_section_num': u'26422', 'run_num': u'1'}, 
-					{'lumi_section_num': u'29838', 'run_num': u'1'}
-					], 
-			    'file_parent_list': [ ], 
-			    'event_count': u'1619',
-			    'creation_date' : 1234, 
-			    'create_by': u'/DC=org/DC=doegrids/OU=People/CN=Ajit Kumar Mohapatra 867118', 
-			    'last_modified_by': u'anzar@cmssrv49.fnal.gov', 
-			    'logical_file_name': u'/1store/mc/Summer08/QCD_BCtoMu_Pt20/GEN-SIM-RAW/IDEAL_V9_v1/0013/4A31C2E6-E2BE-DD11-9FE1-001E680F64A2.root', 
-			    'block': u'/QCD_BCtoMu_Pt20/Summer08_IDEAL_V9_v1/GEN-SIM-RAW#f930d82a-f72b-4f9e-8351-8a3cb0c43b79', 
-			    'is_file_valid': 1
-			},
-			    {  'adler32': u'NOTSET', 'file_type': 'EDM', 
-			    'file_output_config_list': [
-							{'release_version': u'CMSSW_2_1_7', 'pset_hash': u'NO_PSET_HASH', 
-							    'app_name': u'cmsRun', 'output_module_label': u'Merged'}, 
-							{'release_version': u'CMSSW_2_1_7', 'pset_hash': u'76e303993a1c2f842159dbfeeed9a0dd', 
-							    'app_name': u'cmsRun', 'output_module_label': u'output'}
-							], 
-			    'dataset': u'/QCD_BCtoMu_Pt20/Summer08_IDEAL_V9_v1/GEN-SIM-RAW', 
-			    'file_size': u'2012211901', 'last_modification_date': u'1263231727', 'auto_cross_section': 0.0, 'md5': u'NOTSET', 
-			    'check_sum': u'1504266448', 
-			    'file_lumi_list': [
-					{'lumi_section_num': u'27414', 'run_num': u'1'}, 
-					{'lumi_section_num': u'26422', 'run_num': u'1'}, 
-					{'lumi_section_num': u'29838', 'run_num': u'1'}
-					], 
-			    'file_parent_list': [ ], 
-			    'event_count': u'1619',
-			    'creation_date' : 1234, 
-			    'create_by': u'/DC=org/DC=doegrids/OU=People/CN=Ajit Kumar Mohapatra 867118', 
-			    'last_modified_by': u'anzar@cmssrv49.fnal.gov', 
-			    'logical_file_name': u'/2store/mc/Summer08/QCD_BCtoMu_Pt20/GEN-SIM-RAW/IDEAL_V9_v1/0013/4A31C2E6-E2BE-DD11-9FE1-001E680F64A2.root', 
-			    'block': u'/QCD_BCtoMu_Pt20/Summer08_IDEAL_V9_v1/GEN-SIM-RAW#f930d82a-f72b-4f9e-8351-8a3cb0c43b79', 
-			    'is_file_valid': 1
-			},
-			
+		{'adler32': u'NOTSET', 'file_type': 'EDM', 'file_output_config_list': [{'release_version': 'CMSSW_1_2_3', 'pset_hash': '76e303993a1c2f842159dbfeeed9a0dd', 'app_name': 
+		'cmsRun', 'output_module_label': 'Merged'}], 'dataset': '/unittest_web_primary_ds_name_684/unittest_web_dataset_684/GEN-SIM-RAW', 
+		'file_size': u'2012211901', 'auto_cross_section': 0.0, 'check_sum': u'1504266448', 
+		'file_lumi_list': [{'lumi_section_num': u'27414', 'run_num': u'1'}, 
+				    {'lumi_section_num': u'26422', 'run_num': u'1'}, 
+				    {'lumi_section_num': u'29838', 'run_num': u'1'}], 
+				    'file_parent_list': [], 'event_count': u'1619', 'logical_file_name': 
+					'/store/mc/parent_684/0.root', 
+		'block': '/unittest_web_primary_ds_name_684/unittest_web_dataset_684/GEN-SIM-RAW#684',
+		'creation_date' : 1234, 'create_by' : 'anzar', "last_modification_date" : 1234, "last_modified_by" : "anzar",
+		}
 		]
 		bo.insertFile(binput)
+	
 
 test=Test()
+#test.testPrimary()
 #test.testOutputConfig()
-import pdb
-pdb.set_trace()
-test.testDatasetInsert()
+#test.testDatasetInsert()
 #test.testAcquisitionEra()
 #test.testProcessingEra()
 #test.testBlock()
-#test.testFiles()
+test.testFiles()
     
--- Server/Python/tests/SimpleDAOTest.py
+++ Server/Python/tests/SimpleDAOTest.py
@@ -3,12 +3,14 @@ from WMCore.Database.DBFactory import DBFactory
 
 from dbs.dao.Oracle.PrimaryDataset.Insert import Insert as PriInsert 
 from dbs.dao.Oracle.FileParentBlock import List as FPBList
+from dbs.dao.Oracle.Block import List as LBlock
 
 class Test:
 
     def __init__(self):
-        #url = "oracle://user:password@host:port/sid"
+        url = "oracle://user:password@host:port/sid"
 	url="oracle://anzar:anzar_cms2009@uscmsdb03.fnal.gov:1521/cmscald"
+	url="mysql://dbs3:dbs3_pass@cmssrv49.fnal.gov:3306/CMS_DBS3"
         self.logger = logging.getLogger("dbs test logger")
         self.dbi = DBFactory(self.logger, url).connect()
 
@@ -17,7 +19,7 @@ class Test:
 	This method is being used for testing primary datasets's insert DAO
 	"""
 
-	dao = PriInsert(self.logger, self.dbi, "anzar")
+	dao = PriInsert(self.logger, self.dbi, "")
 	dinput = { "JUNK" : "XYZ" , "primary_ds_id":1001, "primary_ds_name": "TkCosmics38T", "primary_ds_type_id": 1, "creation_date": 1234, "create_by":"anzar"}
         dao.execute(dinput)
 
@@ -29,7 +31,11 @@ class Test:
 	ret=dao.execute(file_id=13841)
 	print ret
 
+    def testListBlock(self):
+	dao = LBlock.List(self.logger, self.dbi, "__MYSQL__")
+	ret=dao.execute(block_name="shatongra*")
     
 test=Test()
 #test.testPrimaryInsert()
-test.testFileParentBlock()
+#test.testFileParentBlock()
+test.testListBlock()
