fafe0e8c849038a9aad3e08f2ee48fb5c646be25
yuyi <yuyi>
2010-09-16 01:48:35 +0000

    bug fixed
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10191 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/PhysicsGroup/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/PhysicsGroup/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for PhysicsGroups table """ 
 
-__revision__ = "$Revision: 1.6 $"
-__version__  = "$Id: Insert.py,v 1.6 2010/06/23 21:21:25 afaq Exp $ "
+__revision__ = "$Revision: 1.7 $"
+__version__  = "$Id: Insert.py,v 1.7 2010/08/09 18:22:07 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -37,7 +37,7 @@ class Insert(DBFormatter):
     def execute( self, conn, physics_groupsObj, transaction=False ):
 	if not conn:
 	    raise Exception("dbs/dao/Oracle/PhysicsGroup/Insert expects db connection from upper layer.")
-	result = self.dbi.processData(self.sql, binds, conn, transaction)
+	result = self.dbi.processData(self.sql, physics_groupsObj, conn, transaction)
 	return
 
 
