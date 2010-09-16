7d7f7309e2264f263cfd2bcaf4fcf8e4bb82e994
yuyi <yuyi>
2010-09-16 00:54:07 +0000

    db connection management change
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9868 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/DatasetOutputMod_config/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/DatasetOutputMod_config/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for DatasetOutputMod_configs table """ 
 
-__revision__ = "$Revision: 1.5 $"
-__version__  = "$Id: Insert.py,v 1.5 2010/02/11 22:51:59 afaq Exp $ "
+__revision__ = "$Revision: 1.6 $"
+__version__  = "$Id: Insert.py,v 1.6 2010/03/05 16:32:48 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 from sqlalchemy import exceptions
@@ -16,7 +16,9 @@ class Insert(DBFormatter):
             self.sql = """INSERT INTO %sDATASET_OUTPUT_MOD_CONFIGS ( DS_OUTPUT_MOD_CONF_ID, DATASET_ID, OUTPUT_MOD_CONFIG_ID) 
 				VALUES (:ds_output_mod_conf_id, :dataset_id, :output_mod_config_id)""" % (self.owner)
 
-    def execute( self, dataset_output_mod_configsObj, conn=None, transaction=False ):
+    def execute( self, conn, dataset_output_mod_configsObj, transaction=False ):
+	if not conn:
+	    raise Exception("dbs/dao/Oracle/DatasetOutputMod_config/Insert expects db connection from up layer.")
 	try:
             result = self.dbi.processData(self.sql, dataset_output_mod_configsObj, conn, transaction)
 	except Exception, ex:
