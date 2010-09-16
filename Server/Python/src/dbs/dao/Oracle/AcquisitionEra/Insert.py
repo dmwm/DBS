4d31c32b8ed5e01d0161a4955e45935921b701de
yuyi <yuyi>
2010-09-16 00:56:41 +0000

    *** empty log message ***
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9883 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/AcquisitionEra/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/AcquisitionEra/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for ACQUISITION_ERAS table """
 
-__revision__ = "$Revision: 1.7 $"
-__version__  = "$Id: Insert.py,v 1.7 2010/03/05 14:57:52 yuyi Exp $ "
+__revision__ = "$Revision: 1.8 $"
+__version__  = "$Id: Insert.py,v 1.8 2010/03/08 19:35:51 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 from sqlalchemy import exceptions
@@ -10,7 +10,7 @@ from dbs.dao.Oracle.InsertTable.Insert import InsertSingle
 
 class Insert(InsertSingle):
     """ ACQUISITION_ERAS Insert DAO Class"""
-    def execute(self, daoinput, conn, transaction = False):
+    def execute(self, conn, daoinput, transaction = False):
 	if not conn:
 	    raise Exception("dbs/dao/Oarcle?AcquisitionEra/Insert expects db connection from up layer.")
         try:
