a7139729cf293eec62650c7c5e38ab1ae47a7311
yuyi <yuyi>
2010-09-16 00:52:24 +0000

    add serarch by dataset
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9858 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/PrimaryDSType/List.py
+++ Server/Python/src/dbs/dao/Oracle/PrimaryDSType/List.py
@@ -2,8 +2,8 @@
 """
 This module provides PrimaryDSType.List data access object.
 """
-__revision__ = "$Id: List.py,v 1.7 2010/02/11 18:03:28 afaq Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: List.py,v 1.8 2010/03/04 20:13:50 yuyi Exp $"
+__version__ = "$Revision: 1.8 $"
 
 
 from WMCore.Database.DBFormatter import DBFormatter
@@ -19,19 +19,29 @@ class List(DBFormatter):
 	self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
         self.sql = \
 """
-SELECT PT.PRIMARY_DS_TYPE_ID, PT.PRIMARY_DS_TYPE
-FROM %sPRIMARY_DS_TYPES PT 
+SELECT PDT.PRIMARY_DS_TYPE_ID, PDT.PRIMARY_DS_TYPE AS DATA_TYPE
+FROM %sPRIMARY_DS_TYPES PDT 
 """ % (self.owner)
 
-    def execute(self, pattern = "", conn = None, transaction = False):
+    def execute(self, conn, dsType = "", dataset="", transaction = False):
         """
-        Lists all primary dataset types if pattern is not provided.
+        Lists all primary dataset types if no user input is provided.
         """
+	
         sql = self.sql
-        if pattern == "":
-            result = self.dbi.processData(sql, conn=conn, transaction=transaction)
-        else:
+        if not dsType  and not dataset:
+            result = self.dbi.processData(sql, conn, transaction=transaction)
+        if dsType and not dataset:
             sql += "WHERE PT.PRIMARY_DS_TYPE = :primdstype" 
-            binds = {"primdstype":pattern}
-            result = self.dbi.processData(sql, binds, conn, transaction)
+            binds = {"primdstype":dsType}
+            result = self.dbi.processData(sql, binds, conn, transaction=transaction)
+	if dataset and not dsType:
+	    op = ("=", "like")["%" in dataset]
+	    sql += "JOIN PRIMARY_DATASETS PDS on PDS.PRIMARY_DS_ID = PDT.PRIMARY_DS_TYPE_ID \
+	            JOIN DATASETS DS ON DS.PRIMARY_DS_ID = PDS.PRIMARY_DS_ID \"\
+	            WHERE DS.DATASET %s :dataset;"  %op
+	    binds={"dataset":dataset}
+	    result = self.dbi.processData(sql, binds, conn, transaction=transaction)
+	else:
+	    raise Exception("Wrong user input for dao Primary_DS_TYPE List.")
         return self.formatDict(result)
