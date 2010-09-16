470ba61a6f42f1d5410d317f6e2778d6217787f1
yuyi <yuyi>
2010-09-16 01:51:31 +0000

    added list parentage by block id to work around the problem that framework does not support where clause with key word in
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10208 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/FileParent/List.py
+++ Server/Python/src/dbs/dao/Oracle/FileParent/List.py
@@ -2,8 +2,8 @@
 """
 This module provides FileParent.List data access object.
 """
-__revision__ = "$Id: List.py,v 1.8 2010/08/05 16:15:15 yuyi Exp $"
-__version__ = "$Revision: 1.8 $"
+__revision__ = "$Id: List.py,v 1.9 2010/08/13 14:04:22 yuyi Exp $"
+__version__ = "$Revision: 1.9 $"
 
 
 from WMCore.Database.DBFormatter import DBFormatter
@@ -28,15 +28,21 @@ JOIN %sFILE_PARENTS FP ON FP.PARENT_FILE_ID = PF.FILE_ID
 JOIN %sFILES F ON  F.FILE_ID = FP.THIS_FILE_ID 
 """ % ((self.owner,)*3)
 
-    def execute(self, conn, logical_file_name, transaction=False):
+    def execute(self, conn, logical_file_name='', block_id=0, transaction=False):
         """
-        Lists all primary datasets if pattern is not provided.
+        return {} if condition is not provided.
         """
         if not conn:
             raise Exception("dbs/dao/Oracle/FileParent/List expects db connection from upper layer.")
         sql = self.sql
-        sql += "WHERE F.LOGICAL_FILE_NAME = :logical_file_name"
-        binds = {"logical_file_name":logical_file_name}
+	if logical_file_name:
+	    sql += "WHERE F.LOGICAL_FILE_NAME = :logical_file_name"
+	    binds = {"logical_file_name":logical_file_name}
+	elif block_id != 0:
+	    sql += "WHERE F.BLOCK_ID = :block_id"
+	    binds ={'block_id':block_id}
+	else:
+	    return{}
 	cursors = self.dbi.processData(sql, binds, conn, transaction=transaction, returnCursor=True)
 	if len(cursors) != 1:
 	    raise Exception("File Parents does not exist.")
