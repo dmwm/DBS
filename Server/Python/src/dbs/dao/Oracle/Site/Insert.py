dea2eec0800680371b8c2f73f7b259fa9c4745eb
yuyi <yuyi>
2010-09-16 00:16:40 +0000

    rewrote it
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9647 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Site/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/Site/Insert.py
@@ -1,40 +1,19 @@
 #!/usr/bin/env python
 """ DAO Object for Sites table """ 
 
-__revision__ = "$Revision: 1.2 $"
-__version__  = "$Id: Insert.py,v 1.2 2009/10/20 02:19:23 afaq Exp $ "
+__revision__ = "$Revision: 1.3 $"
+__version__  = "$Id: Insert.py,v 1.3 2009/12/23 20:45:47 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
+from sqlalchemy import exceptions
+from dbs.dao.Oracle.InsertTable.Insert import InsertSingle
 
-class Insert(DBFormatter):
-
-    def __init__(self, logger, dbi):
-            DBFormatter.__init__(self, logger, dbi)
-            self.owner = "%s." % self.dbi.engine.url.username
-
-            self.sql = """INSERT INTO %sSITES ( SITE_ID, SITE_NAME) VALUES (:siteid, :sitename) % (self.owner) ;"""
-
-    def getBinds_delme( self, sitesObj ):
-            binds = {}
-            if type(sitesObj) == type ('object'):
-            	binds = {
-			'siteid' : sitesObj['siteid'],
-			'sitename' : sitesObj['sitename'],
-                 }
-
-            elif type(sitesObj) == type([]):
-               binds = []
-               for item in sitesObj:
-                   binds.append({
- 	                'siteid' : item['siteid'],
- 	                'sitename' : item['sitename'],
- 	                })
-               return binds
-
-
-    def execute( self, sitesObj, conn=None, transaction=False ):
-            ##binds = self.getBinds( sitesObj )
-            result = self.dbi.processData(self.sql, binds, conn, transaction)
-            return
+class Insert(InsertSingle):
+    """ DAO for Insert Site """
+    def execute( self, daoinput, conn=None, transaction=False ):
+	try:
+            self.executeSingle(daoinput, "SITES", conn = None, transaction = False)
+        except Exception:
+            raise
 
 
