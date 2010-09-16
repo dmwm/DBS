52a6c2b93e9837a11510f53afaeaabd4c6b061b5
yuyi <yuyi>
2010-09-16 00:13:43 +0000

    using correct binding names
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9628 4525493e-7705-40b1-a816-d608a930855b

--- Client/utils/insertprimary.py
+++ Client/utils/insertprimary.py
@@ -8,7 +8,10 @@ url="http://cmssrv48.fnal.gov:8989/dbs3"
 # API Object    
 dbs3api = DbsApi(url=url)
 #print dbs3api.listPrimaryDatasets()
-print dbs3api.insertPrimaryDataset({"primary_ds_type": "test", "primary_ds_name": "TkCosmics38T_ANZAR00r65"})
+try:
+    dbs3api.insertPrimaryDataset({"primary_ds_type": "test", "primary_ds_name": "yuyi3"})
+except Exception, e:
+    print e
 # Is service Alive
 """
 print dbs3api.ping()
