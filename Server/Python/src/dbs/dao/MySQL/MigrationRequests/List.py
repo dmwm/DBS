f427b2518117ece88d3b1aa9b382ba821f6ad258
akhukhun <akhukhun>
2010-09-16 01:18:10 +0000

    initial check-in
    From: akhukhun <akhukhun>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10006 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/MySQL/MigrationRequests/List.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+This module provides MigrationRequests.List data access object.
+"""
+__revision__ = "$Id: List.py,v 1.1 2010/04/22 08:05:17 akhukhun Exp $"
+__version__ = "$Revision: 1.1 $"
+
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class List(DBFormatter):
+    """
+    MigrationRequest List DAO class.
+    """
+    def __init__(self, logger, dbi, owner=""):
+        """
+        Add schema owner and sql.
+        """
+        DBFormatter.__init__(self, logger, dbi)
+        self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
+        self.sql = \
+"""
+SELECT MR.MIGRATION_ID, MR.MIGRATION_URL, 
+       MR.MIGRATION_DATASET, MR.MIGRATION_STATUS
+FROM %sMIGRATION_REQUESTS MR
+""" % (self.owner)
+
+    def execute(self, conn, migration_dataset, transaction=False):
+        """
+        Lists all primary datasets if pattern is not provided.
+        """
+        sql = self.sql
+        binds = {}
+        op = ("=", "like")["%" in migration_dataset]
+        sql += "WHERE MR.MIGRATION_DATASET %s :migration_dataset" % op 
+        binds = {'migration_dataset':migration_dataset}
+	cursors = self.dbi.processData(sql, binds, conn, transaction, returnCursor=True)
+	assert len(cursors) == 1
+        result = self.formatCursor(cursors[0])
+        return result
---
+++ Server/Python/src/dbs/dao/MySQL/MigrationRequests/Update.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+This module provides Migration.Update data access object.
+"""
+__revision__ = "$Id: Update.py,v 1.1 2010/04/22 08:05:21 akhukhun Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from WMCore.Database.DBFormatter import DBFormatter
+class Update(DBFormatter):
+    """
+    Migration Update DAO class.
+    """
+    def __init__(self, logger, dbi, owner):
+        """
+        Add schema owner and sql.
+        """
+        DBFormatter.__init__(self, logger, dbi)
+	self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
+        self.sql = \
+"""UPDATE %sMIGRATION_REQUESTS 
+SET MIGRATION_STATUS=:migration_status 
+WHERE MIGRATION_DATASET=:migration_dataset""" %  self.owner 
+        
+    def execute(self, conn, daoinput, transaction = False):
+        """
+	    daoinput keys:
+	    migration_status, migration_id
+        """	
+        result = self.dbi.processData(self.sql, daoinput, conn, transaction)
