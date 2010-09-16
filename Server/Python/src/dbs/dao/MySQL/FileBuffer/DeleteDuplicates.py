703c70a827f4eb3ac986cc65221d71d697dc3609
afaq <afaq>
2010-09-16 01:27:09 +0000

    mysql DAOs for finding duplicates and remiving them from the buffer
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10062 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/MySQL/FileBuffer/DeleteDuplicates.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides FileBuffer.DeleteDuplicates data access object.
+"""
+__revision__ = "$Id: DeleteDuplicates.py,v 1.1 2010/05/27 19:38:20 afaq Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from dbs.dao.Oracle.FileBuffer.DeleteDuplicates import DeleteDuplicates as OraFileBufferDeleteDuplicates
+
+class DeleteDuplicates(OraFileBufferDeleteDuplicates):
+                pass
+
---
+++ Server/Python/src/dbs/dao/MySQL/FileBuffer/FindDuplicates.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides FileBuffer.FindDuplicates data access object.
+"""
+__revision__ = "$Id: FindDuplicates.py,v 1.1 2010/05/27 19:38:20 afaq Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from dbs.dao.Oracle.FileBuffer.FindDuplicates import FindDuplicates as OraFileBufferFindDuplicates
+
+class FindDuplicates(OraFileBufferFindDuplicates):
+                pass
+
