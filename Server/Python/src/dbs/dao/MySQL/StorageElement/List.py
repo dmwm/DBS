dc0a4a6d1912dca876f9a55fdcef4ff5a295bef3
afaq <afaq>
2010-09-16 00:49:53 +0000

    mysql dao for storagelements
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9843 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/MySQL/StorageElement/List.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides StorageElement.List data access object.
+"""
+__revision__ = "$Id: List.py,v 1.1 2010/03/02 20:23:36 afaq Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from dbs.dao.Oracle.StorageElement.List import List as OraStorageElementList
+
+class List(OraStorageElementList):
+        pass
+
---
+++ Server/Python/src/dbs/dao/MySQL/StorageElement/ListBlockSE.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+This module provides StorageElement.ListBlockSE data access object.
+"""
+
+__revision__ = "$Id: ListBlockSE.py,v 1.1 2010/03/02 20:23:36 afaq Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from dbs.dao.Oracle.StorageElement.ListBlockSE import List as OraStorageElementListBlockSE
+
+class ListBlockSE(OraStorageElementListBlockSE):
+        pass
+
