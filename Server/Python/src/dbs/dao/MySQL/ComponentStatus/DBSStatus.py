99ea2033a164bf489c72a5462ea1f80f70d7e535
afaq <afaq>
2010-09-16 01:32:07 +0000

    dao to get status of dbs
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10093 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/MySQL/ComponentStatus/DBSStatus.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" DAO Object for ComponentStatus table """ 
+
+__revision__ = "$Revision: 1.1 $"
+__version__  = "$Id: DBSStatus.py,v 1.1 2010/06/14 15:10:58 afaq Exp $ "
+
+from dbs.dao.Oracle.ComponentStatus.DBSStatus import DBSStatus as OraDBSStatus
+
+class DBSStatus(OraDBSStatus):
+            pass
+	
