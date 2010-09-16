a3616a214fc4808de6eb8d0476f2bee374a50a62
yuyi <yuyi>
2010-09-16 01:00:48 +0000

    using singleTable insert class
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9907 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Dataset/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/Dataset/Insert.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 """ DAO Object for Datasets table """ 
 
-__revision__ = "$Revision: 1.12 $"
-__version__  = "$Id: Insert.py,v 1.12 2010/03/05 16:04:02 yuyi Exp $ "
+__revision__ = "$Revision: 1.13 $"
+__version__  = "$Id: Insert.py,v 1.13 2010/03/15 14:49:45 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 from sqlalchemy import exceptions
+from dbs.dao.Oracle.InsertTable.Insert import InsertSingle
 
-class Insert(DBFormatter):
+class Insert(InsertSingle):
     """ Dataset Insert DAO Class"""
     
     def __init__(self, logger, dbi, owner):
@@ -15,27 +16,27 @@ class Insert(DBFormatter):
         self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
         self.logger = logger
 
-        self.sql = \
-	"""
-	INSERT INTO %sDATASETS 
-		(DATASET_ID, DATASET, IS_DATASET_VALID, 
-			PRIMARY_DS_ID, PROCESSED_DS_ID, DATA_TIER_ID, DATASET_TYPE_ID, 
-			PHYSICS_GROUP_ID, XTCROSSSECTION, GLOBAL_TAG, CREATION_DATE, CREATE_BY, 
-			LAST_MODIFICATION_DATE, LAST_MODIFIED_BY) 
-			VALUES
-			(:dataset_id, :dataset, :is_dataset_valid, 
-			:primary_ds_id, :processed_ds_id, :data_tier_id, :dataset_type_id, 
-			:physics_group_id, :xtcrosssection, :global_tag, :creation_date, :create_by, 
-			:last_modification_date, :last_modified_by) """ % self.owner
-
-    def processInput(self, daoinput):
-                #valid_keys = self.sql.split("VALUES")[1].split('(')[1].split(')')[0].replace(",","").strip().split(":")
-                valid_keys = self.sql.split("VALUES")[1].split('(')[1].split(')')[0].replace(',','').replace(':','').strip().split()
-                for akey in daoinput.keys():
-                        if akey not in valid_keys:
-                                del daoinput[akey]
-				self.logger.warning("DROPPING Key: "+akey)
-                return daoinput
+#        self.sql = \
+#	"""
+#	INSERT INTO %sDATASETS 
+#		(DATASET_ID, DATASET, IS_DATASET_VALID, 
+#			PRIMARY_DS_ID, PROCESSED_DS_ID, DATA_TIER_ID, DATASET_TYPE_ID, 
+#			PHYSICS_GROUP_ID, XTCROSSSECTION, GLOBAL_TAG, CREATION_DATE, CREATE_BY, 
+#			LAST_MODIFICATION_DATE, LAST_MODIFIED_BY) 
+#			VALUES
+#			(:dataset_id, :dataset, :is_dataset_valid, 
+#			:primary_ds_id, :processed_ds_id, :data_tier_id, :dataset_type_id, 
+#			:physics_group_id, :xtcrosssection, :global_tag, :creation_date, :create_by, 
+#			:last_modification_date, :last_modified_by) """ % self.owner
+#
+#    def processInput(self, daoinput):
+#                #valid_keys = self.sql.split("VALUES")[1].split('(')[1].split(')')[0].replace(",","").strip().split(":")
+#                valid_keys = self.sql.split("VALUES")[1].split('(')[1].split(')')[0].replace(',','').replace(':','').strip().split()
+#                for akey in daoinput.keys():
+#                        if akey not in valid_keys:
+#                                del daoinput[akey]
+#				self.logger.warning("DROPPING Key: "+akey)
+#                return daoinput
 
     def execute(self, conn, daoinput, transaction = False):
 
@@ -48,5 +49,10 @@ class Insert(DBFormatter):
 	"""
 	if not conn:
 	    raise Exception("dbs/dao/Oracle/Dataset/Insert expects db connection from up layer.")
-	daoinput=self.processInput(daoinput)
-        self.dbi.processData(self.sql, daoinput, conn, transaction)
+	#daoinput=self.processInput(daoinput)
+        #self.dbi.processData(self.sql, daoinput, conn, transaction)
+	try:
+	    self.executeSingle(conn, daoinput, "DATASETS", transaction)
+	except Exception:
+	    raise
+					
