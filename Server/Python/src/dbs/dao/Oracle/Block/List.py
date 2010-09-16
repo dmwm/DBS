f00a101166786e191ebcacd12bc86e624c5838ed
akhukhun <akhukhun>
2010-09-16 01:43:31 +0000

    SELECT DISTINCT when run_num parameter is provided
    From: akhukhun <akhukhun>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10162 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Block/List.py
+++ Server/Python/src/dbs/dao/Oracle/Block/List.py
@@ -2,8 +2,8 @@
 """
 This module provides Block.List data access object.
 """
-__revision__ = "$Id: List.py,v 1.22 2010/06/23 21:21:18 afaq Exp $"
-__version__ = "$Revision: 1.22 $"
+__revision__ = "$Id: List.py,v 1.23 2010/07/30 12:00:55 akhukhun Exp $"
+__version__ = "$Revision: 1.23 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 from WMCore.Database.MySQLCore import  MySQLInterface
@@ -18,7 +18,7 @@ class List(DBFormatter):
 	"""
 	DBFormatter.__init__(self, logger, dbi)
         self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
-        self.sql1 = \
+        self.sql = \
     """
 SELECT B.BLOCK_ID, B.BLOCK_NAME, B.OPEN_FOR_WRITING, 
         B.BLOCK_SIZE, B.FILE_COUNT,
@@ -35,79 +35,84 @@ JOIN %sDATASETS DS ON DS.DATASET_ID = B.DATASET_ID
 	"""	
 	if not conn:
 	    raise Exception("dbs/dao/Oarcle/Block/List expects db connection from upper layer.")
-	sql1 = self.sql1
+	sql = self.sql
 	binds = {}
+
 	if logical_file_name and logical_file_name != "%":
 	    op = ("=", "like")["%" in logical_file_name] 
-	    sql1 += " JOIN %sFILES FL ON FL.BLOCK_ID = B.BLOCK_ID" %(self.owner)
+	    sql += " JOIN %sFILES FL ON FL.BLOCK_ID = B.BLOCK_ID" %(self.owner)
 	    if run_num and run_num !=-1:
-		sql1 += " JOIN %s FILE_LUMIS FLM on FLM.FILE_ID = FL.FILE_ID" %(self.owner)
-	    sql1 += " WHERE LOGICAL_FILE_NAME %s :logical_file_name" % op
+		sql += " JOIN %s FILE_LUMIS FLM on FLM.FILE_ID = FL.FILE_ID" %(self.owner)
+	    sql += " WHERE LOGICAL_FILE_NAME %s :logical_file_name" % op
 	    binds.update( logical_file_name = logical_file_name)
 	    if run_num and run_num !=-1:
-		sql1 += " AND RUN_NUM = :run_num"
+		sql += " AND RUN_NUM = :run_num"
 		binds.update(run_num = run_num)
 	    if  block_name and  block_name !="%":
 		op = ("=", "like")["%" in block_name]
-		sql1 += " AND B.BLOCK_NAME %s :block_name" % op
+		sql += " AND B.BLOCK_NAME %s :block_name" % op
 		binds.update({"block_name":block_name})
 		if dataset and dataset !="%": 
 		    op = ("=", "like")["%" in dataset]
-		    sql1 += " AND DS.DATASET %s :dataset" %op
+		    sql += " AND DS.DATASET %s :dataset" %op
 		    binds.update(dataset=dataset)
 		if origin_site_name and  origin_site_name != "%":
 		    op = ("=", "like")["%" in origin_site_name]
-		    sql1 += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
+		    sql += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
 		    binds.update(origin_site_name = origin_site_name)	
 	    elif dataset and dataset !="%": 
 		op = ("=", "like")["%" in dataset]
-		sql1 += " AND DS.DATASET %s :dataset" %op
+		sql += " AND DS.DATASET %s :dataset" %op
 		binds.update(dataset=dataset)
 		if origin_site_name and  origin_site_name != "%":
 		    op = ("=", "like")["%" in origin_site_name]
-		    sql1 += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
+		    sql += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
 		    binds.update(origin_site_name = origin_site_name)
 	    elif origin_site_name and  origin_site_name != "%": 
 		op = ("=", "like")["%" in origin_site_name] 
-		sql1 += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
+		sql += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
 		binds.update(origin_site_name = origin_site_name)
 	elif block_name and  block_name !="%":
 	    if run_num and run_num !=-1:
-		sql1 += """ JOIN %sFILES FL ON FL.BLOCK_ID = B.BLOCK_ID
+		sql += """ JOIN %sFILES FL ON FL.BLOCK_ID = B.BLOCK_ID
 		            JOIN %s FILE_LUMIS FLM on FLM.FILE_ID = FL.FILE_ID
 			""" %((self.owner,)*2)
 	    op = ("=", "like")["%" in block_name]
-	    sql1 += " WHERE B.BLOCK_NAME %s :block_name" % op
+	    sql += " WHERE B.BLOCK_NAME %s :block_name" % op
 	    binds.update({"block_name":block_name}) 
 	    if run_num and run_num !=-1:
-                sql1 += " AND RUN_NUM = :run_num"
+                sql += " AND RUN_NUM = :run_num"
                 binds.update(run_num = run_num)
 	    if dataset and dataset !="%": 
 		op = ("=", "like")["%" in dataset]
-		sql1 += " AND DS.DATASET %s :dataset" %op
+		sql += " AND DS.DATASET %s :dataset" %op
 		binds.update(dataset=dataset)
 	    if origin_site_name and  origin_site_name != "%":
 		op = ("=", "like")["%" in origin_site_name]
-		sql1 += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
+		sql += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
 		binds.update(origin_site_name = origin_site_name)	
 	elif dataset and dataset !="%":
 	    if run_num and run_num !=-1:
-                sql1 += """ JOIN %sFILES FL ON FL.BLOCK_ID = B.BLOCK_ID 
+                sql += """ JOIN %sFILES FL ON FL.BLOCK_ID = B.BLOCK_ID 
 		            JOIN %s FILE_LUMIS FLM on FLM.FILE_ID = FL.FILE_ID
 			""" %((self.owner,)*2)
 	    op = ("=", "like")["%" in dataset] 
-	    sql1 += " WHERE DS.DATASET %s :dataset" %op
+	    sql += " WHERE DS.DATASET %s :dataset" %op
 	    binds.update(dataset=dataset) 
 	    if run_num and run_num !=-1:
-		sql1 += " AND RUN_NUM = :run_num"
+		sql += " AND RUN_NUM = :run_num"
                 binds.update(run_num = run_num)
 	    if origin_site_name and  origin_site_name != "%": 
 		op = ("=", "like")["%" in origin_site_name] 
-		sql1 += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
+		sql += " AND B.ORIGIN_SITE_NAME %s :origin_site_name" %op
 		binds.update(origin_site_name = origin_site_name)
-	#print "sql1=%s" %sql1
+
+	if run_num and run_num !=-1:
+	    sql = sql.replace("SELECT", "SELECT DISTINCT")
+
+	#print "sql=%s" %sql
 	#print "binds=%s" %binds
-	cursors = self.dbi.processData(sql1, binds, conn, transaction, returnCursor=True)
+	cursors = self.dbi.processData(sql, binds, conn, transaction, returnCursor=True)
 	assert len(cursors) == 1, "block does not exist"
 	result = self.formatCursor(cursors[0])
 	return result
