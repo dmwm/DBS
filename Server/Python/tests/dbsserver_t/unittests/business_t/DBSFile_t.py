04f505822606d1669608ec8a8462b8b0e8a75a81
dsr <dsr>
2010-09-16 00:36:25 +0000

    update business logic tests to match server changes
    From: dsr <dsr>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9767 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/tests/dbsserver_t/unittests/business_t/DBSFile_t.py
+++ Server/Python/tests/dbsserver_t/unittests/business_t/DBSFile_t.py
@@ -2,8 +2,8 @@
 business unittests
 """
 
-__revision__ = "$Id: DBSFile_t.py,v 1.1 2010/01/01 19:54:37 akhukhun Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: DBSFile_t.py,v 1.2 2010/01/28 00:01:37 dsr Exp $"
+__version__ = "$Revision: 1.2 $"
 
 import os
 import unittest
@@ -24,19 +24,19 @@ class DBSFile_t(unittest.TestCase):
         dbi = DBFactory(self.logger, dburl).connect()
         bo = DBSFile(self.logger, dbi, dbowner)
 
-        result = bo.listFiles('%')
+        result = bo.listFiles('NoSuchFile%')
         self.assertTrue(type(result) == list)
         self.assertEqual(len(result), 0)
         
-        result = bo.listFiles(dataset='%')
+        result = bo.listFiles(dataset='NoSuchDataset%')
         self.assertTrue(type(result) == list)
         self.assertEqual(len(result), 0)
         
-        result = bo.listFiles(block_name='%')
+        result = bo.listFiles(block_name='NoSuchBlock%')
         self.assertTrue(type(result) == list)
         self.assertEqual(len(result), 0)
         
-        result = bo.listFiles(logical_file_name='%')
+        result = bo.listFiles(logical_file_name='NoSuchLFN%')
         self.assertTrue(type(result) == list)
         self.assertEqual(len(result), 0)
         
