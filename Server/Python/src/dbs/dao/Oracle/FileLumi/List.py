5f91ead35a26b77dd3bf7a91b98ca7d70ace99f8
yuyi <yuyi>
2010-09-16 01:47:02 +0000

    reduced joins
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10182 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/FileLumi/List.py
+++ Server/Python/src/dbs/dao/Oracle/FileLumi/List.py
@@ -2,8 +2,8 @@
 """
 This module provides FileLumi.List data access object.
 """
-__revision__ = "$Id: List.py,v 1.6 2010/06/23 21:21:23 afaq Exp $"
-__version__ = "$Revision: 1.6 $"
+__revision__ = "$Id: List.py,v 1.7 2010/08/05 16:08:24 yuyi Exp $"
+__version__ = "$Revision: 1.7 $"
 
 
 from WMCore.Database.DBFormatter import DBFormatter
@@ -20,32 +20,33 @@ class List(DBFormatter):
         self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
         self.sql = \
 """
-SELECT FL.FILE_LUMI_ID, FL.RUN_NUM, FL.LUMI_SECTION_NUM,
-       F.LOGICAL_FILE_NAME
-FROM %sFILE_LUMIS FL
-JOIN %sFILES F ON F.FILE_ID = FL.FILE_ID
-JOIN %sBLOCKS B ON B.BLOCK_ID = F.BLOCK_ID
-""" % ((self.owner,)*3)
+SELECT FL.RUN_NUM as RUN_NUM, FL.LUMI_SECTION_NUM as LUMI_SECTION_NUM
+"""
 
     def execute(self, conn, logical_file_name='', block_name=''):
         """
-        Lists all primary datasets if pattern is not provided.
+        Lists lumi section numbers with in a file or a block.
         """
         if not conn:
             raise Exception("dbs/dao/Oracle/FileLumi/List expects db connection from upper layer.")
             
-        sql = self.sql
+        #sql = self.sql
         
         if logical_file_name:
-            sql+="WHERE F.LOGICAL_FILE_NAME = :logical_file_name"
-            binds = {'logical_file_name': logical_file_name}
+            sql = self.sql + """ FROM %sFILE_LUMIS FL JOIN %sFILES F ON F.FILE_ID = FL.FILE_ID 
+	    WHERE F.LOGICAL_FILE_NAME = :logical_file_name""" % ((self.owner,)*2)
+	    binds = {'logical_file_name': logical_file_name}
         elif block_name:
-            sql+="WHERE B.BLOCK_NAME = :block_name"
+            sql = self.sql + """ , F.LOGICAL_FILE_NAME as LOGICAL_FILE_NAME   
+	              FROM %sFILE_LUMIS FL JOIN %sFILES F ON F.FILE_ID = FL.FILE_ID  
+	              JOIN %sBLOCKS B ON B.BLOCK_ID = F.BLOCK_ID  
+		      WHERE B.BLOCK_NAME = :block_name"""  % ((self.owner,)*3)
             binds = {'block_name': block_name}
         else:
             raise Exception("Either logocal_file_name or block_name must be provided.")
         
 	cursors = self.dbi.processData(sql, binds, conn, transaction=False, returnCursor=True)
-	assert len(cursors) == 1, "file lumi does not exist"
+	if len(cursors) != 1:
+	    raise Exception("file lumi does not exist.")
         result = self.formatCursor(cursors[0])
         return result
