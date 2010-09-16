7223f7691693a8beb18cc93cb553a290f0ece243
yuyi <yuyi>
2010-09-16 01:40:25 +0000

    replace DBSMigrationService to avoid naming confusion
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10143 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/components/migration/DBSMigrationMover.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+DBS Migration Service Component Module
+"""
+__revision__ = "$Id: DBSMigrationMover.py,v 1.1 2010/07/21 19:24:30 yuyi Exp $"
+__version__ = "$Revision: 1.1 $"
+
+
+import logging
+import threading
+# harness class that encapsulates the basic component logic.
+from WMCore.Agent.Harness import Harness
+from WMCore.WMFactory import WMFactory
+from dbs.components.insertbuffer.DBSInsertBufferPoller import DBSInsertBufferPoller
+
+#['__call__', '__doc__', '__init__', '__module__', '__str__', 'config', 'handleMessage', 'initInThread', 'initialization', 'logState', 'postInitialization', 'preInitialization', 'prepareToStart', 'publishItem', 'startComponent']
+
+class DBSMigrationMover(Harness):
+    def __init__(self, config):
+	# call the base class
+	Harness.__init__(self, config)
+	self.pollTime = 1
+	print "DBS Migration Service Initialization"
+	
+
+    def preInitialization(self):
+					    
+	# use a factory to dynamically load handlers.
+	factory = WMFactory('generic')
+					    
+	# Add event loop to worker manager
+	myThread = threading.currentThread()
+
+	pollInterval = self.config.DBSMigrationMover.pollInterval
+	logging.info("Setting poll interval to %s seconds for the migration service" % pollInterval)
+	myThread.workerThreadManager.addWorker(DBSMigrationServicePoller(self.config), pollInterval)
+	return
+
+
+	    
