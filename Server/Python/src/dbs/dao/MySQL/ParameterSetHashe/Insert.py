7750dbe5d6a131d5e33771dbcf795eac000c4a1a
afaq <afaq>
2010-09-16 00:42:25 +0000

    updated the seqmgr and other DAOs
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9801 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/MySQL/ParameterSetHashe/Insert.py
+++ Server/Python/src/dbs/dao/MySQL/ParameterSetHashe/Insert.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """ DAO Object for ParameterSetHashes table """ 
 
-__revision__ = "$Revision: 1.2 $"
-__version__  = "$Id: Insert.py,v 1.2 2010/02/11 19:39:33 afaq Exp $ "
+__revision__ = "$Revision: 1.3 $"
+__version__  = "$Id: Insert.py,v 1.3 2010/02/11 22:52:53 afaq Exp $ "
 
-from dbs.dao.Oracle.ParameterSetHashes.Insert import Insert as OraParameterSetHashesInsert
+from dbs.dao.Oracle.ParameterSetHashe.Insert import Insert as OraParameterSetHasheInsert
 
-class Insert(OraParameterSetHashesInsert):
+class Insert(OraParameterSetHasheInsert):
             pass
 
--- Server/Python/src/dbs/dao/MySQL/SequenceManager.py
+++ Server/Python/src/dbs/dao/MySQL/SequenceManager.py
@@ -3,9 +3,8 @@
 This module manages sequences.
 """
 
-__revision__ = "$Id: SequenceManager.py,v 1.2 2010/02/11 17:31:04 afaq Exp $"
-__version__ = "$Revision: 1.2 $"
-
+__revision__ = "$Id: SequenceManager.py,v 1.3 2010/02/11 22:52:53 afaq Exp $"
+__version__ = "$Revision: 1.3 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -17,19 +16,21 @@ class  SequenceManager(DBFormatter):
         DBFormatter.__init__(self, logger, dbi)
         self.logger = logger
 
-    def increment(self, seqName, conn = None, transaction = False, seqCount=1):
+    def increment(self, seqName, conn = None, transaction = False, incCount=1):
         """
         increments the sequence `seqName` by default `Incremented by one`
         and returns its value
         """
 	try:
-	    sql = "UPDATE sequence SET %s=LAST_INSERT_ID(:seq_count)" % (seqName)
-	    seqparms={"seq_count" : seqCount}
-	    self.dbi.processData(sql, seqparms, conn, transaction)
-    
-	    sql = "select LAST_INSERT_ID() as val"
-	    result = self.dbi.processData(sql, conn=conn, transaction=transaction)
+	    #seqTable = "%sS" %seqName
+	    seqTable = "%s" %seqName
+	    sql = "select ID from %s" % seqTable
+	    result = self.dbi.processData(sql, [], conn, transaction)
 	    resultlist = self.formatDict(result)
-	    return resultlist[0]['val']
+	    newSeq = resultlist[0]['id']+incCount
+	    sql = "UPDATE %s SET ID=:seq_count" % seqTable
+	    seqparms={"seq_count" : newSeq}
+	    self.dbi.processData(sql, seqparms, conn, transaction)
+	    return newSeq
 	except:
 	    raise
