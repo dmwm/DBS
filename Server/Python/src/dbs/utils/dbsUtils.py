0c9fe4bf09b254289042cf5a2b0c5a71908a9dc3
akhukhun <akhukhun>
2010-09-16 01:48:05 +0000

    Add decodeLumiIntervals method
    From: akhukhun <akhukhun>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10188 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/utils/dbsUtils.py
+++ Server/Python/src/dbs/utils/dbsUtils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 """ DBS utility class """
 
-__revision__ = "$Revision: 1.3 $"
-__version__  = "$Id: dbsUtils.py,v 1.3 2009/12/23 20:30:40 yuyi Exp $ "
+__revision__ = "$Revision: 1.4 $"
+__version__  = "$Id: dbsUtils.py,v 1.4 2010/08/09 10:59:14 akhukhun Exp $ "
 
+import cjson
 from time import time
 
 class dbsUtils:
@@ -20,4 +21,32 @@ class dbsUtils:
 
     def getModifiedBy(self):
 	return self.getCreateBy()		
+
+
+    def decodeLumiIntervals(self, lumi_list):
+	"""lumi_list must be of one of the two following formats: 
+	   '[[a,b], [c,d],' or 
+	   [a1, a2, a3] """
+
+	errmessage = " lumi intervals must be of one of the two following formats: '[[a,b], [c,d], ...],' or [a1, a2, a3 ...] "
+	try:
+	    lumi_list = cjson.decode(lumi_list)
+	except:
+	    raise Exception("Could not decode the input lumilist: %s" % lumi_list)
+	else:
+	    assert type(lumi_list) == list, errmessage
+	    #check only the first element... in case [1, '2', '3'] is passed, exception will not be raised here.
+	    if type(lumi_list[0]) == int:
+		return lumi_list 
+	    elif type(lumi_list[0]) == list:
+		result = []
+		resultext = result.extend
+		for lumiinterval in lumi_list:
+		    assert type(lumiinterval) == list, errmessage
+		    assert len(lumiinterval) == 2, errmessage
+		    resultext(range(lumiinterval[0], lumiinterval[1]+1))
+		result = list(set(result)) #removes the dublicates, no need to sort
+		return result
+	    else: 
+		raise Exception('Unsupported lumi format: %s. %s' % (lumi_list, errmessage))
 
