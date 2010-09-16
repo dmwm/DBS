08165f7885b885ccd9b73ac62468d82654463599
afaq <afaq>
2010-09-16 00:42:45 +0000

    fixed couple of typos
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9803 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/MySQL/Block/ListStats.py
+++ Server/Python/src/dbs/dao/MySQL/Block/ListStats.py
@@ -3,10 +3,10 @@
 This module provides Block.ListStats data access object.
 Block parameters based on current conditions at DBS, are listed by this DAO
 """
-__revision__ = "$Id: ListStats.py,v 1.2 2010/02/11 19:39:29 afaq Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: ListStats.py,v 1.3 2010/02/11 22:57:25 afaq Exp $"
+__version__ = "$Revision: 1.3 $"
 
-from dbs.dao.Oracle.Block.ListStats import List as OraBlockListStats
+from dbs.dao.Oracle.Block.ListStats import ListStats as OraBlockListStats
 
 class ListStats(OraBlockListStats):
         pass
--- Server/Python/src/dbs/dao/MySQL/Dataset/List1.py
+++ Server/Python/src/dbs/dao/MySQL/Dataset/List1.py
@@ -3,10 +3,10 @@
 This module provides Dataset.List data access object.
 Lists dataset_parent and output configuration parameters too.
 """
-__revision__ = "$Id: List1.py,v 1.2 2010/02/11 19:39:30 afaq Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: List1.py,v 1.3 2010/02/11 22:57:37 afaq Exp $"
+__version__ = "$Revision: 1.3 $"
 
-from dbs.dao.Oracle.Dataset.List1 import List as OraDatasetList1
+from dbs.dao.Oracle.Dataset.List1 import List1 as OraDatasetList1
 
 class List1(OraDatasetList1):
         pass
