ac56fa239bebb99b4b0acbfdb9f24301098f8db5
yuyi <yuyi>
2010-09-16 01:46:51 +0000

    initial checkin
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10181 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/Oracle/ProcessingEra/List.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+This module provides DataTier.List data access object.
+"""
+__vision__ = "$Id: List.py,v 1.1 2010/08/05 14:55:33 yuyi Exp $"
+__revision__ = "$Revision: 1.1 $"
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class List(DBFormatter):
+    """
+    DataTier List DAO class.
+    """
+    def __init__(self, logger, dbi, owner):
+        """
+        Add schema owner and sql.
+        """
+        DBFormatter.__init__(self, logger, dbi)
+        self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
+        self.sql = \
+"""
+SELECT PE.PROCESSING_VERSION, PE.CREATE_DATE, PE.CREATE_BY, PE.DESCRIPTION   
+FROM %sPROCESSING_ERAS PE 
+""" % (self.owner)
+
+    def execute(self, conn, processingV, transaction = False):
+	if not conn:
+	    raise Exception("dbs/dao/Oracle/ProcessingEra/List expects db connection from upper layer.")
+        sql = self.sql
+	binds={}
+	if processingV:
+	    sql += "WHERE PE.PROCESSING_VERSION = :processingV" 
+	    binds = {"processingV":processingV}
+        result = self.dbi.processData(sql, binds, conn, transaction)
+        plist = self.formatDict(result)
+        return plist
