2edf129f235fbe969419890c47f4d3491182f942
afaq <afaq>
2010-09-16 01:51:11 +0000

    added new dao for acq/proc eras, mysql
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10206 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/MySQL/AcquisitionEra/List.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides AcquisitionEra.List data access object.
+"""
+__revision__ = "$Id: List.py,v 1.1 2010/08/12 19:48:40 afaq Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from dbs.dao.Oracle.AcquisitionEra.List import List as OraAcquisitionEraList
+
+class List(OraAcquisitionEraList):
+        pass
+
---
+++ Server/Python/src/dbs/dao/MySQL/ProcessingEra/List.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This module provides ProcessingEra.List data access object.
+"""
+__revision__ = "$Id: List.py,v 1.1 2010/08/12 19:48:40 afaq Exp $"
+__version__ = "$Revision: 1.1 $"
+
+from dbs.dao.Oracle.ProcessingEra.List import List as OraProcessingEraList
+
+class List(OraProcessingEraList):
+            pass
+
