987a1afd8a1e29c7759ce58f955b1fc7d7822445
afaq <afaq>
2010-09-16 00:11:43 +0000

    Added a simple check to avoid extra keys at insert time, but we need to discuss that before wider use
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9615 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/Dataset/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/Dataset/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for Datasets table """ 
 
-__revision__ = "$Revision: 1.7 $"
-__version__  = "$Id: Insert.py,v 1.7 2009/11/27 09:55:03 akhukhun Exp $ "
+__revision__ = "$Revision: 1.8 $"
+__version__  = "$Id: Insert.py,v 1.8 2009/12/17 23:15:41 afaq Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 from sqlalchemy import exceptions
@@ -14,31 +14,45 @@ class Insert(DBFormatter):
         DBFormatter.__init__(self, logger, dbi)
         self.owner = "%s." % owner
         self.logger = logger
+
         self.sql = \
-"""
-INSERT INTO %sDATASETS 
-(DATASET_ID, DATASET, IS_DATASET_VALID, 
-PRIMARY_DS_ID, PROCESSED_DS_ID, DATA_TIER_ID, DATASET_TYPE_ID, 
-PHYSICS_GROUP_ID, 
-XTCROSSSECTION, GLOBAL_TAG, CREATION_DATE, CREATE_BY, 
-LAST_MODIFICATION_DATE, LAST_MODIFIED_BY) 
-VALUES(:datasetid, :dataset, :isdatasetvalid, 
-:primaryds, :processedds, :datatier, :datasettype, 
-:physicsgroup, 
-:xtcrosssection, :globaltag, :creationdate, :createby, 
-:lastmodificationdate, :lastmodifiedby)
-""" % self.owner
+	"""
+	INSERT INTO %sDATASETS 
+		(DATASET_ID, DATASET, IS_DATASET_VALID, 
+			PRIMARY_DS_ID, PROCESSED_DS_ID, DATA_TIER_ID, DATASET_TYPE_ID, 
+			PHYSICS_GROUP_ID, XTCROSSSECTION, GLOBAL_TAG, CREATION_DATE, CREATE_BY, 
+			LAST_MODIFICATION_DATE, LAST_MODIFIED_BY) 
+			VALUES
+			(:dataset_id, :dataset, :is_dataset_valid, 
+			:primary_ds_id, :processed_ds_id, :data_tier_id, :dataset_type_id, 
+			:physics_group_id, :xtcrosssection, :global_tag, :creation_date, :create_by, 
+			:last_modification_date, :last_modified_by) """ % self.owner
+
+    def processInput(self, daoinput):
+                print daoinput
+                #valid_keys = self.sql.split("VALUES")[1].split('(')[1].split(')')[0].replace(",","").strip().split(":")
+                valid_keys = self.sql.split("VALUES")[1].split('(')[1].split(')')[0].replace(',','').replace(':','').strip().split()
+                print valid_keys
+                for akey in daoinput.keys():
+                        if akey not in valid_keys:
+                                del daoinput[akey]
+				self.logger.warning("DROPPING Key: "+akey)
+                print daoinput
+                print self.sql
+                return daoinput
 
     def execute(self, daoinput, conn = None, transaction = False):
+
         """
         daoinput must be a dictionary with the following keys:
-        datasetid, dataset, isdatasetvalid, primaryds(id), processedds(id),
-        datatier(id), datasettype(id), 
-        physicsgroup(id), xtcrosssection, globaltag, 
-        creationdate, createby, 
-        lastmodificationdate, lastmodifiedby"""
+	dataset_id, dataset, is_dataset_valid, 
+	primary_ds_id, processed_ds_id, data_tier_id, dataset_type_id, 
+	physics_group_id, xtcrosssection, global_tag, creation_date, create_by, 
+	last_modification_date, last_modified_by
+	"""
 
         try:
+	    daoinput=self.processInput(daoinput)
             self.dbi.processData(self.sql, daoinput, conn, transaction)
         except exceptions.IntegrityError, ex:
             self.logger.warning("Unique constraint violation being ignored...")
