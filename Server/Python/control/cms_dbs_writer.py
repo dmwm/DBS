897028a61463c9916daa9bd84967f4beeae03f86
yuyi <yuyi>
2010-09-16 01:40:15 +0000

    configure file for all of DBS3 components.
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10142 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/control/cms_dbs_writer.py
@@ -0,0 +1,70 @@
+"""
+DBS Server  configuration file
+"""
+import os, logging
+from WMCore.Configuration import Configuration
+import WMCore.WMInit
+
+config = Configuration()
+
+#Agent section is required by WMCore
+config.section_("Agent")
+config.Agent.contact ="dbs_support@cern.ch"
+#Only these two lines in "Agent" are required in order to turn off the default services.
+config.Agent.useMsgService = False
+config.Agent.useTrigger = False
+
+config.section_("General")
+config.General.workDir = os.environ['DBS3_ROOT']
+
+#CoreDatabase section is required by WMCore. 
+config.section_("CoreDatabase")
+# User specific parameter
+# mySql
+config.CoreDatabase.dbowner = '__MYSQL__'
+config.CoreDatabase.version = 'DBS_3_0_0'
+config.CoreDatabase.connectUrl = 'mysql://username:pd@hostname:port#/CMS_DBS3'
+config.CoreDatabase.engineParameters = {'pool_size': 50, 'max_overflow': 10, 'pool_timeout':200 }
+#Oracle
+#config.CoreDatabase.dbowner = 'DB_OWNER'
+#config.CoreDatabase.version = 'DBS_3_0_0'
+#config.CoreDatabase.connectUrl = 'oracle://username:pd@dbname'
+#config.CoreDatabase.engineParameters = {'pool_size': 15, 'max_overflow': 10, 'pool_timeout': 200 }
+
+#config web server
+config.webapp_("cmsdbs")
+config.cmsdbs.componentDir = config.General.workDir + "/Logs/DBSServer"
+config.cmsdbs.server.host = "::"
+config.cmsdbs.server.port = 8686
+config.cmsdbs.templates = WMCore.WMInit.getWMBASE() + '/src/templates/WMCore/WebTools'
+config.cmsdbs.title = 'DBS Server'
+config.cmsdbs.description = 'CMS DBS Service'
+config.cmsdbs.default_expires=300
+config.cmsdbs.section_('views')
+active=config.cmsdbs.views.section_('active')
+
+#DBS server page/view
+DBS = active.section_('DBS')
+DBS.object = 'WMCore.WebTools.RESTApi'
+DBS.section_('model')
+DBS.model.object = 'dbs.web.DBSWriterModel'
+DBS.section_('formatter')
+active.DBS.formatter.object = 'WMCore.WebTools.RESTFormatter'
+
+#Migration server
+MIGRATE = active.section_('MIGRATE')
+MIGRATE.object = 'WMCore.WebTools.RESTApi'
+MIGRATE.section_('model')
+MIGRATE.model.object = 'dbs.web.DBSMigrateModel'
+MIGRATE.section_('formatter')
+MIGRATE.formatter.object = 'WMCore.WebTools.RESTFormatter'
+MIGRATE.version = 'DBS_3_0_0'
+MIGRATE.nthreads = 4
+
+#config migration mover
+config.component_('DBSMigrationMover')
+config.DBSMigrationMover.default_expires=300
+config.DBSMigrationMover.pollInterval = 1
+config.DBSMigrationMover.namespace= "dbs.components.migration.DBSMigrationMover"
+config.DBSMigrationMover.componentDir = config.General.workDir + "/Logs/MigrationMover"
+config.DBSMigrationMover.workerThreads = 1
