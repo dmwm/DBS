f6818877667c9dae5497fae4f6418a96533a6925
afaq <afaq>
2010-09-16 01:33:00 +0000

    added an alias to cmd tool
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10099 4525493e-7705-40b1-a816-d608a930855b

--- Client/setup.sh
+++ Client/setup.sh
@@ -4,4 +4,4 @@ export cjson=$DBS3_ROOT/External/python-cjson-1.0.5/build/lib.linux-x86_64-2.6/
 export PYTHONPATH=$PWD/src/python:$cjson:$PYTHONPATH
 export DBS_READER_URL=http://vocms09.cern.ch:8585/DBS
 export DBS_WRITER_URL=http://vocms09.cern.ch:8585/DBS
-
+alias dbs="python $PWD/cmdline/dbs.py"
