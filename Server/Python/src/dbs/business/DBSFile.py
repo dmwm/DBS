6bd1d1050bcaff1f653f24f0cb77b88f473106bd
akhukhun <akhukhun>
2010-09-16 00:08:03 +0000

    open and close connection here.
    From: akhukhun <akhukhun>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9591 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/business/DBSFile.py
+++ Server/Python/src/dbs/business/DBSFile.py
@@ -3,8 +3,8 @@
 This module provides business object class to interact with File. 
 """
 
-__revision__ = "$Id: DBSFile.py,v 1.9 2009/11/30 09:52:31 akhukhun Exp $"
-__version__ = "$Revision: 1.9 $"
+__revision__ = "$Id: DBSFile.py,v 1.10 2009/12/07 15:46:06 akhukhun Exp $"
+__version__ = "$Revision: 1.10 $"
 
 from WMCore.DAOFactory import DAOFactory
 
@@ -32,7 +32,10 @@ class DBSFile:
         """
         either dataset(and lfn pattern) or block(and lfn pattern) must be specified.
         """
-        return self.filelist.execute(dataset, block, lfn)
+        conn = self.dbi.connection()
+        result = self.filelist.execute(dataset, block, lfn, conn)
+        conn.close()
+        return result
 
 
     def insertFile(self, businput):
