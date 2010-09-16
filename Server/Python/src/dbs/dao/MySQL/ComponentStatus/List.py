d5d0fb6d5ef4c8995f1d70f7c07a598a79807f83
afaq <afaq>
2010-09-16 01:31:26 +0000

    added the mysql dao for component status
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10089 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/MySQL/ComponentStatus/List.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" DAO Object for ComponentStatus table """ 
+
+__revision__ = "$Revision: 1.1 $"
+__version__  = "$Id: List.py,v 1.1 2010/06/14 14:34:34 afaq Exp $ "
+
+from dbs.dao.Oracle.ComponentStatus.List import List as OraComponentStatusList
+
+class List(OraComponentStatusList):
+            pass
+	
