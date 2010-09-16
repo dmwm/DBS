c48e20be48a0847be82ecb21e974a00cdbed89bb
afaq <afaq>
2010-09-16 00:51:37 +0000

    updated the update dao
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9853 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/File/UpdateStatus.py
+++ Server/Python/src/dbs/dao/Oracle/File/UpdateStatus.py
@@ -2,15 +2,17 @@
 """
 This module provides File.UpdateStatus data access object.
 """
-__revision__ = "$Id: UpdateStatus.py,v 1.1 2010/03/03 22:35:54 afaq Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: UpdateStatus.py,v 1.2 2010/03/04 17:25:31 afaq Exp $"
+__version__ = "$Revision: 1.2 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class UpdateStatus(DBFormatter):
+
     """
     File Update Statuss DAO class.
     """
+
     def __init__(self, logger, dbi, owner):
         """
         Add schema owner and sql.
@@ -19,9 +21,10 @@ class UpdateStatus(DBFormatter):
 	self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
         self.sql = """UPDATE %sFILES SET IS_FILE_VALID = :is_file_valid where LOGICAL_FILE_NAME = :logical_file_name""" %  self.owner 
         
-    def execute(self, logical_file_name, is_file_valid, conn = None, transaction = False):
+    def execute ( self, conn, transaction, logical_file_name, is_file_valid ):
         """
         for a given file
         """	
 	binds = { "logical_file_name" : logical_file_name , "is_file_valid" : is_file_valid }
-        result = self.dbi.processData(self.sql, blockStats, conn, transaction)
+        result = self.dbi.processData(self.sql, binds, conn, transaction)
+    
