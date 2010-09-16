04a8d0e39e2fe5c06427535ea23d04ce1920c246
afaq <afaq>
2010-09-16 01:29:38 +0000

    updated APIs after tests, also updated the incode documentation, we can now easily use pydoc
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10078 4525493e-7705-40b1-a816-d608a930855b

--- Client/src/python/dbs/apis/dbsClient.py
+++ Client/src/python/dbs/apis/dbsClient.py
@@ -1,6 +1,6 @@
 # 
-# $Revision: 1.46 $"
-# $Id: dbsClient.py,v 1.46 2010/06/04 15:22:14 afaq Exp $"
+# $Revision: 1.47 $"
+# $Id: dbsClient.py,v 1.47 2010/06/04 20:39:32 afaq Exp $"
 # @author anzar
 #
 import os, sys, socket
@@ -19,6 +19,11 @@ except:
 
 class DbsApi:
         def __init__(self, url="", proxy=""):
+		"""
+		* DbsApi CTOR
+		url: serevr URL
+		proxy: http proxy; this feature is TURNED OFF at the moemnt
+		"""
                 self.url=url
 		self.proxy=proxy
 		self.opener =  urllib2.build_opener()
@@ -27,13 +32,12 @@ class DbsApi:
 		"""
         	* callServer
 		* API to make HTTP call to the DBS Server
-		* url: serevr URL
-        	* params: parameters to server, includes api name to be invoked
-
+		* urlplus: addition to URL, this is generall where the VERB is provided for the REST call, as '/files'
+        	* params: parameters to server
+		* callmethod; the HTTP method used, by default it is HTTP-GET, possible values are GET, POST and PUT
 		"""
 		UserID=os.environ['USER']+'@'+socket.gethostname()
 		headers =  {"Content-type": "application/json", "Accept": "application/json", "UserID": UserID }
-		#headers =  {"Content-type": "application/json", "Accept": "*/*", "UserID": UserID }
 
 		res=""
 		try:
@@ -66,6 +70,9 @@ class DbsApi:
 		return json_ret
 		
 	def parseForException(self, data):
+	    """
+	    An internal method, should not be used by clients
+	    """
 	    if type(data)==type("abc"):
 		data=json.loads(data)
             if type(data) == type({}) and data.has_key('exception'):
@@ -75,8 +82,7 @@ class DbsApi:
 		
         def serverinfo(self):
                 """
-                * API to retrieve DAS interface for DQM Catalog Service
-                * serverinfo
+                * API to retrieve DAS interface and status information
 		* can be used a PING
                 """
                 return self.callServer("/serverinfo")
@@ -84,6 +90,7 @@ class DbsApi:
 	def listPrimaryDatasets(self, dataset=""):
 		"""
 		* API to list ALL primary datasets in DBS 
+		* dataset: If provided, will list THAT primary dataset
 		"""
 		if dataset:
 		    return self.callServer("/primarydatasets?primary_ds_name=%s" %dataset )
@@ -92,14 +99,20 @@ class DbsApi:
         def insertPrimaryDataset(self, primaryDSObj={}):
                 """
                 * API to insert A primary dataset in DBS 
-                * primaryDSObj : primary dataset object of type {}
+                * primaryDSObj : primary dataset object of type {} , with key(s) :-
+			* primary_ds_type : TYPE (out of valid types in DBS, MC, DATA) --REQUIRED
+			* primary_ds_name : Name of the primary dataset --REQUIRED
                 """
                 return self.callServer("/primarydatasets", params = primaryDSObj, callmethod='POST' )
 
 	def insertOutputConfig(self, outputConfigObj={}):
                 """
                 * API to insert An OutputConfig in DBS 
-                * outputConfigObj : Output Config object of type {}
+                * outputConfigObj : Output Config object of type {}, with key(s) :-
+			    * app_name : App Name  --REQUIRED
+			    * release_version : Release Version --REQUIRED
+			    * pset_hash : Pset Hash --REQUIRED
+			    * output_module_label : Output Module Label --REQUIRED
                 """
                 return self.callServer("/outputconfigs", params = outputConfigObj , callmethod='POST' )
 
@@ -112,8 +125,9 @@ class DbsApi:
 		* pset_hash : pset hash
 		* app_name : Application name (generally it is cmsRun)
 		* output_module_label : output_module_label
-		*
+		* 
 		* You can use ANY combination of these parameters in this API
+		* All parameters are optional, if you do not provide any parameter, ALL configs will be listed from DBS
                 """
 		add_to_url=""
 		amp=False
@@ -149,7 +163,8 @@ class DbsApi:
     	def insertAcquisitionEra(self, acqEraObj={}):
                 """
                 * API to insert An Acquisition Era in DBS 
-                * acqEraObj : Acquisition Era object of type {}
+                * acqEraObj : Acquisition Era object of type {}, with key(s) :-
+			* acquisition_era_name : Acquisition Era Name --REQUIRED
                 """
                 return self.callServer("/acquisitionras", params = acqEraObj , callmethod='POST' )
 		
@@ -157,10 +172,13 @@ class DbsApi:
                 """
                 * API to insert A Processing Era in DBS 
                 * procEraObj : Processing Era object of type {}
+			    * processing_version : Processing Version --REQUIRED
+			    * description : Description --REQUIRED
                 """
                 return self.callServer("/processingeras", params = procEraObj , callmethod='POST' )
 
-        def listDatasets(self, dataset="", parent_dataset="", release_version="", pset_hash="", app_name="", output_module_label="", processing_version="", acquisition_era=""):
+        def listDatasets(self, dataset="", parent_dataset="", release_version="", pset_hash="", app_name="", output_module_label="", processing_version="", acquisition_era="",
+		run_num="", physics_group_name="", logical_file_name="", primary_ds_name="", primary_ds_type="", data_tier_name="", dataset_access_type=""):
                 """
                 * API to list dataset(s) in DBS 
                 * dataset : Full dataset (path) of the dataset
@@ -169,10 +187,17 @@ class DbsApi:
 		* pset_hash : pset hash
 		* app_name : Application name (generally it is cmsRun)
 		* output_module_label : output_module_label
+		* processing_version : Processing Version
+		* acquisition_era : Acquisition Era
+		* primary_ds_name : Primary Dataset Name
+		* primary_ds_type : Primary Dataset Type (Type of data, MC/DATA)
+		* data_tier_name : Data Tier 
+	        * dataset_access_type : Dataset Access Type ( PRODUCTION, DEPRECATED etc.)
 		*
 		* You can use ANY combination of these parameters in this API
+		* In absence of parameters, all datasets know to DBS instance will be returned
                 """
-		add_to_url=""
+		ADD_TO_Url=""
 		amp=False
 		if dataset: 
 		    add_to_url += "dataset=%s"%dataset
@@ -204,6 +229,26 @@ class DbsApi:
 		if acquisition_era:
 		    add_to_url += "acquisition_era=%s"%acquisition_era
 		    amp=True
+		if logical_file_name : 
+		    if amp: add_to_url += "&"
+		    add_to_url += "logical_file_name=%s" % logical_file_name
+		    amp=True
+		if primary_ds_name : 
+		    if amp: add_to_url += "&"
+		    add_to_url += "primary_ds_name=%s" % primary_ds_name
+		    amp=True
+		if data_tier_name: 
+		    if amp: add_to_url += "&"
+		    add_to_url += "data_tier_name=%s" % data_tier_name
+		    amp=True
+		if dataset_access_type : 
+		    if amp: add_to_url += "&"
+		    add_to_url += "dataset_access_type=%s" % dataset_access_type
+		    amp=True
+		if dataset_access_type : 
+		    if amp: add_to_url += "&"
+		    add_to_url += "dataset_access_type=%s" % dataset_access_type
+		    amp=True
 		if add_to_url:
 		    return self.callServer("/datasets?%s" % add_to_url )
 		# Default, list all datasets
@@ -212,42 +257,62 @@ class DbsApi:
 	def listDatasetParents(self, dataset):
                 """
                 * API to list A datasets parents in DBS 
-		* dataset : dataset
+		* dataset : dataset --REQUIRED
                 """
                 return self.callServer("/datasetparents?dataset=%s" %dataset )
 
 	def listDatasetChildren(self, dataset):
                 """
                 * API to list A datasets children in DBS 
-		* dataset : dataset
+		* dataset : dataset --REQUIRED
                 """
                 return self.callServer("/datasetchildren?dataset=%s" %dataset )
 
         def insertDataset(self, datasetObj={}):
                 """
                 * API to list A primary dataset in DBS 
-                * datasetObj : dataset object of type {}
+                * datasetObj : dataset object of type {}, with key(s) :-
+		    *  processed_ds_name : Processed Dataset Name
+		    * primary_ds_name : Primary Dataset Name
+		    * is_dataset_valid : Is Dataset Valid (1/0)
+		    * xtcrosssection : Xtcrosssection
+		    * global_tag : Global Tag
+		    * output_configs : Output Configs (List of)
+			    o app_name : App Name
+			    o release_version : Release Version
+			    o pset_hash : Pset Hash
+			    o output_module_label : Output Module Label 
                 """
                 return self.callServer("/datasets", params = datasetObj , callmethod='POST' )
 
 	def insertSite(self, siteObj={}):
                 """
                 * API to insert a site in DBS 
-                * siteObj : Site object of type {}
+                * siteObj : Site object of type {}, with key(s) :-
+		    site_name : Site Name (T1_CMS_FNAL) 
                 """
                 return self.callServer("/sites", params = siteObj , callmethod='POST' )
 
         def insertBlock(self, blockObj={}):
                 """
                 * API to insert a block into DBS 
-                * blockObj : block object
+                * blockObj : block object, with key(s) :-
+		        * open_for_writing : Open For Writing (1/0) (Default 1)
+			* block_size : Block Size (Default 0)
+			* file_count : File Count (Default 0)
+			* block_name : Block Name --REQUIRED
+			* origin_site_name : Origin Site Name --REQUIRED 
                 """
                 return self.callServer("/blocks", params = blockObj , callmethod='POST' )
 
         def listBlocks(self, block_name="", dataset="", logical_file_name="", origin_site_name="", run_num=-1):
                 """
                 * API to list A block in DBS 
-                * name : name of the block
+                * block_name : name of the block
+		* dataset : dataset
+		* logical_file_name : Logical File Name
+		* origin_site_name : Origin Site Name
+		* run_num : Run Number
                 """
 		add_to_url=""
 		amp=False
@@ -373,34 +438,18 @@ class DbsApi:
 		    return self.callServer("/files?qInserts=%s" % qInserts, params = filesList , callmethod='POST' )    
                 return self.callServer("/files", params = filesList , callmethod='POST' )
 
-        def listRuns(self, dataset="", block="", logical_file_name="", minrun="", maxrun=""):
+        def listRuns(self, minrun="", maxrun=""):
                 """
-                * API to list A runs in DBS 
-                * logical_file_name : logical_file_name of file
-		* dataset : dataset
-		* block : block name
-		
-		You can use logical_file_name or dataset or block
-		The presedence order is : dataset, block, logical_file_name
-		* minrun/maxrun : if you want to look for a run range use these 
-		Use minrun=maxrun for a specific run, say for runNumber 2000 use minrun=2000, maxrun=2000
+                * API to list runs in DBS 
+		* minrun: minimum run number	
+		* maxrun: maximum run number
+		* (minrun, max)	defines the run range
+		*
+		* If you omit both min/maxrun, then all runs known to DBS will be listed
+		* Use minrun=maxrun for a specific run, say for runNumber 2000 use minrun=2000, maxrun=2000
                 """
-
 		add_to_url=""
 		amp=False
-		if dataset: 
-		    add_to_url += "dataset=%s"%dataset
-		    amp=True
-		if block :
-		    parts=block.split('#')
-		    block_name=parts[0]+urllib.quote_plus('#')+parts[1]
-		    if amp: add_to_url += "&"
-		    add_to_url += "block_name=%s" %block_name	
-		    amp=True    
-		if logical_file_name : 
-		    if amp: add_to_url += "&"
-		    add_to_url += "logical_file_name=%s" %logical_file_name
-		    amp=True
 		if minrun:
 		    if amp: add_to_url += "&"
 		    add_to_url += "minrun=%s" %minrun
@@ -417,8 +466,11 @@ class DbsApi:
 	def listSites(self, block_name="", site_name=""):
                 """
                 * API to list sites (or for a block) in DBS 
-                * block_name : name of the block
+                * block_name : name of the block (The listed site is the ORIGIN_SITE of the block)
 		* site_name : name of site
+		* Both parameters are optional, and mutually exclusive.
+		* In case none provided, DBS returns list of all known sites
+		* In case both provided, block_name takes precedence
                 """
 		add_to_url=""
 		amp=False
@@ -438,16 +490,16 @@ class DbsApi:
 	def updateFileStatus(self, logical_file_name="", is_file_valid=1):
 	    """
 	    API to update file status
-	    * logical_file_name : logical_file_name
-	    * is_file_valid : valid=1, invalid=0
+	    * logical_file_name : logical_file_name --REQUIRED
+	    * is_file_valid : valid=1, invalid=0 --REQUIRED
 	    """
 	    return self.callServer("/files?logical_file_name=%s&is_file_valid=%s" %(logical_file_name, is_file_valid), params={}, callmethod='PUT')
 
 	def updateDatasetType(self, dataset, dataset_type):
 	    """
 	    API to update dataset status
-	    * dataset : dataset name
-	    * dataset_type : production, deprecated, ...etc
+	    * dataset : Dataset --REQUIRED
+	    * dataset_type : production, deprecated, etc --REQUIRED
 	    *
 	    """
 	    return self.callServer("/datasets?dataset=%s&dataset_type=%s" %(dataset, dataset_type), params={}, callmethod='PUT')    
@@ -455,21 +507,12 @@ class DbsApi:
 	def updateDatasetStatus(self, dataset, is_dataset_valid):
 	    """
 	    API to update dataset status
-	    * dataset : dataset name
-	    * is_dataset_valid : valid=1, invalid=0
+	    * dataset : dataset name --REQUIRED
+	    * is_dataset_valid : valid=1, invalid=0 --REQUIRED
 	    *
 	    """
 	    return self.callServer("/datasets?dataset=%s&is_dataset_valid=%s" %(dataset, is_dataset_valid), params={}, callmethod='PUT')    
 		
-	def updateDatasetRunStatus(self, dataset="", run_number=-1, complete=1):
-	    """
-	    API to update dataset run status 
-	    * dataset : dataset name
-	    * run_number : the run number taht needs to be updated
-	    * complete : 1/0 mark it complete
-	    """
-	    return self.callServer("/runs?dataset=%s&run_number=%s&complete=%s" %(dataset, run_number, complete), params={}, callmethod='PUT')
-
 	def updateBlockStatus(self, block_name, open_for_writing):
 	    """
 	    API to update block status
@@ -482,7 +525,8 @@ class DbsApi:
 
 	def listDataTypes(self, dataset=""):
 	    """
-	    API to list data types
+	    API to list data types known to dbs (when no parameter supplied)
+	    dataset: If provided, will return data type (of primary dataset) of the dataset
 	    """
 
 	    add_to_url=""
@@ -493,7 +537,8 @@ class DbsApi:
 
         def listDataTiers(self, datatier=""):
 	    """
-	    API to list data types
+	    API to list data tiers  known to DBS
+	    datatier : when supplied, dbs will list details on this tier
 	    """
 
 	    add_to_url=""
@@ -505,7 +550,7 @@ class DbsApi:
 	def listBlockParents(self, block_name=""):
 	    """
 	    API to list block parents
-	    * block_name : name of block whoes parents needs to be found
+	    * block_name : name of block whoes parents needs to be found --REQUIRED
 	    """
 	    parts=block_name.split('#')
 	    block_name=parts[0]+urllib.quote_plus('#')+parts[1]
@@ -514,7 +559,7 @@ class DbsApi:
 	def listBlockChildren(self, block_name=""):
 	    """
 	    API to list block children
-	    * block_name : name of block whoes children needs to be found
+	    * block_name : name of block whoes children needs to be found --REQUIRED
 	    """
 	    parts=block_name.split('#')
 	    block_name=parts[0]+urllib.quote_plus('#')+parts[1]
@@ -523,7 +568,8 @@ class DbsApi:
 	def insertDataTier(self, dataTierObj={}):
             """
             * API to insert A Data Tier in DBS 
-            * dataTierObj : Data Tier object of type {}
+            * dataTierObj : Data Tier object of type {}, with kys :-
+		    data_tier_name : Data Tier that needs to be inserted
             """
             return self.callServer("/datatiers", params = dataTierObj , callmethod='POST' )
 
@@ -534,9 +580,9 @@ class DbsApi:
 
 if __name__ == "__main__":
 	# DBS Service URL
-	url="http://cmssrv18.fnal.gov:8586/dbs3"
-	read_proxy="http://cmst0frontier1.cern.ch:3128"
-	read_proxy="http://cmsfrontier1.fnal.gov:3128"
+	url="http://cmssrv18.fnal.gov:8585/dbs3"
+	#read_proxy="http://cmst0frontier1.cern.ch:3128"
+	#read_proxy="http://cmsfrontier1.fnal.gov:3128"
 	read_proxy=""
 	api = DbsApi(url=url, proxy=read_proxy)
 	print api.listPrimaryDatasets()
