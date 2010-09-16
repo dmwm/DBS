7808b7a2cc764e80e3bda5ed7d1c8c7b1f837cd4
yuyi <yuyi>
2010-09-16 00:15:53 +0000

    update to match the new server site key requirement
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9642 4525493e-7705-40b1-a816-d608a930855b

--- Client/utils/insertdataset.py
+++ Client/utils/insertdataset.py
@@ -1,11 +1,13 @@
 #DBS-3 imports
 from dbs.apis.dbsClient import *
-#url="http://cmssrv48.fnal.gov:8989/DBSServlet"
-url="http://cmssrv18.fnal.gov:8585/dbs3"
+url="http://cmssrv48.fnal.gov:8989/dbs3"
+#url="http://cmssrv18.fnal.gov:8585/dbs3"
 # API Object    
 dbs3api = DbsApi(url=url)
 
-dataset={'IS_DATASET_VALID': 1, 'PRIMARY_DS_NAME': 'TkCosmics38T', 'PHYSICS_GROUP_NAME': 'Tracker', 'ACQUISITION_ERA_NAME': '', 'GLOBAL_TAG': 'STARTUP31X_V3::All', 'PRIMARY_DS_TYPE': 'test', 'PROCESSED_DATASET_NAME': 'Summer09-STARTUP31X_V3-v1', 'DATASET': '/TkCosmics38T/Summer09-STARTUP31X_V3-v1/GEN-SIM-DIGI-RAW', 'DATASET_TYPE': 'PRODUCTION', 'XTCROSSSECTION': 123, 'PROCESSING_VERSION': '1', 'DATA_TIER_NAME': 'GEN-SIM-DIGI-RAW'}
+dataset={'is_dataset_valid': 1, 'primary_ds_name': 'yuyi2', 'physics_group_name': 'Tracker',
+'global_tag': 'startup31x_v3::all', 'primary_ds_type': 'test', 'processed_ds_name':
+'Summer09-STARTUP31X_V3-v1', 'dataset': '/yuyi2/Summer09-STARTUP31X_V3-v1/GEN-SIM-DIGI-RAW', 'dataset_type': 'PRODUCTION', 'xtcrosssection': 123, 'data_tier_name': 'GEN-SIM-DIGI-RAW'}
 
 print dataset.keys()
 
