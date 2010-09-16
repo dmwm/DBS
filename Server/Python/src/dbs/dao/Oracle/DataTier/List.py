2b7379cf6c32d26c0bf97f44bd296b9a904d54e1
yuyi <yuyi>
2010-09-16 01:48:45 +0000

    bug fixed
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10192 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/DataTier/List.py
+++ Server/Python/src/dbs/dao/Oracle/DataTier/List.py
@@ -2,8 +2,8 @@
 """
 This module provides DataTier.List data access object.
 """
-__revision__ = "$Id: List.py,v 1.4 2010/08/05 14:35:30 yuyi Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: List.py,v 1.5 2010/08/09 18:43:08 yuyi Exp $"
+__version__ = "$Revision: 1.5 $"
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -19,7 +19,7 @@ class List(DBFormatter):
         self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
         self.sql = \
 """
-SELECT DT.DATA_TIER_ID, DT.DATA_TIER_NAME, DT.CREATE_DATE, DT.CREATE_BY  
+SELECT DT.DATA_TIER_ID, DT.DATA_TIER_NAME, DT.CREATION_DATE, DT.CREATE_BY  
 FROM %sDATA_TIERS DT 
 """ % (self.owner)
 
