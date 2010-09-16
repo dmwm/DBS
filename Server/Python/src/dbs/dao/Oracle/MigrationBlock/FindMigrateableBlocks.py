da159482ab5afd254ceff42599793eed36b1734f
yuyi <yuyi>
2010-09-16 01:53:24 +0000

    initial checking
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10219 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/Oracle/MigrationBlock/FindMigrateableBlocks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+This module provides MigrationRequests.List data access object.
+"""
+__revision__ = "$Id: FindMigrateableBlocks.py,v 1.1 2010/08/18 19:08:39 yuyi Exp $"
+__version__ = "$Revision: 1.1 $"
+
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class FindMigrateableBlocks(DBFormatter):
+    """
+    MigrationRequest FindMigrateableBlocks DAO class.
+    """
+    def __init__(self, logger, dbi, owner=""):
+        """
+        Add schema owner and sql.
+        """
+        DBFormatter.__init__(self, logger, dbi)
+        self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
+        self.sql = """SELECT MIGRATION_BLOCK_ID, MIGRATION_BLOCK_NAME, MIGRATION_ORDER, 
+			FROM %s MIGRATION_BLOCKS 
+			WHERE MIGRATION_REQUEST_ID=:migration_request_id AND MIGRATION_STATUS= 1 ORDER BY MIGRATION_ORDER DESC
+			""" % (self.owner)
+
+    def execute(self, conn, migration_request_id="", transaction=False):
+        """
+        Lists all primary datasets if pattern is not provided.
+        """
+        binds = { "migration_request_id" : migration_request_id }
+	cursors = self.dbi.processData(self.sql, binds, conn, transaction, returnCursor=True)
+        result = self.formatCursor(cursors[0])
+        return result
