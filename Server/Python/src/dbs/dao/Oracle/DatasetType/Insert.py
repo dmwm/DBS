44f1d850245446e37858b1fc8bc4c48f1ced7e22
yuyi <yuyi>
2010-09-16 02:03:02 +0000

    correct errors and this dao was first used in migration code. Changes made by follow our convention
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@10275 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/DatasetType/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/DatasetType/Insert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ DAO Object for DatasetTypes table """ 
 
-__revision__ = "$Revision: 1.9 $"
-__version__  = "$Id: Insert.py,v 1.9 2010/08/18 19:43:36 yuyi Exp $ "
+__revision__ = "$Revision: 1.10 $"
+__version__  = "$Id: Insert.py,v 1.10 2010/09/14 14:34:11 yuyi Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -12,30 +12,14 @@ class Insert(DBFormatter):
             DBFormatter.__init__(self, logger, dbi)
 	    self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
 
-            self.sql = """INSERT INTO %sDATASET_ACCESS_TYPES ( DATASET_ACCESS_TYPE_ID, DATASET_ACCESS_TYPE) VALUES (:datasettypeid, :datasettype)""" % (self.owner)
-
-    def getBinds_delme( self, dataset_typesObj ):
-            binds = {}
-            if type(dataset_typesObj) == type ('object'):
-            	binds = {
-			'datasettypeid' : dataset_typesObj['datasettypeid'],
-			'datasettype' : dataset_typesObj['datasettype'],
-                 }
-
-            elif type(dataset_typesObj) == type([]):
-               binds = []
-               for item in dataset_typesObj:
-                   binds.append({
- 	                'datasettypeid' : item['datasettypeid'],
- 	                'datasettype' : item['datasettype'],
- 	                })
-               return binds
+            self.sql = """INSERT INTO %sDATASET_ACCESS_TYPES ( DATASET_ACCESS_TYPE_ID, DATASET_ACCESS_TYPE) VALUES
+            (:dataset_access_type_id, :dataset_access_type)""" % (self.owner)
 
 
     def execute( self, conn, dataset_typesObj, transaction=False ):
 	if not conn:
 	    raise Exception("dbs/dao/Oracle/DatasetType/Insert expects db connection from upper layer.")
-	result = self.dbi.processData(self.sql, binds, conn, transaction)
+	result = self.dbi.processData(self.sql, dataset_typesObj, conn, transaction)
 	return
 
 
