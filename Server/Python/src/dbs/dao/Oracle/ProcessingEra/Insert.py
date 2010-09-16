699ddd3ff0cceae9410f14912cf91b1df0c3c00a
afaq <afaq>
2010-09-16 00:17:45 +0000

    acqera and prcver apis ready
    From: afaq <afaq>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9654 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/dao/Oracle/ProcessingEra/Insert.py
+++ Server/Python/src/dbs/dao/Oracle/ProcessingEra/Insert.py
@@ -1,45 +1,21 @@
 #!/usr/bin/env python
 """ DAO Object for ProcessingEras table """ 
 
-__revision__ = "$Revision: 1.2 $"
-__version__  = "$Id: Insert.py,v 1.2 2009/10/20 02:19:23 afaq Exp $ "
+__revision__ = "$Revision: 1.3 $"
+__version__  = "$Id: Insert.py,v 1.3 2009/12/28 16:58:04 afaq Exp $ "
 
 from WMCore.Database.DBFormatter import DBFormatter
 
 class Insert(DBFormatter):
 
-    def __init__(self, logger, dbi):
+    def __init__(self, logger, dbi, owner):
             DBFormatter.__init__(self, logger, dbi)
-            self.owner = "%s." % self.dbi.engine.url.username
+            self.owner = "%s." % owner
 
-            self.sql = """INSERT INTO %sPROCESSING_ERAS ( PROCESSING_ERA_ID, PROCESSING_VERSION, CREATION_DATE, CREATE_BY, DESCRIPTION) VALUES (:processingeraid, :processingversion, :creationdate, :createby, :description) % (self.owner) ;"""
+            self.sql = """INSERT INTO %sPROCESSING_ERAS ( PROCESSING_ERA_ID, PROCESSING_VERSION, CREATION_DATE, CREATE_BY, DESCRIPTION) 
+			    VALUES (:processing_era_id, :processing_version, :creation_date, :create_by, :description)""" % (self.owner) 
 
-    def getBinds_delme( self, processing_erasObj ):
-            binds = {}
-            if type(processing_erasObj) == type ('object'):
-            	binds = {
-			'processingeraid' : processing_erasObj['processingeraid'],
-			'processingversion' : processing_erasObj['processingversion'],
-			'creationdate' : processing_erasObj['creationdate'],
-			'createby' : processing_erasObj['createby'],
-			'description' : processing_erasObj['description'],
-                 }
-
-            elif type(processing_erasObj) == type([]):
-               binds = []
-               for item in processing_erasObj:
-                   binds.append({
- 	                'processingeraid' : item['processingeraid'],
- 	                'processingversion' : item['processingversion'],
- 	                'creationdate' : item['creationdate'],
- 	                'createby' : item['createby'],
- 	                'description' : item['description'],
- 	                })
-               return binds
-
-
-    def execute( self, processing_erasObj, conn=None, transaction=False ):
-            ##binds = self.getBinds( processing_erasObj )
+    def execute( self, binds, conn=None, transaction=False ):
             result = self.dbi.processData(self.sql, binds, conn, transaction)
             return
 
