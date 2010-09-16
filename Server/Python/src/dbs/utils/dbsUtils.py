2931ad486520c63796490c29adab4a5e721d10ba
yuyi <yuyi>
2010-09-16 00:12:01 +0000

    *** empty log message ***
    From: yuyi <yuyi>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@9617 4525493e-7705-40b1-a816-d608a930855b

--- Server/Python/src/dbs/utils/dbsUtils.py
+++ Server/Python/src/dbs/utils/dbsUtils.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 """ DBS utility class """
 
-__revision__ = "$Revision: 1.1 $"
-__version__  = "$Id: dbsUtils.py,v 1.1 2009/12/16 20:12:23 yuyi Exp $ "
+__revision__ = "$Revision: 1.2 $"
+__version__  = "$Id: dbsUtils.py,v 1.2 2009/12/18 17:48:07 yuyi Exp $ "
 
 from time import time
 
 class dbsUtils:
     """dbsUtils class provides time, client names, etc functions."""
 
-    def __init__(self, logger):
+    def __init__(self):
 	pass
 
     def getTime(self):
