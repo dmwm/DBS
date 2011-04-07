a09029aae1b7c1eacfe2243f36dd3d4a9518ddba
yuyi <yuyi@fnal.gov>
2011-04-07 18:03:52 +0000

    From Manuel:DBS2 to DBS3 migration validation
    From: yuyi <yuyi@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@12226 4525493e-7705-40b1-a816-d608a930855b

--- DBS2To3Migration/test/DBS2SqlApi.py
+++ DBS2To3Migration/test/DBS2SqlApi.py
@@ -1,3 +1,8 @@
+"""
+DBS2 SQL API for unittests to validate the DBS2 to DBS3 migration
+Author: Manuel Giffels <giffels@physik.rwth-aachen.de>
+"""
+
 from WMCore.Database.DBFactory import DBFactory
 from WMCore.Database.DBFormatter import DBFormatter
 
@@ -7,7 +12,7 @@ def modifyDatasetList(datasetList):
         dataset['dataset'] = '/'+dataset['primary_ds_name']+'/'+dataset['processed_ds_name']+'/'+dataset['data_tier_name']
         dataset['is_dataset_valid'] = int(dataset['status'] in ('VALID', 'RO', 'PRODUCTION', 'IMPORTED', 'EXPORTED'))
 
-        if dataset['status'] in ('VALID'):
+        if dataset['status'] in ('VALID','INVALID'):
             dataset['dataset_access_type'] = 'UNKNOWN_DBS2_TYPE'
         else:
             dataset['dataset_access_type'] = dataset['status']
@@ -17,13 +22,6 @@ def modifyDatasetList(datasetList):
         
     return datasetList
 
-def modifyBlocklist(blockList):
-    for block in blockList:
-        if block['origin_site_name'] == 'unknown':
-            block['origin_site_name'] = 'unknow'
-    
-    return blockList
-
 class DBS2SqlApi(object):
     def __init__(self,logger,connectUrl,owner):
         object.__init__(self)
@@ -31,7 +29,24 @@ class DBS2SqlApi(object):
         self.dbi = dbFactory.connect()
         self.dbFormatter = DBFormatter(logger,self.dbi)
 
-        self.sqlDict = {'Block':
+        self.sqlDict = {'AcquisitionEras':
+                        """SELECT DISTINCT PCD.AQUISITIONERA acquisition_era_name,
+                        NULL description,
+                        NULL create_by,
+                        NULL creation_date
+                        FROM %s.PROCESSEDDATASET PCD
+                        WHERE AQUISITIONERA IS NOT NULL
+                        ORDER BY PCD.AQUISITIONERA
+                        """ % (owner),
+                        ##############################################
+                        'ApplicationExecutables':
+                        """SELECT AE.ID app_exec_id,
+                        AE.EXECUTABLENAME app_name
+                        FROM %s.APPEXECUTABLE AE
+                        ORDER BY app_exec_id
+                        """ % (owner),
+                        ##############################################
+                        'Block':
                         """SELECT BL.ID block_id,
                         BL.NAME block_name,
                         BL.PATH dataset,
@@ -43,13 +58,22 @@ class DBS2SqlApi(object):
                         BL.LASTMODIFICATIONDATE last_modification_date ,
                         BL.CREATIONDATE creation_date,
                         BL.DATASET dataset_id, '/' || PD.NAME || '/' || DS.NAME || '/' || DT.NAME dataset,
-                        'unknown' origin_site_name
+                        'UNKNOWN' origin_site_name
                         FROM %s.BLOCK BL
                         JOIN %s.PERSON PS1 ON BL.CREATEDBY=PS1.ID
                         JOIN %s.PERSON PS2 ON BL.LASTMODIFIEDBY=PS2.ID
                         JOIN %s.PROCESSEDDATASET DS ON DS.ID=BL.DATASET
                         JOIN %s.PRIMARYDATASET PD on DS.PRIMARYDATASET=PD.ID
-                        JOIN %s.DATATIER DT ON DS.DATATIER=DT.ID""" % (owner,owner,owner,owner,owner,owner),
+                        JOIN %s.DATATIER DT ON DS.DATATIER=DT.ID
+                        ORDER BY block_id
+                        """ % (owner,owner,owner,owner,owner,owner),
+                        ##############################################
+                        'BlockParents':
+                        """SELECT BP.THISBLOCK this_block_id,
+                        BP.ITSPARENT parent_block_id
+                        FROM %s.BLOCKPARENT BP
+                        ORDER BY BP.THISBLOCK,BP.ITSPARENT
+                        """ % (owner),
                         ##############################################
                         'Dataset':
                         """SELECT DS.ID dataset_id,
@@ -72,7 +96,33 @@ class DBS2SqlApi(object):
                         JOIN %s.PROCDSSTATUS ST ON ST.ID=DS.STATUS
                         JOIN %s.PERSON PS1 ON DS.CREATEDBY=PS1.ID
                         JOIN %s.PERSON PS2 ON DS.LASTMODIFIEDBY=PS2.ID
-                        JOIN %s.PRIMARYDSTYPE PT ON PT.ID=PD.TYPE""" % (owner,owner,owner,owner,owner,owner,owner,owner),
+                        JOIN %s.PRIMARYDSTYPE PT ON PT.ID=PD.TYPE
+                        ORDER BY dataset_id
+                        """ % (owner,owner,owner,owner,owner,owner,owner,owner),
+                        ##############################################
+                        ## Some datatypes are not existing anymore in DBS3
+                        'DatasetAccessTypes':
+                        """SELECT PDS.ID dataset_access_type_id,
+                        PDS.STATUS dataset_access_type
+                        FROM %s.PROCDSSTATUS PDS
+                        WHERE PDS.ID!=2 AND PDS.ID!=61
+                        ORDER BY dataset_access_type_id
+                        """ % (owner),
+                        ##############################################
+                        'DatasetOutputModConfigs':
+                        """SELECT PA.ID ds_output_mod_conf_id,
+                        PA.DATASET dataset_id,
+                        PA.ALGORITHM output_mod_config_id
+                        FROM %s.PROCALGO PA
+                        ORDER BY ds_output_mod_conf_id
+                        """ % (owner),
+                        ##############################################
+                        'DatasetParents':
+                        """SELECT DP.THISDATASET this_dataset_id,
+                        DP.ITSPARENT parent_dataset_id
+                        FROM %s.PROCDSPARENT DP
+                        ORDER BY this_dataset_id,parent_dataset_id
+                        """ % (owner),
                         ##############################################
                         'DataTier':
                         """SELECT DT.ID data_tier_id,
@@ -80,43 +130,52 @@ class DBS2SqlApi(object):
                         PS.DISTINGUISHEDNAME create_by,
                         DT.CREATIONDATE creation_date
                         FROM %s.DATATIER DT
-                        JOIN %s.PERSON PS ON PS.ID=DT.CREATEDBY""" % (owner,owner),
+                        JOIN %s.PERSON PS ON PS.ID=DT.CREATEDBY
+                        ORDER BY data_tier_id
+                        """ % (owner,owner),
                         ##############################################
-                        'FileMinMaxCount':
-                        """SELECT MIN(FS.ID) MINIMUM, MAX(FS.ID) MAXIMUM, COUNT(*) COUNT FROM %s.FILES FS""" % (owner),
+                        'FileDataTypes':
+                        """SELECT FDT.ID file_type_id,
+                        FDT.TYPE file_type
+                        FROM %s.FILETYPE FDT
+                        ORDER BY file_type_id
+                        """ % (owner),
                         ##############################################
-                        'Files':
-                        """SELECT
-                        FS.ID file_id,
-                        FS.LOGICALFILENAME logical_file_name,
-                        BL.NAME block_name,
-                        FS.CHECKSUM check_sum,
-                        FS.NUMBEROFEVENTS event_count,
-                        FS.FILESIZE file_size,
-                        FS.FILETYPE file_type_id,
-                        FT.TYPE file_type,
-                        FS.VALIDATIONSTATUS is_file_valid,
-                        PS1.DISTINGUISHEDNAME create_by,
-                        PS2.DISTINGUISHEDNAME last_modified_by,
-                        FS.LASTMODIFICATIONDATE last_modification_date,
-                        FS.CREATIONDATE creation_date,
-                        FS.FILEBRANCH branch_hash_id,
-                        FS.AUTOCROSSSECTION auto_cross_section,
-                        FS.ADLER32,
-                        FS.MD5,
-                        '/' || PD.NAME || '/' || DS.NAME || '/' || DT.NAME dataset,
-                        FS.DATASET dataset_id,
-                        FS.BLOCK block_id
-                        FROM %s.FILES FS
-                        JOIN %s.PROCESSEDDATASET DS ON DS.ID=FS.DATASET
-                        JOIN %s.PRIMARYDATASET PD on DS.PRIMARYDATASET=PD.ID
-                        JOIN %s.DATATIER DT ON DS.DATATIER=DT.ID
-                        JOIN %s.PERSON PS1 ON FS.CREATEDBY=PS1.ID
-                        JOIN %s.PERSON PS2 ON FS.LASTMODIFIEDBY=PS2.ID
-                        JOIN %s.BLOCK BL ON FS.BLOCK=BL.ID
-                        JOIN %s.FILETYPE FT ON FT.ID=FS.FILETYPE
-                        WHERE FS.ID BETWEEN :minID AND :maxID
-                        """ % (owner,owner,owner,owner,owner,owner,owner,owner),
+                        'OutputModule':
+                        """
+                        SELECT AC.ID output_mod_config_id,
+                        APPEX.EXECUTABLENAME app_name,
+                        APPVER.VERSION release_version,
+                        AC.PARAMETERSETID parameter_set_hash_id,
+                        TO_CHAR(AC.APPLICATIONFAMILY) output_module_label,
+                        'UNKNOWN' global_tag,
+                        NULL scenario,
+                        QPS.HASH pset_hash,
+                        QPS.NAME pset_name,
+                        AC.CREATIONDATE creation_date,
+                        PS.DISTINGUISHEDNAME create_by
+                        FROM %s.ALGORITHMCONFIG AC
+                        JOIN %s.APPEXECUTABLE APPEX ON APPEX.ID=AC.EXECUTABLENAME
+                        JOIN %s.APPVERSION APPVER ON APPVER.ID=AC.APPLICATIONVERSION
+                        JOIN %s.PERSON PS ON PS.ID=AC.CREATEDBY
+                        JOIN %s.QUERYABLEPARAMETERSET QPS ON QPS.ID=AC.PARAMETERSETID
+                        ORDER BY output_mod_config_id
+                        """ % (owner,owner,owner,owner,owner),
+                        ##############################################
+                        'ParametersetHashes':
+                        """SELECT QP.ID parameter_set_hash_id,
+                        QP.HASH pset_hash,
+                        QP.NAME
+                        FROM %s.QUERYABLEPARAMETERSET QP
+                        ORDER BY parameter_set_hash_id
+                        """ % (owner),
+                        ##############################################
+                        'PhysicsGroups':
+                        """SELECT PG.ID physics_group_id,
+                        PG.PHYSICSGROUPNAME physics_group_name
+                        FROM %s.PHYSICSGROUP PG
+                        ORDER BY physics_group_id
+                        """ % (owner),
                         ##############################################
                         'PrimaryDS':
                         """SELECT PD.ID primary_ds_id,
@@ -126,36 +185,106 @@ class DBS2SqlApi(object):
                         PS.DISTINGUISHEDNAME create_by
                         FROM %s.PRIMARYDATASET PD
                         JOIN %s.PERSON PS ON PS.ID=PD.CREATEDBY
-                        JOIN %s.PRIMARYDSTYPE PT ON PT.ID=PD.TYPE""" % (owner,owner,owner)
+                        JOIN %s.PRIMARYDSTYPE PT ON PT.ID=PD.TYPE
+                        ORDER BY primary_ds_id
+                        """ % (owner,owner,owner),
+                        ##############################################
+                        'PrimaryDSTypes':
+                        """SELECT PDST.ID primary_ds_type_id,
+                        PDST.TYPE primary_ds_type
+                        FROM %s.PRIMARYDSTYPE PDST
+                        ORDER BY primary_ds_type_id
+                        """ % (owner),
+                        ##############################################
+                        'ProcessedDatasets':
+                        """SELECT DISTINCT PCD.NAME processed_ds_name
+                        FROM %s.PROCESSEDDATASET PCD
+                        ORDER BY PCD.NAME
+                        """ % (owner),
+                        ##############################################
+                        'ReleaseVersions':
+                        """SELECT RV.ID release_version_id,
+                        RV.VERSION release_version
+                        FROM %s.APPVERSION RV
+                        ORDER BY release_version_id
+                        """ % (owner)
                         }
                                 
-        self.sqlPrimaryKey = {'Block':'block_id',
+        self.sqlPrimaryKey = {'AcquisitionEras':'acquisition_era_name',
+                              'ApplicationExecutables':'app_exec_id',
+                              'Block':'block_id',
+                              'BlockParents':'this_block_id',
                               'Dataset':'dataset_id',
+                              'DatasetAccessTypes':'dataset_access_type_id',
+                              'DatasetOutputModConfigs':'ds_output_mod_conf_id',
+                              'DatasetParents':'this_dataset_id',
+                              'DatasetRuns':'dataset_run_id',
                               'DataTier':'data_tier_id',
-                              'FileMinMaxCount':'minimum',
                               'Files':'file_id',
-                              'PrimaryDS':'primary_ds_id'}
+                              'FileDataTypes':'file_type_id',
+                              'OutputModule':'output_mod_config_id',
+                              'ParametersetHashes':'parameter_set_hash_id',
+                              'PhysicsGroups':'physics_group_id',
+                              'PrimaryDS':'primary_ds_id',
+                              'PrimaryDSTypes':'primary_ds_type_id',
+                              'ProcessedDatasets':'processed_ds_name',
+                              'ReleaseVersions':'release_version_id'}
+
+    def acquisitionEras(self,sort=True):
+        return self._queryDB('AcquisitionEras',sort=sort)
+
+    def applicationExecutables(self,sort=True):
+        return self._queryDB('ApplicationExecutables',sort=sort)
 
     def blockList(self,sort=True):
-        return modifyBlocklist(self._queryDB('Block',sort=sort))
+        return self._queryDB('Block',sort=sort)
+
+    def blockParents(self,sort=True):
+        return self._queryDB('BlockParents',sort=sort)
 
     def datasetList(self,sort=True):
         #ToDo mapping DBS2 PROCDSSTATUS to is_dataset_valid and dataset_access_type in DBS3
         #ToDo compare GlobalTAG when comparing output_module_configs
         return modifyDatasetList(self._queryDB('Dataset',sort=sort))
 
+    def datasetAccessTypes(self,sort=True):
+        return self._queryDB('DatasetAccessTypes',sort=sort)
+
+    def datasetOutputModConfigs(self,sort=True):
+        return self._queryDB('DatasetOutputModConfigs',sort=sort)
+    
+    def datasetParents(self,sort=True):
+        return self._queryDB('DatasetParents',sort=sort)
+
+    def datasetRuns(self,sort=True):
+        return self._queryDB('DatasetRuns',sort=sort)
+    
     def dataTierList(self,sort=True):
         return self._queryDB('DataTier',sort=sort)
 
-    def fileMinMaxCount(self,sort=False):
-        return self._queryDB('FileMinMaxCount',sort=sort)
+    def fileDataTypes(self,sort=True):
+        return self._queryDB('FileDataTypes',sort=sort)        
 
-    def fileList(self,minimum,maximum,sort=True):
-        binds = {'minID':minimum,'maxID':maximum}
-        return self._queryDB('Files',binds=binds,sort=sort)
+    def outputModuleConfig(self,sort=True):
+        return self._queryDB('OutputModule',sort=sort)
+
+    def parametersetHashes(self,sort=True):
+        return self._queryDB('ParametersetHashes',sort=sort)
+
+    def physicsGroups(self,sort=True):
+        return self._queryDB('PhysicsGroups',sort=sort)
     
     def primaryDatasetList(self,sort=True):
         return self._queryDB('PrimaryDS',sort=sort)
+
+    def primaryDSTypes(self,sort=True):
+        return self._queryDB('PrimaryDSTypes',sort=sort)
+
+    def processedDatasets(self,sort=True):
+        return self._queryDB('ProcessedDatasets',sort=sort)
+
+    def releaseVersions(self,sort=True):
+        return self._queryDB('ReleaseVersions',sort=sort)
 
     def _queryDB(self,query,binds={},sort=True):
         connection = self.dbi.connection()
--- DBS2To3Migration/test/ValidateMigration_t.py
+++ DBS2To3Migration/test/ValidateMigration_t.py
@@ -1,4 +1,8 @@
-import itertools
+"""
+Unittests to validate the DBS2 to DBS3 migration
+Author: Manuel Giffels <giffels@physik.rwth-aachen.de>
+"""
+
 import logging
 import unittest
 
@@ -42,89 +46,189 @@ def listComparision(resultsDBS2,resultsDBS3):
                     diffList.append(resultDBS3)
             return False
 
+def diffKeys(resultDBS2,resultDBS3):
+    for i in resultDBS3.keys():
+        if resultDBS2.has_key(i):
+            if resultDBS3[i]==resultDBS2[i]:
+                pass
+            else:
+                print "%s differs: %s,%s" % (i,resultDBS2[i],resultDBS3[i])
+        else:
+            print "DBS2 has no key %s" % (i)
+
 class CompareDBS2ToDBS3(unittest.TestCase):
     
     def setUp(self):
-        ownerDBS3 = 'owner'
-        connectUrlDBS3 = 'oracle://owner:passwd@instance'
+        #ownerDBS3 = 'owner'
+        #connectUrlDBS3 = 'oracle://owner:passwd@instance'
 
-        ownerDBS2 = 'owner'
-        connectUrlDBS2 = 'oracle://owner:passwd@instance'
+        #ownerDBS2 = 'owner'
+        #connectUrlDBS2 = 'oracle://owner:passwd@instance'
+
+        ownerDBS3 = 'cms_dbs3_local2'
+        connectUrlDBS3 = 'oracle://cms_dbs3_local2:ylxnseyjh?@int2r_lb'
+
+        ownerDBS2 = 'CMS_DBS_PROD_GLOBAL'
+        connectUrlDBS2 = 'oracle://cms_dbs_prod_global:dbs3test112010@int2r_lb'
 
         logger = logging.getLogger()
 
         self.dbs2sqlapi = DBS2SqlApi(logger,connectUrlDBS2,ownerDBS2)
-        self.dbs3sqlapi = DBS3SqlApi(logger,connectUrlDBS3,ownerDBS3)
-        
-    def test_blocks(self):
-        resultsDBS2 = self.dbs2sqlapi.blockList(sort=True)
-        resultsDBS3 = self.dbs3sqlapi.blockList(sort=True)
-                    
+        self.dbs3sqlapi = DBS3SqlApi(logger,connectUrlDBS3,ownerDBS3,ownerDBS2)
+
+    def test_acquisition_eras(self):
+        resultsDBS2 = self.dbs2sqlapi.acquisitionEras(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.acquisitionEras(sort=False)
+
         self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
-     
-    def test_datasets(self):
-        resultsDBS2 = self.dbs2sqlapi.datasetList(sort=True)
-        resultsDBS3 = self.dbs3sqlapi.datasetList(sort=True)
+
+    def test_application_executables(self):
+        resultsDBS2 = self.dbs2sqlapi.applicationExecutables(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.applicationExecutables(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_blocks(self):
+        resultsDBS2 = self.dbs2sqlapi.blockList(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.blockList(sort=True)#not sorted in SQL Query
 
         self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
         
+    def test_block_parents(self):
+        resultsUnion = self.dbs3sqlapi.blockParents(sort=False)
+                    
+        self.assertEqual(len(resultsUnion),0)
+    
+    def test_datasets(self):
+        resultsDBS2 = self.dbs2sqlapi.datasetList(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.datasetList(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_dataset_access_types(self):
+        resultsDBS2 = self.dbs2sqlapi.datasetAccessTypes(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.datasetAccessTypes(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_dataset_output_mod_configs(self):
+        resultsDBS2 = self.dbs2sqlapi.datasetOutputModConfigs(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.datasetOutputModConfigs(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_dataset_parents(self):
+        resultsDBS2 = self.dbs2sqlapi.datasetParents(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.datasetParents(sort=False)
+        
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_dataset_runs(self):
+        resultsDBS2 = self.dbs2sqlapi.datasetRuns(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.datasetRuns(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+    
     def test_data_tiers(self):
-        resultsDBS2 = self.dbs2sqlapi.dataTierList(sort=True)
-        resultsDBS3 = self.dbs3sqlapi.dataTierList(sort=True)
+        resultsDBS2 = self.dbs2sqlapi.dataTierList(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.dataTierList(sort=True)#not sorted in SQL Query
 
         self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
 
     def test_files(self):
-        minmaxcountDBS2 = self.dbs2sqlapi.fileMinMaxCount(sort=False)[0]
-        minmaxcountDBS3 = self.dbs3sqlapi.fileMinMaxCount(sort=False)[0]
-
-        stepWidth = 100000
-
-        results = []
-
-        step = 0
-
-        estimatedSteps = ((minmaxcountDBS3['maximum']-minmaxcountDBS2['minimum'])/stepWidth)+1
-        
-        while True:
-
-            minIDDBS2 = minmaxcountDBS2['minimum']+step*stepWidth
-            maxIDDBS2 = min(minIDDBS2+stepWidth,minmaxcountDBS2['maximum'])
-
-            minIDDBS3 = minmaxcountDBS3['minimum']+step*stepWidth
-            maxIDDBS3 = min(minIDDBS3+stepWidth,minmaxcountDBS3['maximum'])
-
-            resultsDBS2 = self.dbs2sqlapi.fileList(minimum=minIDDBS2,maximum=maxIDDBS2,sort=True)
-            resultsDBS3 = self.dbs3sqlapi.fileList(minimum=minIDDBS3,maximum=maxIDDBS3,sort=True)
-
-            print len(resultsDBS2),len(resultsDBS3)
-
-            results.append(listComparision(resultsDBS2,resultsDBS3))
-
-            print "%s/%s completed" % (step,estimatedSteps)
-
-            step += 1
-
-            if minIDDBS2 > minmaxcountDBS2['maximum'] or minIDDBS3 > minmaxcountDBS3['maximum']:
-                break
+        resultsUnion = self.dbs3sqlapi.fileList(sort=False)
             
-        self.assertTrue(0 not in results)
-        
+        self.assertEqual(len(resultsUnion),0)
 
+    def test_file_data_types(self):
+        resultsDBS2 = self.dbs2sqlapi.fileDataTypes(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.fileDataTypes(sort=False)
+
+        diffKeys(resultsDBS2[0],resultsDBS3[0])
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_file_lumis(self):
+        resultsUnion = self.dbs3sqlapi.fileLumis(sort=False)
+                    
+        self.assertEqual(len(resultsUnion),0)
+        
+    def test_file_output_mod_configs(self):
+        resultsUnion = self.dbs3sqlapi.fileOutputModConfigs(sort=False)
+                    
+        self.assertEqual(len(resultsUnion),0)
+    
+    def test_file_parents(self):
+        resultsUnion = self.dbs3sqlapi.fileParents(sort=False)
+                    
+        self.assertEqual(len(resultsUnion),0)
+    
+    def test_output_module_config(self):
+        resultsDBS2 = self.dbs2sqlapi.outputModuleConfig(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.outputModuleConfig(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_parameter_set_hashes(self):
+        resultsDBS2 = self.dbs2sqlapi.parametersetHashes(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.parametersetHashes(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_physics_groups(self):
+        resultsDBS2 = self.dbs2sqlapi.physicsGroups(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.physicsGroups(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
 
     def test_primary_datasets(self):
-        resultsDBS2 = self.dbs2sqlapi.primaryDatasetList(sort=True)
-        resultsDBS3 = self.dbs3sqlapi.primaryDatasetList(sort=True)
+        resultsDBS2 = self.dbs2sqlapi.primaryDatasetList(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.primaryDatasetList(sort=True)#not sorted in SQL Query
 
-        self.assertTrue(listComparision2(resultsDBS2,resultsDBS3))
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_primary_ds_types(self):
+        resultsDBS2 = self.dbs2sqlapi.primaryDSTypes(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.primaryDSTypes(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+        
+    def test_processed_datasets(self):
+        resultsDBS2 = self.dbs2sqlapi.processedDatasets(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.processedDatasets(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
+
+    def test_release_versions(self):
+        resultsDBS2 = self.dbs2sqlapi.releaseVersions(sort=False)
+        resultsDBS3 = self.dbs3sqlapi.releaseVersions(sort=False)
+
+        self.assertTrue(listComparision(resultsDBS2,resultsDBS3))
 
 if __name__ == '__main__':
     TestSuite = unittest.TestSuite()
+    TestSuite.addTest(CompareDBS2ToDBS3('test_acquisition_eras'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_application_executables'))
     TestSuite.addTest(CompareDBS2ToDBS3('test_blocks'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_block_parents'))
     TestSuite.addTest(CompareDBS2ToDBS3('test_data_tiers'))
     TestSuite.addTest(CompareDBS2ToDBS3('test_datasets'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_dataset_access_types'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_dataset_output_mod_configs'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_dataset_parents'))
     TestSuite.addTest(CompareDBS2ToDBS3('test_files'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_file_data_types'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_file_lumis'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_file_output_mod_configs'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_file_parents'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_output_module_config'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_parameter_set_hashes'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_physics_groups'))
     TestSuite.addTest(CompareDBS2ToDBS3('test_primary_datasets'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_primary_ds_types'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_processed_datasets'))
+    TestSuite.addTest(CompareDBS2ToDBS3('test_release_versions'))
+    
     unittest.TextTestRunner(verbosity=2).run(TestSuite)
     
 
