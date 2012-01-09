8a341d12e240573eaae6a06ce15d9b274f58b2a2
yuyi <yuyi@fnal.gov>
2012-01-09 16:19:41 +0000

    add missing files for AcauisitionEra dao
    From: yuyi <yuyi@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DBS/trunk@14905 4525493e-7705-40b1-a816-d608a930855b

---
+++ Server/Python/src/dbs/dao/Oracle/AcquisitionEra/List_CI.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+This module provides Acquisition.Lis-CI data access object.
+"""
+
+from WMCore.Database.DBFormatter import DBFormatter
+from dbs.utils.dbsExceptionHandler import dbsExceptionHandler
+
+class List_CI(DBFormatter):
+    """
+    DataTier List DAO class.
+    """
+    def __init__(self, logger, dbi, owner):
+        """
+        Add schema owner and sql.
+        """
+        DBFormatter.__init__(self, logger, dbi)
+        self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
+        self.sql = \
+"""
+SELECT AE.ACQUISITION_ERA_NAME, AE.CREATION_DATE, AE.CREATE_BY, AE.DESCRIPTION   
+FROM %sACQUISITION_ERAS AE 
+""" % (self.owner)
+
+    def execute(self, conn, acquisitionEra="", transaction = False):
+	if not conn:
+	    dbsExceptionHandler("dbsException-db-conn-failed","Oracle/AcquisitionEra/List. Expects db connection from upper layer.")
+        sql = self.sql
+	binds={}
+	if acquisitionEra:
+            op = ("=", "like")["%" in acquisitionEra]
+	    sql += "WHERE AE.ACQUISITION_ERA_NAME %s :acquisitionEra" %op 
+	    binds = {"acquisitionEra":acquisitionEra}
+        self.dbi.processData("alter session set NLS_COMP=LINGUISTIC", None, conn, transaction)
+        self.dbi.processData("alter session set NLS_SORT=BINARY_CI", None, conn, transaction)
+        result = self.dbi.processData(sql, binds, conn, transaction)
+        plist = self.formatDict(result)
+        self.dbi.processData("alter session set NLS_COMP=BINARY", None, conn, transaction)
+        self.dbi.processData("alter session set NLS_SORT=BINARY", None, conn, transaction)
+        return plist
---
+++ Server/Python/src/dbs/dao/Oracle/AcquisitionEra/UpdateEndDate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+This module provides acquisitionEra.UpdateEndDate data access object.
+"""
+
+from WMCore.Database.DBFormatter import DBFormatter
+from dbs.utils.dbsExceptionHandler import dbsExceptionHandler
+
+class UpdateEndDate(DBFormatter):
+    """
+    Acquisition UpdateEndDate DAO class.
+    """
+    def __init__(self, logger, dbi, owner):
+        """
+        Add schema owner and sql.
+        """
+        DBFormatter.__init__(self, logger, dbi)
+	self.owner = "%s." % owner if not owner in ("", "__MYSQL__") else ""
+        self.sql = """UPDATE %sACQUISITION_ERAS SET END_DATE=:end_date where acquisition_era_name=:acquisition_era_name""" %  self.owner 
+        
+    def execute(self, conn, acquisition_era_name,end_date, transaction = False):
+        """
+        for a given block_id
+        """	
+	if not conn:
+	    dbsExceptionHandler("dbsException-db-conn-failed", "dbs/dao/Oracle/AcquisitionEra/updateEndDate expects db connection from upper layer.")
+        binds = { "acquisition_era_name" :acquisition_era_name  , "end_date" : end_date  }
+        result = self.dbi.processData(self.sql, binds, conn, transaction)
