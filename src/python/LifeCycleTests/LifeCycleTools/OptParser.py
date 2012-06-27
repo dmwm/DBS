e3d869b8309f8800db51e253c750caa601905f65
Manuel Giffels <giffels@gmail.com>
2012-06-27 11:36:14 +0200

    Fix OptParser bug from decompiling pyc

--- src/python/LifeCycleTests/LifeCycleTools/OptParser.py
+++ src/python/LifeCycleTests/LifeCycleTools/OptParser.py
@@ -5,8 +5,8 @@ def get_command_line_options(executable_name, arguments):
     parser.add_option('-i', '--in', type='string', dest='input', help='input payload')
     parser.add_option('-o', '--out', type='string', dest='output', help='output payload')
     options, args = parser.parse_args()
-    if options.input:
-        options.output or parser.print_help()
+    if not (options.input and options.output):
+        parser.print_help()
         parser.error('You need to provide following options, --in=input_payload.json and --out=output_payload.json')
     return options
 
