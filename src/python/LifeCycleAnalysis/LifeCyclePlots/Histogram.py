0a6e0004671a4ab966eb09cdeb5e7df9c331efda
Manuel Giffels <giffels@gmail.com>
2012-07-20 16:01:14 +0200

    Adding own stats style

--- src/python/LifeCycleAnalysis/LifeCyclePlots/Histogram.py
+++ src/python/LifeCycleAnalysis/LifeCyclePlots/Histogram.py
@@ -1,8 +1,8 @@
-from ROOT import TCanvas, TFile, TH1F, TH2F
+from ROOT import gStyle, TCanvas, TFile, TH1F, TH2F, TPaveStats
 
 class BasicHisto(object):
     def __init__(self, name, title, fill_fkt, condition, label,
-                 log, color, draw_options, add_options):
+                 log, color, stats, draw_options, add_options):
         self._name = name
         self._title = title
         self._fill_fkt = fill_fkt
@@ -10,6 +10,7 @@ class BasicHisto(object):
         self._label = label
         self._log = log
         self._color = color
+        self._stats = stats
         self._draw_options = draw_options
         self._add_options = add_options
 
@@ -29,12 +30,24 @@ class BasicHisto(object):
         if self._condition(data):
             self._histogram.Fill(*self._fill_fkt(self, data))
 
+    def create_canvas(self):
+        self._canvas = TCanvas(self._name, self._name)
+
+        if self._stats:
+            self._canvas.SetRightMargin(0.183908)
+            self._canvas.SetTopMargin(0.06991526)
+            gStyle.SetOptStat("mreou");
+
+        gStyle.SetCanvasBorderMode(0);
+        gStyle.SetPadBorderMode(0);
+
+        self._canvas.cd()
+
     def draw(self):
         # create canvas to draw histogram
         if not getattr(self,'_canvas', None):
-            self._canvas = TCanvas(self._name, self._name)
+            self.create_canvas()
 
-        self._canvas.cd()
         # set axis label
         self._histogram.GetXaxis().SetTitle(self._label.get('x',""));
         self._histogram.GetYaxis().SetTitle(self._label.get('y',""));
@@ -59,6 +72,12 @@ class BasicHisto(object):
 
             callable_fkt(*value)
 
+        #set my stat style
+        if self._stats:
+            self.set_stats_style()
+        else:
+            self._histogram.SetStats(False)
+
         #draw histogram to canvas
         self._histogram.Draw(self._draw_options)
 
@@ -69,6 +88,19 @@ class BasicHisto(object):
             print "You have to draw histograms before saving."
             pass
 
+    def set_stats_style(self):
+        stat_box = TPaveStats(0.816092,0.7415254,0.9971264,0.9300847,"brNDC")
+        stat_box.SetBorderSize(1);
+        stat_box.SetFillColor(0);
+        stat_box.SetTextAlign(12);
+        stat_box.SetTextFont(42);
+        stat_box.SetName("stats_%s" % (self._name))
+        stat_box.Draw()
+        self._histogram.GetListOfFunctions().Add(stat_box)
+        stat_box.SetParent(self._histogram.GetListOfFunctions())
+        self._canvas.Modified()
+        self._canvas.Update()
+
 class Histo1D(BasicHisto):
     def __init__(self, name, title, xnbins, xmin, xmax,
                  x_value_to_fill,
@@ -77,9 +109,10 @@ class Histo1D(BasicHisto):
                  label={'x' : "", 'y' : ""},
                  log={'x' : False, 'y' : False},
                  color={'line':1, 'fill':0}, #ROOT Color Codes
+                 stats=True,
                  draw_options="",
                  add_options={}):
-        super(Histo1D, self).__init__(name, title, fill_fkt, condition, label, log, color, draw_options, add_options)
+        super(Histo1D, self).__init__(name, title, fill_fkt, condition, label, log, color, stats, draw_options, add_options)
         self._histogram = TH1F(name, title, xnbins, xmin, xmax)
         self._x_value_to_fill = x_value_to_fill
 
@@ -93,9 +126,10 @@ class Histo2D(BasicHisto):
                  label={'x' : "", 'y' : ""},
                  log={'x' : False, 'y' : False},
                  color={'line':1, 'fill':0}, #ROOT Color Codes
+                 stats=True,
                  draw_options="",
                  add_options={}):
-        super(Histo2D, self).__init__(name, title, fill_fkt, condition, label, log, color, draw_options, add_options)
+        super(Histo2D, self).__init__(name, title, fill_fkt, condition, label, log, color, stats, draw_options, add_options)
         self._histogram = TH2F(name, title, xnbins, xmin, xmax, ynbins, ymin, ymax)
         self._x_value_to_fill = x_value_to_fill
         self._y_value_to_fill = y_value_to_fill
