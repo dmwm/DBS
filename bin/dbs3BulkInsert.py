594cc4146ed863e6ea116cdc37ee5a48f140f1bb
Manuel Giffels <giffels@gmail.com>
2012-08-13 14:37:50 +0200

    Adding dbs3BulkInsert.py for LifeCycleTests

---
+++ bin/dbs3BulkInsert.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from LifeCycleTests.LifeCycleTools.APIFactory import create_api
+from LifeCycleTests.LifeCycleTools.PayloadHandler import PayloadHandler, increase_interval
+from LifeCycleTests.LifeCycleTools.Timing import TimingStat
+from LifeCycleTests.LifeCycleTools.OptParser import get_command_line_options
+from LifeCycleTests.LifeCycleTools.StatsClient import StatsPipeClient
+
+import os
+import sys
+import tempfile
+
+options = get_command_line_options(__name__, sys.argv)
+
+stat_output_dir = os.path.dirname(options.input)
+
+config = {'url':os.environ.get("DBS_WRITER_URL","https://cmsweb.cern.ch/dbs/int/global/DBSWriter/")}
+
+api = create_api('DbsApi',config=config)
+
+payload_handler = PayloadHandler()
+
+payload_handler.load_payload(options.input)
+
+named_pipe = payload_handler.payload['workflow']['NamedPipe']
+
+stat_client = StatsPipeClient(named_pipe)
+
+initial = payload_handler.payload['workflow']['dataset']
+
+## list primary data type
+timing = {'stats':{'query' : initial, 'api' : 'insertBulkBlock'}}
+with TimingStat(timing, stat_client) as timer:
+    ds_type = api.insertBulkBlock()
+
+request_processing_time, request_time = api.requestTimingInfo
+timer.update_stats({'server_request_timing' : float(request_processing_time)/1000000.0,
+                    'server_request_timestamp' : float(request_time)/1000000.0,
+                    'request_content_length' : int(api.requestContentLength)})
+
+timer.stat_to_server()
+p = payload_handler.clone_payload()
+payload_handler.append_payload(p)
+
+payload_handler.save_payload(options.output)
