24165e49bb2c6d501e0bc6fff0b1cf9f5071da92
Manuel Giffels <giffels@gmail.com>
2013-02-04 16:36:37 +0100

    Add dbs3dasComparision template

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ systems = \
     'srcdir': 'src/python',
     'bin': ['bin/dbs3BulkInsert.py',
             'bin/dbs3CrabWorkflow.py',
+            'bin/dbs3dasComparision.py',
             'bin/dbs3IntroduceFailures.py',
             'bin/dbs3GetBlocks.py',
             'bin/dbs3GetDatasets.py',
