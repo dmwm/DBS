d9760b44790c9a8e65fdf4cfb14085dbe891c25d
Manuel Giffels <giffels@gmail.com>
2013-03-06 16:41:27 +0100

    Add repititions for DAS stress-test

--- bin/dbs3DASGetQueries.py
+++ bin/dbs3DASGetQueries.py
@@ -25,14 +25,17 @@ named_pipe = payload_handler.payload['workflow']['NamedPipe']
 stat_client = StatsPipeClient(named_pipe)
 
 data_file = payload_handler.payload['workflow']['DASQueryDataFile']
+num_of_repetitions = payload_handler.payload['workflow']['NRepetitions']
 
 data_file = os.path.join(os.getenv('DBS3_LIFECYCLE_ROOT'), 'data', data_file)
 
 with open(data_file, 'r') as f:
     queries = json.load(f)
+    events = ['DASAccess' for _ in xrange(num_of_repetitions)]
     for query in queries:
         p = payload_handler.clone_payload()
         p['workflow']['DASQuery'] = query
+        p['workflow']['Events'].extend(events)
         payload_handler.append_payload(p)
 
 payload_handler.save_payload(options.output)
