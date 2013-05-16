8ed8ed63152e273eef23ddb93638d58a49e7cc5f
Manuel Giffels <giffels@gmail.com>
2013-05-16 15:21:23 +0200

    Add input validation for datasets

--- src/python/dbs/utils/Validation.py
+++ src/python/dbs/utils/Validation.py
@@ -9,7 +9,6 @@ string_validation_rx = re.compile(r"^[-A-Za-z0-9_]+$")
 ###validation of datasets
 dataset_validation_rx = re.compile(r"^/(\*|[a-zA-Z\*][a-zA-Z0-9_\*\-]{0,100})(/(\*|[a-zA-Z0-9_\.\-\*]{1,100})){0,1}(/(\*|[A-Z\-\*]{1,50})){0,1}$")
 
-
 ###validation of logical file names
 lfnParts = {
     'era'           : r'([a-zA-Z0-9\-_]+)',
@@ -38,3 +37,21 @@ filename_validation_rx = re.compile(r"(%s|%s|%s|%s|%s|%s)" % (lfn_rx1, lfn_rx2, 
 
 ### validation of run
 run_validation_rx = re.compile(r"^[0-9\-]+$")
+
+### processing version
+processing_version_validation_rx = re.compile(r"^[0-9]+$")
+
+### acquisition_era
+acquisition_era_validation_rx = re.compile(r"[a-zA-Z][a-zA-Z0-9_]*$")
+
+### primary_ds_name
+primary_ds_name_validation_rx = re.compile(r"^[a-zA-Z][a-zA-Z0-9\-_]*$")
+
+### processed_ds_name
+processed_ds_name_validation_rx = re.compile(r"[a-zA-Z][a-zA-Z0-9_]*(\-[a-zA-Z0-9_]+){0,2}-v[0-9]*$")
+
+### data tier name
+data_tier_name_validation_rx = re.compile(r"[A-Z\-_]+")
+
+### release version
+release_version_validation_rx = re.compile("CMSSW(_\d+){3}(_[a-zA-Z0-9_]+)?$")
--- src/python/dbs/web/DBSDatasets.py
+++ src/python/dbs/web/DBSDatasets.py
@@ -8,8 +8,8 @@ from WMCore.REST.Validation import *
 
 from dbs.dao.Oracle.Dataset.List import List
 
-from dbs.utils.Validation import dataset_validation_rx
-from dbs.utils.DBSTransformInputType import transformInputType
+from dbs.utils.Validation import *
+from dbs.utils.DBSTransformInputType import transformInputType, parseRunRange, run_tuple
 
 class Datasets(RESTEntity):
     def __init__(self, app, api, config, mount):
@@ -22,6 +22,35 @@ class Datasets(RESTEntity):
         Validate input data
         """
         validate_strlist("datasets", param, safe, dataset_validation_rx)
+        validate_num("is_dataset_valid", param, safe, minval=0, maxval=1, optional=True)
+        validate_str("parent_dataset", param, safe, dataset_validation_rx, optional=True)
+        validate_str("release_version", param, safe, release_version_validation_rx, optional=True)
+        validate_str("pset_hash", param, safe, string_validation_rx, optional=True)
+        validate_str("app_name", param, safe, string_validation_rx, optional=True)
+        validate_str("output_module_label", param, safe, string_validation_rx, optional=True)
+        validate_str("processing_version", param, safe, processing_version_validation_rx, optional=True)
+        validate_str("acquisition_era", param, safe, acquisition_era_validation_rx, optional=True)
+        run_param = param.kwargs.get('run_num')
+        if isinstance(run_param, list):
+            ###to simplify input validation, convert every item to a string
+            param.kwargs['run_num'] = map(str, run_param)
+        validate_strlist("run_num", param, safe, run_validation_rx)
+        validate_str("physics_group_name", param, safe, string_validation_rx, optional=True)
+        validate_str("logical_file_name", param, safe, filename_validation_rx, optional=True)
+        validate_str("primary_ds_name", param, safe, primary_ds_name_validation_rx, optional=True)
+        validate_str("primary_ds_type", param, safe, string_validation_rx, optional=True)
+        validate_str("processed_ds_name", param, safe, processed_ds_name_validation_rx, optional=True)
+        validate_str("data_tier_name", param, safe, data_tier_name_validation_rx, optional=True)
+        validate_str("dataset_access_type", param, safe, string_validation_rx, optional=True)
+        validate_num("prep_id", param, safe, optional=True)
+        validate_str("create_by", param, safe, string_validation_rx, optional=True)
+        validate_str("last_modified_by", param, safe, string_validation_rx, optional=True)
+        validate_num("min_cdate", param, safe, optional=True, minval=0)
+        validate_num("max_cdate", param, safe, optional=True, minval=0)
+        validate_num("min_ldate", param, safe, optional=True, minval=0)
+        validate_num("max_ldate", param, safe, optional=True, minval=0)
+        validate_num("cdate", param, safe, optional=True, minval=0)
+        validate_num("ldate", param, safe, optional=True, minval=0)
 
     @restcall
     def get(self, datasets, is_dataset_valid, parent_dataset,
