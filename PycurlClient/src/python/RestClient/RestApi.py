53ff829127f34cd1c696ae81af5ea0777134a970
Manuel Giffels <giffels@gmail.com>
2014-01-30 11:11:55 +0100

    Adding possibilty to add additional curl options

--- PycurlClient/src/python/RestClient/RestApi.py
+++ PycurlClient/src/python/RestClient/RestApi.py
@@ -3,8 +3,13 @@ from RestClient.RequestHandling.HTTPRequest import HTTPRequest
 import pycurl
 
 class RestApi(object):
-    def __init__(self, auth=None, proxy=None):
+    def __init__(self, auth=None, proxy=None, additional_curl_options=None):
         self._curl = pycurl.Curl()
+
+        if additional_curl_options:
+            self._additional_curl_options = additional_curl_options
+        else:
+            self._additional_curl_options = {}
 
         if auth:
             auth.configure_auth(self._curl)
@@ -15,17 +20,25 @@ class RestApi(object):
         self._curl.close()
 
     def get(self, url, api, params={}, data=None, request_headers={}):
-        http_request = HTTPRequest(method='GET', url=url, api=api, params=params, data=data, request_headers=request_headers, additional_curl_options={})
+        http_request = HTTPRequest(method='GET', url=url, api=api, params=params,
+                                   data=data, request_headers=request_headers,
+                                   additional_curl_options=self._additional_curl_options)
         return http_request(self._curl)
 
     def post(self, url, api, params={}, data="", request_headers={}):
-        http_request = HTTPRequest(method='POST', url=url, api=api, params=params, data=data, request_headers=request_headers, additional_curl_options={})
+        http_request = HTTPRequest(method='POST', url=url, api=api, params=params, data=data,
+                                   request_headers=request_headers,
+                                   additional_curl_options=self._additional_curl_options)
         return http_request(self._curl)
 
     def put(self, url, api, params={}, data="", request_headers={}):
-        http_request = HTTPRequest(method='PUT', url=url, api=api, params=params, data=data, request_headers=request_headers, additional_curl_options={})
+        http_request = HTTPRequest(method='PUT', url=url, api=api, params=params, data=data,
+                                   request_headers=request_headers,
+                                   additional_curl_options=self._additional_curl_options)
         return http_request(self._curl)
 
     def delete(self, url, api, params={}, data=None, request_headers={}):
-        http_request = HTTPRequest(method='DELETE', url=url, api=api, params=params, data=data, request_headers=request_headers, additional_curl_options={})
+        http_request = HTTPRequest(method='DELETE', url=url, api=api, params=params, data=data,
+                                   request_headers=request_headers,
+                                   additional_curl_options=self._additional_curl_options)
         return http_request(self._curl)
