f1c7d317e2368490bd7f87a2d7da89381af51001
Yuyi Guo <yuyi@fnal.gov>
2014-05-29 11:19:50 -0500

    fix bug in auto generated sql, add new script to calcutae event per day for dataOps, add option for verfypeer of DBS Client.

--- Client/utils/DataOpsScripts/EventsPerDay.py
+++ Client/utils/DataOpsScripts/EventsPerDay.py
@@ -1,80 +1,81 @@
 #!/usr/bin/env python
-"""
-
+"""            
+               
  DBS 3 script to calculate accumulated events per day for a interval.
-
+                   
  python EventsPerDay.py -d '/*/*Fall13-POST*/GEN-SIM'
-
-"""
-
-import  sys,time
+                       
+""" 
+import  sys, time
 from optparse import OptionParser
 from time import gmtime
 from dbs.apis.dbsClient import DbsApi
 from dbs.exceptions.dbsClientException import dbsClientException
 
 def main():
-    usage="%prog <options>"
 
-    parser = OptionParser(usage=usage)
-    parser.add_option("-u", "--url", dest="url", help="DBS Instance url. default is https://cmsweb.cern.ch/dbs/prod/global/DBSReader", metavar="<url>")
-    parser.add_option("-l", "--length", dest="length", help="Number of days for calculate the accumated events. It is Optional, default is 30 days.", metavar="<length>")
-    parser.add_option("-d", "--dataset", dest="dataset", help="The dataset name for cacluate the events. Can be optional if datatier is used.", metavar="<dataset>")
-    parser.add_option("-t", "--datatier", dest="datatier", help="The datatier name for cacluate the events. Can be optional if dataset is used. In this version datatier is not supported yet.", metavar="<data_tier_name>")
-    parser.add_option("-a", "--access_type", dest="ds_access_type", help="Dataset access types: VALID, PRODUCTION or ALL(VALID+PRODUCTION). Default is ALL", metavar="<dataset_access_type>")
-   
-    parser.set_defaults(url="https://cmsweb.cern.ch/dbs/prod/global/DBSReader")
-    parser.set_defaults(length=30)
-    parser.set_defaults(ds_access_type="ALL")
-   
-    (opts, args) = parser.parse_args()
-    if not (opts.dataset or opts.datatier):
-	parser.print_help()
-	parser.error('either --dataset or --datatier is required')	
+	usage="%prog <options>"
 
-    data = opts.dataset
-    #seconds per day	
-    sdays = 86400
-    len = opts.length
-    now = time.time() 
-    #print now	
-    then = now - sdays*len
-    #print data
-    url = opts.url
-    api=DbsApi(url=url)
-    outputDataSets = []
-    if (opts.ds_access_type == "ALL"):
-	outputDataSetsValid = api.listDatasets(dataset=data,detail=1, dataset_access_type="VALID")
-	outputDataSetsProd = api.listDatasets(dataset=data,detail=1, dataset_access_type="PRODUCTION")
-	outputDataSets = outputDataSetsValid + outputDataSetsProd
-    elif (opts.ds_access_type == "VALID"):
-	outputDataSets = api.listDatasets(dataset=data,detail=1, dataset_access_type="VALID")
-    elif (opts.ds_access_type == "PRODUCTION"):
-	outputDataSets = api.listDatasets(dataset=data,detail=1, dataset_access_type="PRODUCTION")		
+	parser = OptionParser(usage=usage)
+	parser.add_option("-u", "--url", dest="url", help="DBS Instance url. default is https://cmsweb.cern.ch/dbs/prod/global/DBSReader", metavar="<url>")
+	parser.add_option("-l", "--length", dest="length", help="Number of days for calculate the accumated events. It is Optional, default is 30 days.", metavar="<length>")
+	parser.add_option("-d", "--dataset", dest="dataset", help="The dataset name for cacluate the events. Can be optional if datatier is used.", metavar="<dataset>")
+	parser.add_option("-t", "--datatier", dest="datatier", help="The datatier name for cacluate the events. Can be optional if dataset is used. In this version datatier is not supported yet.", metavar="<data_tier_name>")
+	parser.add_option("-a", "--access_type", dest="ds_access_type", help="Dataset access types: VALID, PRODUCTION or ALL(VALID+PRODUCTION). Default is ALL", metavar="<dataset_access_type>")
+	parser.set_defaults(url="https://cmsweb.cern.ch/dbs/prod/global/DBSReader")
+	parser.set_defaults(length=30)
+	parser.set_defaults(ds_access_type="ALL")
 
-    f = []
+	(opts, args) = parser.parse_args()
+	if not (opts.dataset or opts.datatier):
+		parser.print_help()
+		parser.error('either --dataset or --datatier is required')
 
-    for i in range(len):
-       f.append(0)
-
-    for dataset in outputDataSets:
-       inp=dataset['dataset']
-       ct = dataset['creation_date']
-       if ct > (then-30*sdays):
-           blocks = api.listBlocks(dataset=inp, detail=True)
-           for block in blocks:
-               reply= api.listBlockSummaries(block_name=block['block_name'])
-               neventsb= reply[0]['num_event']
-               reply=api.listFiles(block_name=block['block_name'],detail=True)
-               ct=reply[0]['last_modification_date']
-               for x in range (len):
-                   tnow = now - (len-x)*86400
-                   if ct > then and ct < tnow:
-                       f[x]=f[x]+ neventsb
-    for i in range(len):
-       print i, ":  ", f[i] 
-    sys.exit(0);
+	dataset	 = opts.dataset
+	#seconds per day    
+	sdays = 86400
+	lenth = int(opts.length)
+	now = time.time()
+	#now = 1391353032
+	then = now - sdays*lenth
+	url = opts.url
+	api=DbsApi(url=url)
+	outputDataSets = []
+    
+	f = [0 for x in range(lenth)]
+	min_cdate = int(then)
+	max_cdate = int(now)
+	if (opts.ds_access_type == "ALL"):
+		outputDataSetsValid = api.listDatasets(dataset=dataset, min_cdate=min_cdate-30*sdays, 
+                          max_cdate=max_cdate, dataset_access_type="VALID")
+		outputDataSetsProd = api.listDatasets(dataset=dataset, min_cdate=min_cdate-30*sdays,
+                          max_cdate=max_cdate, dataset_access_type="PRODUCTION")
+		outputDataSets = outputDataSetsValid + outputDataSetsProd
+	elif (opts.ds_access_type == "VALID"):
+		outputDataSets = api.listDatasets(dataset=dataset, min_cdate=min_cdate-30*sdays,
+                          max_cdate=max_cdate, dataset_access_type="VALID")
+	elif (opts.ds_access_type == "PRODUCTION"):
+		outputDataSets = api.listDatasets(dataset=dataset, min_cdate=min_cdate-30*sdays,
+                          max_cdate=max_cdate, dataset_access_type="PRODUCTION")
+	for dataset in outputDataSets:
+		outputBlocks = api.listBlocks(dataset=dataset["dataset"], detail=1, min_cdate=min_cdate, max_cdate=max_cdate)
+		blockList = []
+		blockCdate = {}
+		for block in outputBlocks:
+			blockList.append(block["block_name"])
+			blockCdate[block["block_name"]] = block["creation_date"]
+		blockSum = []
+		if blockList: 
+			blockSum = api.listBlockSummaries(block_name=blockList, detail=1)
+		for b in blockSum:
+			cdate= blockCdate[b["block_name"]]
+			day = int((now-cdate)/sdays)
+			f[day] = f[day] + b["num_evernt"] 
+	for i in range(lenth):
+		#print (lenth-1)-i, ":  ", f[i], "  ", sum(item['all'] for item in f[i:lenth]) 
+		print i, ": ", f[(lenth-1)-i], " ", sum(item for item in f[(lenth-1)-i:lenth])
+	sys.exit(0);
 
 if __name__ == "__main__":
-    main()
-    sys.exit(0);
+	main()
+	sys.exit(0);
