ee3ebdd8a73ad29d076a5ab4adfdd5a732a5cf96
Yuyi Guo <yuyi@fnal.gov>
2014-07-23 13:54:32 -0500

    add new client side unit test for listFileSummaries API

--- Client/tests/dbsclient_t/unittests/DBSClientReader_t.py
+++ Client/tests/dbsclient_t/unittests/DBSClientReader_t.py
@@ -293,6 +293,30 @@ class DBSClientReader_t(unittest.TestCase):
         """test46b unittestDBSClientReader_t.listFileLumis: basic test"""
         self.api.listFileLumis(block_name=self.testparams['block'], run_num=self.testparams['runs'][0])
 
+    def test046c(self):
+        """test46c unittestDBSClientReader_t.listFileSummaries: basic test"""
+        self.api.listFileLumis(dataset=self.testparams['dataset'])
+
+    def test046d(self):
+        """test46d unittestDBSClientReader_t.listFileSummaries: basic test"""
+        self.api.listFileLumis(dataset=self.testparams['dataset'], validFileOnly=1 )
+
+    def test046e(self):
+        """test46e unittestDBSClientReader_t.listFileSummaries: basic test"""
+        self.api.listFileLumis(dataset=self.testparams['dataset'], run_num=self.testparams['runs'][0], validFileOnly=1)
+
+    def test046f(self):
+        """test46f unittestDBSClientReader_t.listFileSummaries: basic test"""
+        self.api.listFileLumis(block_name=self.testparams['block'])
+
+    def test046g(self):
+        """test46g unittestDBSClientReader_t.listFileSummaries: basic test"""
+        self.api.listFileLumis(block_name=self.testparams['block'], validFileOnly=1 )
+
+    def test046h(self):
+        """test46h unittestDBSClientReader_t.listFileSummaries: basic test"""
+        self.api.listFileLumis(block_name=self.testparams['block'], run_num=self.testparams['runs'][0], validFileOnly=1)
+
     def test047(self):
         """test47 unittestDBSClientReader_t.listRuns : basic test"""
         self.assertRaises(dbsClientException, self.api.listRuns)
