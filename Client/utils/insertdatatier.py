c8b0ac160a72ed703627c2f73ee749326582ebd1
Yuyi Guo <yuyi@fnal.gov>
2016-11-17 22:29:06 +0100

    split DBS instances into mutiple servers

--- Client/utils/insertdatatier.py
+++ Client/utils/insertdatatier.py
@@ -4,18 +4,23 @@ from dbs.apis.dbsClient import *
 #url="http://cmssrv18.fnal.gov:8585/dbs3"
 #url for tunneling through lxplus 
 #url="https://localhost:1443/dbs/DBSWriter"
-url="https://cmsweb-testbed.cern.ch/dbs/prod/global/DBSWriter"
-#url="http://cms-xen39.fnal.gov:8787/dbs/int/global/DBSWriter"
+#url="https://cmsweb-testbed.cern.ch/dbs/prod/global/DBSWriter"
+url2="https://dbs3-test1.cern.ch/dbs/dev/global/DBSWriter"
+url1="https://dbs3-test1.cern.ch/dbs/dev/global/DBSReader"
 # API Object    
 #proxy=os.environ.get('SOCKS5_PROXY')
-dbs3api = DbsApi(url=url)
+dbs3api1 = DbsApi(url=url1)
+dbs3api2 = DbsApi(url=url2)
 #datatier = {'data_tier_name' : 'GEN-SIM-DBSDEBUG2' }
-datatier = {'data_tier_name' :'GEN-SIM'}
+datatier = {'data_tier_name' :'TESTSPLIT-YUYI1'}
 try:
     #import pdb
     #pdb.set_trace()
-    #print dbs3api.listDataTiers()
-    dbs3api.insertDataTier(datatier)
+    print( dbs3api1.listDataTiers())
+    print(dbs3api2.listDataTiers())
+    dbs3api2.insertDataTier(datatier)
+    print(dbs3api1.listDataTiers())
+    print(dbs3api2.listDataTiers())
     #dbs3api.insertPrimaryDataset({"primary_ds_type":"mc" , "primary_ds_name":"RelVal151SingleGammaPt35"})
     print("All Done")
 except Exception as e:
