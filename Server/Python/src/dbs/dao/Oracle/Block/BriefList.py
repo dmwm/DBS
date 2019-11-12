862e297529d7a1b1854901b28808f8b2fe4f6c02
Christian Ariza <christian.ariza@gmail.com>
2019-11-12 10:50:30 +0100

    Block BriefList: fix max_lday condition
    In this condition it should check for the max last modification date instead of the creation date parameter.

--- Server/Python/src/dbs/dao/Oracle/Block/BriefList.py
+++ Server/Python/src/dbs/dao/Oracle/Block/BriefList.py
@@ -94,7 +94,7 @@ class BriefList(DBFormatter):
         elif min_ldate != 0 and max_ldate == 0:
             wheresql += "AND B.LAST_MODIFICATION_DATE > :min_ldate "
             binds.update(min_ldate = min_ldate)
-        elif min_cdate ==0 and max_cdate != 0:
+        elif min_ldate ==0 and max_ldate != 0:
             wheresql += "AND B.LAST_MODIFICATION_DATE < :max_ldate "
             binds.update(max_ldate = max_ldate)
         else:
