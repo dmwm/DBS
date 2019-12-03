5f5a0203d04d8428c368bcca30768a2788119ce4
Valentin Kuznetsov <vkuznet@gmail.com>
2019-12-03 09:00:33 -0500

    Add NATS integration and implement PdmV use case

--- Server/Python/src/dbs/web/DBSWriterModel.py
+++ Server/Python/src/dbs/web/DBSWriterModel.py
@@ -18,6 +18,9 @@ from dbs.utils.DBSTransformInputType import transformInputType
 
 import traceback
 
+# CMSMonitoring modules
+from CMSMonitoring.NATS import NATSManager
+
 
 class DBSWriterModel(DBSReaderModel):
     """
@@ -35,6 +38,11 @@ class DBSWriterModel(DBSReaderModel):
 
         if isinstance(urls, type({})):
             config.database.connectUrl = urls['writer']
+
+        # initialize NATS if requested
+        self.nats = None
+        if hasattr(config, 'use_nats') and config.use_nats:
+            self.nats = NATSManager(config.nats_server, topics=config.nats_topics, default_topic='cms.dbs')
 
         DBSReaderModel.__init__(self, config)
 
@@ -240,6 +248,13 @@ class DBSWriterModel(DBSReaderModel):
 
             # need proper validation
             self.dbsDataset.insertDataset(indata)
+            # send message to NATS if it is configured
+            if self.nats:
+                try:
+                    doc = {'dataset':dataset, 'dataset_type': dataset_access_type}
+                    self.nats.publish(doc)
+                except:
+                    pass
         except cjson.DecodeError as dc:
             dbsExceptionHandler("dbsException-invalid-input2", "Wrong format/data from insert dataset input",  self.logger.exception, str(dc)) 
         except dbsException as de:
@@ -375,6 +390,8 @@ class DBSWriterModel(DBSReaderModel):
             if isinstance(indata, dict):
                 indata = [indata]
             indata = validateJSONInputNoCopy("files", indata)
+            tot_size = 0
+            tot_evts = 0
             for f in indata:
                 f.update({
                      #"dataset":f["dataset"],
@@ -387,7 +404,16 @@ class DBSWriterModel(DBSReaderModel):
                      "file_assoc_list":f.get("assoc_list", []),
                      "file_output_config_list":f.get("file_output_config_list", [])})
                 businput.append(f)
+                tot_evts += f.get('event_count', 0)
+                tot_size += f.get('file_size', 0)
             self.dbsFile.insertFile(businput, qInserts)
+            # send message to NATS if it is configured
+            if self.nats and tot_evts and tot_size:
+                try:
+                    doc = {'dataset':f['dataset'], 'evts': tot_evts, 'size': tot_size}
+                    self.nats.publish(doc)
+                except:
+                    pass
         except cjson.DecodeError as dc:
             dbsExceptionHandler("dbsException-invalid-input2", "Wrong format/data from insert File input",  self.logger.exception, str(dc))
         except dbsException as de:
@@ -450,9 +476,15 @@ class DBSWriterModel(DBSReaderModel):
         try:
             if dataset_access_type != "":
                 self.dbsDataset.updateType(dataset, dataset_access_type)
+                # send message to NATS if it is configured
+                if self.nats:
+                    try:
+                        doc = {'dataset':dataset, 'dataset_type': dataset_access_type}
+                        self.nats.publish(doc)
+                    except:
+                        pass
             else:
                 dbsExceptionHandler("dbsException-invalid-input", "DBSWriterModel/updateDataset. dataset_access_type is required.")
-
         except dbsException as de:
             dbsExceptionHandler(de.eCode, de.message, self.logger.exception, de.message)
         except HTTPError as he:
